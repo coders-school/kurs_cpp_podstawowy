26ddc1d497d0f8dc154e943bedccd919aca0c439
RyszardH <rysiekh@gmail.com>
2020-05-14 22:59:42 +0200

    fibonacci

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -2,10 +2,22 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    
+     if(sequence==0) {return 0;}
+      long long first = 0, second = 1;
+     for(int i=0;i<sequence;i++)
+     {            
+            second += first; 
+            first = second-first; 
+     }
+
+    return first;
 }
 
 int fibonacci_recursive(int sequence) {
+    if(sequence==0) {return 0;}
     // TODO: Your implementation goes here
-    return 0;
+    if (sequence ==1) return 1;
+    
+    return fibonacci_recursive(sequence-1)+fibonacci_recursive(sequence-2);
 }
