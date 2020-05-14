a2fbfb1f825ec1da4c9692f1c7a9f66ccca58519
cnuebred <jonatann01ew@gmail.com>
2020-05-15 00:51:43 +0200

    complete fibonacci string

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+long int fibonacci_iterative(int sequence) {
+    if(sequence == 0)return 0;
+    if(sequence == 1)return 1;
+    long int fib[sequence];
+    fib[0] = 0;
+    fib[1] = 1;
+    for(int i = 2; i <= sequence; i++){
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[sequence];
 }
 
-int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+long int fibonacci_recursive(long int sequence) {
+    if(sequence == 0) return 0;
+    if(sequence == 1 || sequence == 2)return 1;
+    sequence = fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    return sequence;
 }
