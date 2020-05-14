fe165254bf1e25c377e4da9e04d94b5ee0bc4b30
DarkAriesX <skrzys2103@gmail.com>
2020-05-14 22:49:50 +0200

    Both functions pass tests

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,26 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int i = 1;
+    int a = 1;
+    int b = 1;
+    int result = 0;
+    while(i <= sequence)
+    {
+        result = a;
+        if(i == sequence)
+            break;
+        i++;
+        a = a+b;
+        result = b;
+        b = a+b;
+        i++;
+    }
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence <= 1)
+         return sequence;
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
