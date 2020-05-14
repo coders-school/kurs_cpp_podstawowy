2ab46c21cb3fa36d22597121321eddf1bd506e8d
MonikaZ <monika.zelechowska@o2.pl>
2020-05-14 21:00:05 +0200

    Homework fibbonacci first version

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -2,10 +2,27 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    if(sequence == 0)
+        return 0;
+    if(sequence == 1 || sequence == 2)
+        return 1;
+    int first = 1;
+    int second = 1;
+    int result = 1;
+    for(int i = 0; i<(sequence-2); ++i){
+        result = first + second;
+        first = second;
+        second = result;
+    }
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
+
     // TODO: Your implementation goes here
-    return 0;
+    if(sequence == 0)
+        return 0;
+    if(sequence == 1 || sequence ==2)
+        return 1;
+    else return fibonacci_recursive(sequence-1)+fibonacci_recursive(sequence-2);
 }
