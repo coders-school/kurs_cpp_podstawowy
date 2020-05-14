d2dc96fb8945197ee1a51c34a370038c768238e8
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-05-14 22:07:49 +0200

    added adding

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -1,7 +1,13 @@
 #pragma once
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
+std::string calculate(const std::string& command, int first, int second)
+{   
+    if (command == "add")
+    {
+        return std::to_string(first + second);
+    }
+    
+    
     return "";
 }
