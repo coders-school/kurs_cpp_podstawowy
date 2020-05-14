4a6b6a8551bed29ce8de1c09a47d6f8f640cc08a
majkel <michal.sobotka.84@gmail.com>
2020-05-14 23:41:19 +0200

    fibonacci iterative

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_iterative(int sequence) {  
+    int fibonacciSum[sequence + 2];
+    fibonacciSum[0] = 0;
+    fibonacciSum[1] = 1;
+    for (auto it = 2; it <= sequence; it++) {
+       fibonacciSum[it] = fibonacciSum[it - 2] + fibonacciSum[it - 1];
+    }
+    return fibonacciSum[sequence];
 }
 
 int fibonacci_recursive(int sequence) {
