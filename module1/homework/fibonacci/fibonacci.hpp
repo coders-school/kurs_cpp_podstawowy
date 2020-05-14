394e638c11623d0687668f512eeee6ad65df8cc5
Kasper Biegun <kasper@alert.pl>
2020-05-14 13:26:43 +0200

    Fibonacci works

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -2,10 +2,30 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    int x = 0;
+    int y = 1;
+    int next = 0;
+    if (sequence == 0) {
+        return x;
+    } else if (sequence == 1) {
+        return y;
+    }
+    for (int i = 1; i < sequence; ++i) {
+        next = x + y;
+        x = y;
+        y = next;
+    }
+    return next;
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) {
+        return 0;
+    } else if (sequence == 1 ) {
+        return 1;  
+    } else {
+        return fibonacci_recursive(sequence - 1) + 
+               fibonacci_recursive(sequence -2);
+    }
 }
