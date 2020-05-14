5535ba9d19828838faaa410ef2d10d52c516bc5f
majkel <michal.sobotka.84@gmail.com>
2020-05-14 23:05:57 +0200

    tolower method for strings

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -1,21 +1,34 @@
 #pragma once
 #include <string>
+#include <cctype>
+#include <algorithm>
+
+std::string stringToLower(std::string command);
 
 std::string calculate(const std::string& command, int first, int second) {
-    if (command == "add") {
+    if (stringToLower(command) == "add") {
         return std::to_string(first + second);
     }
-    else if (command == "subtract") {
+    else if (stringToLower(command) == "subtract") {
         return std::to_string(first - second);
     }
-    else if (command == "multiply") {
+    else if (stringToLower(command) == "multiply") {
         return std::to_string(first * second);
     }
-    else if (command == "divide") {
+    else if (stringToLower(command) == "divide") {
         if (second == 0) {
             return "Division by 0";
         }
         return std::to_string(first / second);
     }
     return "Invalid data";
+}
+
+std::string stringToLower(std::string command)
+{
+    //https://en.cppreference.com/w/cpp/string/byte/tolower
+    std::transform(command.begin(), command.end(), command.begin(),
+                   [](unsigned char c){ return std::tolower(c); }
+                  );
+    return command;
 }
