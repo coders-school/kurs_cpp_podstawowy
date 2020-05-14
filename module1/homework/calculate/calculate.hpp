a278a447aefd87cb184ca4b20f4f637baea0dd3c
Vicx95 <wiktor.zyla31@gmail.com>
2020-05-14 15:11:41 +0200

    subtract works

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -3,9 +3,15 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
+    int result = 0; 
     if(command == "add")
     {
-        int result = first + second;
+        result = first + second;
+        return std::to_string(result);
+    }
+    else if(command == "subtract")
+    {
+        result = first - second;
         return std::to_string(result);
     }
     return "";
