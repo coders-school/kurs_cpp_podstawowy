367b2c875eb19e724a4ae668978e08acd5ae519c
duszek <marcin.dus@gmail.com>
2020-05-14 23:31:04 +0200

    First attempt, ugly

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -1,7 +1,37 @@
 #pragma once
 #include <string>
+#include <sstream>
 
-std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+std::string calculate(const std::string& command, int first, int second)
+{
+    std::stringstream sstr;
+    if (command == "add")
+    {
+        sstr << first + second;
+    }
+    else if (command == "subtract")
+    {
+        sstr << first - second;
+    }
+    else if (command == "multiply")
+    {
+        sstr << first * second;
+    }
+    else if (command == "divide")
+    {
+        if (second == 0)
+        {
+            sstr << "Division by 0";
+        }
+        else
+        {
+            sstr << first / second;
+        }
+    }
+    else
+    {
+        sstr << "Invalid data";
+    }
+
+    return sstr.str();
 }
