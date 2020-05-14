f6e594d0686a9b04fee05409660b2c902ffd4de7
Kuba C <jakub.cieplinski@outlook.com>
2020-05-14 16:23:42 +0200

    Task completed

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(0 == sequence)
+        return 0;
+    else if(1 == sequence)
+        return 1;
+    
+    int fibNumber{0};
+    int first{0};
+    int second{1};
+
+    for(int i = 2; i <= sequence; i++)
+    {
+        fibNumber = first + second;
+        first = second;
+        second = fibNumber;
+    }
+
+    return fibNumber;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(0 == sequence)
+        return 0;
+    else if(2 >= sequence)
+        return 1;
+
+
+    return fibonacci_recursive(sequence-2) + fibonacci_recursive(sequence-1);
 }
