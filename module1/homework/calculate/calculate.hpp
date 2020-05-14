efb842e7028c1f5562ae75571a7f3e03ddf12d36
Kuba C <jakub.cieplinski@outlook.com>
2020-05-14 15:54:30 +0200

    Impelentation completed

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -3,5 +3,24 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    return "";
+    int result {};
+
+
+    if("add" == command)
+        result = first + second;
+    else if("subtract" == command)
+        result = first - second;
+    else if("multiply" == command)
+        result = first * second;
+    else if("divide" == command)
+    {
+        if(0 == second)
+            return "Division by 0";
+        result = first / second;
+    }
+    else
+        return "Invalid data";
+    
+
+    return std::to_string(result);
 }
