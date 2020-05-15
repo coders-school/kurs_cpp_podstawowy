eed696c80c3ce6399be7bfeea4f8bb882182e944
jakjan95 <jakubjanusz95@gmail.com>
2020-05-15 20:00:15 +0200

    calculate works

---
+++ module1/homework/calculate/bulid/CMakeFiles/calculate-ut.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jakub/Desktop/github/kurs_cpp_podstawowy/module1/homework/calculate/test.cpp" "/home/jakub/Desktop/github/kurs_cpp_podstawowy/module1/homework/calculate/bulid/CMakeFiles/calculate-ut.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "googletest-src/googletest/include"
+  "googletest-src/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jakub/Desktop/github/kurs_cpp_podstawowy/module1/homework/calculate/bulid/googletest-build/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/home/jakub/Desktop/github/kurs_cpp_podstawowy/module1/homework/calculate/bulid/googletest-build/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ module1/homework/calculate/bulid/googletest-build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jakub/Desktop/github/kurs_cpp_podstawowy/module1/homework/calculate/bulid/googletest-src/googlemock
+# Build directory: /home/jakub/Desktop/github/kurs_cpp_podstawowy/module1/homework/calculate/bulid/googletest-build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
