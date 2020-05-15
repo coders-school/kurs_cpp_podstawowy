955bef16339192f5ca7dcb607dca5552ced80d9b
mariomoore <mariusz.murawski84@gmail.com>
2020-05-15 21:44:59 +0200

    fobonacci works

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -2,10 +2,23 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) return 0;
+    if (sequence == 1) return 1;
+    int fibo = 0;
+    int before_last = 0;
+    int last = 1;
+    for (int i = 2; i <= sequence; ++i)
+    {
+        fibo = (before_last + last);
+        before_last = last;
+        last = fibo;
+    }
+    return fibo;
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) return 0;
+    if (sequence == 1) return 1;
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
