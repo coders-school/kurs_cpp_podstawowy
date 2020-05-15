49f8a4c35c723e24deaa72fb3e54833ae6aec326
kozlofski <kozlofski.dev@gmail.com>
2020-05-15 10:43:59 +0200

    Recursive and iterative working

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,26 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence == 0) {
+        return 0;
+    }
+    if(sequence < 3) {
+        return 1;
+    } else {
+        int minus_two = 1;
+        int minus_one = 1;
+        int fib = 2;
+        for(int i = 4; i <= sequence; i++) {            
+            minus_two = minus_one;
+            minus_one = fib;
+            fib = minus_two + minus_one;
+        }
+        return fib;
+    }
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence < 1) {return 0;}    
+    return (sequence < 3) ? 1 : fibonacci_recursive(sequence - 1) + 
+        fibonacci_recursive(sequence - 2);
 }
--- module1/homework/fibonacci/main.cpp
+++ module1/homework/fibonacci/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "fibonacci.hpp"
 
+
+
 int main() {
     std::cout << fibonacci_iterative(10) << "\n";
     std::cout << fibonacci_recursive(10) << "\n";
