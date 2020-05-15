255d482d494c9c24c198d2050c780c612d91b046
duszek <marcin.dus@gmail.com>
2020-05-15 22:39:34 +0200

    Fibonnaci first attempt

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -1,37 +1,33 @@
 #pragma once
 #include <string>
-#include <sstream>
 
 std::string calculate(const std::string& command, int first, int second)
 {
-    std::stringstream sstr;
     if (command == "add")
     {
-        sstr << first + second;
+        return std::to_string(first + second);
     }
     else if (command == "subtract")
     {
-        sstr << first - second;
+        return std::to_string(first - second);
     }
     else if (command == "multiply")
     {
-        sstr << first * second;
+        return std::to_string(first * second);
     }
     else if (command == "divide")
     {
         if (second == 0)
         {
-            sstr << "Division by 0";
+            return std::to_string("Division by 0");
         }
         else
         {
-            sstr << first / second;
+            return std::to_string(first / second);
         }
     }
     else
     {
-        sstr << "Invalid data";
+        return std::to_string("Invalid data");
     }
-
-    return sstr.str();
 }
--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,34 @@
 #pragma once
+#include <iostream>
 
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_iterative(int sequence)
+{
+
+    auto tmp = 0;
+    auto first_ = 0;
+    auto second_ = 1;
+
+    if (sequence == 0)
+        return 0;
+    else if (sequence == 1)
+        return 1;
+    else if (sequence > 2)
+    {
+        for (int i = 1; i < sequence; i++)
+        {
+            tmp = second_;
+            second_ = second_ + first_;
+            first_ = tmp;
+        }
+    }
+    return second_;
 }
 
-int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_recursive(int sequence)
+{
+    if (sequence == 0)
+        return 0;
+    else if (sequence == 1)
+        return 1;
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
