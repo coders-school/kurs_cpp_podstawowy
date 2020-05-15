a917751087791135d4cae2eee09cf1efd998559a
Pawellos <pablox377@gmail.com>
2020-05-15 22:08:21 +0200

    working fib

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -2,10 +2,18 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    int total[sequence+2];
+    total[0] = 0;
+    total[1] = 1;
+
+    for(int i = 2; i <= sequence; i++){ 
+        total[i] = total[i-1]+total[i-2];
+    }
+    return total[sequence];    
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    int total = 0;
+    return sequence < 2 ? total += sequence : total += fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2); 
 }
