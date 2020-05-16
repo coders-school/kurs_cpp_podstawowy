839132161584c338ac3c4837ace65de2b3cb7439
Karol Dulemba <kdulemba@gmail.com>
2020-05-16 22:18:38 +0200

    fibbonaci_function

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,32 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+
+        if(sequence==0) return 0;
+
+        if(sequence==1) return 1;
+	int fib=0;
+        int fib_prev = 1;
+	int fib_prev_prev = 0;
+
+	for (int i=2; i<=sequence; i++)
+	{
+		fib = fib_prev + fib_prev_prev;
+		fib_prev_prev = fib_prev;
+		fib_prev = fib;
+
+	}
+
+    	return fib;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+	
+   	if(sequence==0) return 0; 
+
+	if(sequence<3&&sequence>0)
+		return 1;
+	
+	return fibonacci_recursive(sequence-2)+fibonacci_recursive(sequence-1); 
+	
 }
