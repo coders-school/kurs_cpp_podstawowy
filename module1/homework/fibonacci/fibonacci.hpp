45e5691f2d406bbb6db9c92a274e0fec44832bf5
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-05-16 19:19:50 +0200

    homework added

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,22 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+	if (sequence <= 1)
+		return sequence;
+	int last = 1;
+	int nextToLast = 1;
+	int result = 1;
+	for (int i = 2; i < sequence; ++i) {
+		result = last + nextToLast;
+		nextToLast = last;
+		last = result;
+	}
+	return result;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+	if (sequence <= 1)
+		return sequence;
+	return fibonacci_recursive(sequence - 1) +
+		   fibonacci_recursive(sequence - 2);
 }
