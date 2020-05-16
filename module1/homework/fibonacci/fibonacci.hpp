81a385ec40510e7191535d908d2934027f035e17
Kacu <k.ptychu@gmail.com>
2020-05-16 15:01:30 +0200

    fibonacci_recursive works

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -5,7 +5,13 @@ int fibonacci_iterative(int sequence) {
     return 0;
 }
 
-int fibonacci_recursive(int sequence) {
+int fibonacci_recursive(int sequence, int result = 0, int next = 1) {
     // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 0){
+        return result;
+    }
+    return fibonacci_recursive(sequence-1, next, result+next);
 }
+
+
+    
