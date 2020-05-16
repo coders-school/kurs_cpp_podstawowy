cb9d64dd9f08f01786546547eb5394caecf56414
rjanik <r.janik@outlook.com>
2020-05-16 23:14:21 +0200

    fibonacci all

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,37 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    
+    int f1, f2, f;
+
+    if( sequence == 0) {
+        return 0;
+    }
+
+    if( sequence == 1) {
+        return 1;
+    }
+    
+    f1 = 0;
+    f2 = 1;
+    for (size_t i = 2; i <= sequence; i++) {
+        f = f1 + f2;
+        f1 = f2;
+        f2 = f;
+    }
+    
+    return f;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+
+    if( sequence == 0) {
+        return 0;
+    }
+
+    if( sequence == 1) {
+        return 1;
+    }
+
+    return fibonacci_recursive( sequence - 1 ) + fibonacci_recursive( sequence - 2 );
 }
