441ee7676023244bdec076524123b289538af64b
Adrian Baranowicz <adrbaranowicz@gmail.com>
2020-05-17 01:43:31 +0200

    dividing works

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -4,12 +4,16 @@
 std::string calculate(const std::string& command, int first, int second) {
     if ("add" == command){
         return std::to_string(first + second);
-    }
-    else if ("subtract" == command){
+    } else if ("subtract" == command) {
         return std::to_string(first - second);
-    }
-    else if ("multiply" == command){
+    } else if ("multiply" == command) {
         return std::to_string(first * second);
+    } else if ("divide" == command) {
+        if (0 == second) {
+            return "Division by 0";
+        } else {
+            return std::to_string(first / second);
+        }
     }
 
     return "";
