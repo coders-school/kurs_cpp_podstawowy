d9ea6b9529a076e8857c00e5785252773c1e0aaa
kuchichan <pawel.kucharski@hotmail.com>
2020-05-16 20:57:31 +0200

    it works (probably)

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -1,7 +1,24 @@
 #pragma once
 #include <string>
+#include <map>
+#include <functional>
 
-std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+
+std::string
+calculate(const std::string &command, int first, int second) {
+  
+  std::map<std::string, std::function<int(int,int)>> mapOperator;
+  mapOperator["add"] = std::plus<int>();
+  mapOperator["subtract"] = std::minus<int>();
+  mapOperator["divide"] = std::divides<int>();
+  mapOperator["multiply"] = std::multiplies<int>();
+
+  std::map<std::string, std::function<int(int,int)>>::iterator it = mapOperator.find(command);
+  if (mapOperator.count(command) == 0) {
+    return "Invalid data";
+  }
+  if (command == "divide" and !(second)) {
+    return "Division by 0";
+  }
+  return std::to_string(mapOperator[command](first, second));
 }
