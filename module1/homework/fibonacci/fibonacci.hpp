aa1232b1ffe6c2ec86224fdf55c579131a9d8512
Marcinati <marcin.kapusta.pk@gmail.com>
2020-05-16 19:43:51 +0200

    fib rec, fib ite works

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,40 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
+
+    long int fibi[23601];
+
+    switch (sequence)
+    {
+        case 0: {
+            return 0;
+        }
+        case 1:
+        case 2: {
+            return 1;
+        }
+        default: {
+            fibi[1]=1;
+            fibi[2]=1;
+
+            for (int i=3; i<=sequence; i++)
+            {
+                fibi[i]=fibi[i-1]+fibi[i-2];    
+            }
+            return fibi[sequence];
+        }
+
+    }
     return 0;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
+    if (sequence == 0) {
+        return 0;
+    } else if (sequence == 1 || sequence == 2) {
+        return 1;
+    } else {
+        return fibonacci_recursive(sequence-2) + fibonacci_recursive(sequence-1);
+    }
     return 0;
 }
