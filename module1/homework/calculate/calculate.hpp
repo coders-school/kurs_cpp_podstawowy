6acdfa0208b0f93f6bbea8afd824ca7131bd240a
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-05-16 19:17:54 +0200

    code simplified

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -3,32 +3,19 @@
 
 std::string calculate(const std::string& command, int first, int second) {
 int result;
-    // double result;
-    if (command == "add") {
+    if (command == "add")
         result = first + second;
-    }
-    else if (command == "subtract") {
+    else if (command == "subtract")
         result = first - second;
-    }
-    else if (command == "multiply") {
+    else if (command == "multiply")
         result = first * second;
-    }
     else if (command == "divide") {
-        if (second) {
+        if (second)
             result = first / second;
-            //result = static_cast<double>(first) / static_cast<double>(second);
-        }
-        else {
+        else
             return "Division by 0";
-        }
     }
-    else {
+    else
         return "Invalid data";
-    }
-    std::string str = std::to_string(result);
-    // remove trailing zeroes in double
-    //str.erase(str.find_last_not_of('0') + 1, std::string::npos);
-    // remove dot in double
-    //str.erase(str.find_last_not_of('.') + 1, std::string::npos );
-    return str;
+    return std::to_string(result);
 }
