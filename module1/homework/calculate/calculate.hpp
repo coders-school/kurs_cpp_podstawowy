5cd4af7537404e00b39b27926f4645e2090cc4f6
Karol Dulemba <kdulemba@gmail.com>
2020-05-16 22:32:39 +0200

    Kalkulator z poprawkami

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -3,7 +3,6 @@
 
 std::string calculate(const std::string& command, int first, int second) 
 {
-    std::string result;
 	
    if(command=="add" || command=="subtract" || command=="divide" || command=="multiply"){
    char commandLet = command.at(0);
@@ -12,36 +11,32 @@ std::string calculate(const std::string& command, int first, int second)
 	switch (commandLet)
 	{
 	case 'a':
-	result = std::to_string(first + second);
+	return std::to_string(first + second);
         break;
 
 	case 'm':
-	result = std::to_string(first*second);
+	return std::to_string(first*second);
  	break;
 
 	case 'd':
 	if(second==0){
-	result = "Division by 0";
+	return "Division by 0";
 	break;
 	}
 
-	result = std::to_string(first/second);	
+	return std::to_string(first/second);	
 	break;
 
 	case 's':
-	result = std::to_string(first-second);
+	return std::to_string(first-second);
 	break;
 
 	default:
 	break;
 	}
 
-	
-	return result;
    }
    
-   else { 
 	
 	 return "Invalid data";
-   }
 }
