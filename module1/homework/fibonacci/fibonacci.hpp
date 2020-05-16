bb2ae33f9fc7f2a6e3a5a2a0b83f40c3dccbe90d
Kacu <k.ptychu@gmail.com>
2020-05-16 15:07:24 +0200

    fibonacci_iterative works

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,16 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    // TODO: Your implementation goes here    
+    int result = 0;
+    int next = 1;
+
+    for (int i = 0; i < sequence; ++i){
+        int tmp = result;
+        result = next;
+        next += tmp;
+    }
+    return result;
 }
 
 int fibonacci_recursive(int sequence, int result = 0, int next = 1) {
@@ -10,8 +18,6 @@ int fibonacci_recursive(int sequence, int result = 0, int next = 1) {
     if (sequence <= 0){
         return result;
     }
-    return fibonacci_recursive(sequence-1, next, result+next);
+    return fibonacci_recursive(sequence - 1, next, result + next);
 }
-
-
     
