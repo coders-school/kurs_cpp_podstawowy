52f046ab8be1c4d8f1795ca8128e24e60e6e9869
Pawellos <pablox377@gmail.com>
2020-05-16 14:57:49 +0200

    Update after review

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -5,9 +5,7 @@
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
     
-int total = 0;
 int option = 0;
-std::string result;
 std::map<std::string, int> options { {"add", 1}, {"subtract", 2}, {"multiply", 3}, {"divide", 4}  };
 
 for(auto str : options)
@@ -17,35 +15,25 @@ for(auto str : options)
 switch(option){
 
     case 1:
-    total = first + second;
-    result = std::to_string(total);
-    break;
+    return std::to_string(first + second);
 
     case 2:
-    total = first - second;
-    result = std::to_string(total);
-    break;
+    return std::to_string(first - second);
 
     case 3:
-    total = first * second;
-    result = std::to_string(total);
-    break;
-
+    return std::to_string(first * second);
+    
     case 4:
     if(second == 0){
-    	result = "Division by 0";
+    	return "Division by 0";
     	}
     else{	
-    	total = first / second;
-    	result = std::to_string(total);
+    	return std::to_string(first / second);
     }
-    break;
-
+  
     default:
-    result =  "Invalid data";
-    break;
+    return "Invalid data";
 }
 
-return result;
 }
 
