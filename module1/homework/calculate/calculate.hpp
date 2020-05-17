0c2577502ecb9354aef736be07be7c82122964c1
damixiorek <damixiorek@gmail.com>
2020-05-17 16:17:52 +0200

    subtracting, multipling, dividing, invalidData works

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -7,6 +7,23 @@ if (command == "add") {
 
 return std::to_string(first+second);
 
+} else if (command == "subtract") {
+
+return std::to_string(first-second);
+
+} else if (command == "multiply") {
+
+return std::to_string(first*second);
+
+} else if (command == "divide") {
+
+if(second == 0) {return "Division by 0";};
+return std::to_string(first/second);
+
+} else {
+
+return "Invalid data";
+
 }
 
 return "";
