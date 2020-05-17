e1915c9f2c276988feeeee6ce52787878e7f040a
Przemek Malec <przemyslaw.malec1997@gmail.com>
2020-05-17 20:25:10 +0200

    Fibonaci works in both ways

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,28 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
+if(sequence==0)
     return 0;
+int currentResults[3]{0,1,1};
+
+int swap;
+for (size_t i = 1; i < sequence; i++)
+{
+    currentResults[2]=currentResults[0]+currentResults[1];
+    currentResults[0]=currentResults[1];
+    currentResults[1]=currentResults[2];
 }
 
+return currentResults[2];
+    }
+
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence==1)
+        return 1;
+    if(sequence==0)
+        return 0;
+    else
+    {
+       return fibonacci_recursive(sequence-1)+fibonacci_recursive(sequence-2);
+    }
 }
