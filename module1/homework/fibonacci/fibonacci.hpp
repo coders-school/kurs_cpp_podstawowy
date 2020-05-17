927f53761400852a7a2de2e5746d80f176f46c81
Tal <natalia.nowacka.it@gmail.com>
2020-05-17 18:13:20 +0200

    fibonacci algorithm

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,39 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+//here we should only use unsigned int
+int fibonacci_iterative(int sequence) 
+{
+    if (sequence == 0) 
+    {
+        return 0;
+    }
+
+    int result = 0;
+    int numb1 = 1;
+    int numb2 = 1;
+
+    if (sequence <= 2) 
+    {
+        return numb1;
+    }
+
+    for (int i = 3; i <= sequence, i++) 
+    {
+        result = numb1 + numb2;
+        numb1 = numb2;
+        numb2 = result;
+    }
+    
+    return result;
 }
 
-int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+//here we should only use unsigned int
+int fibonacci_recursive(int sequence) 
+{    
+    if (sequence <= 1) 
+    {
+        return sequence;
+    }
+
+    return fibonacci_recursive(n - 2) + fibonacci_recursive(n - 1);
 }
