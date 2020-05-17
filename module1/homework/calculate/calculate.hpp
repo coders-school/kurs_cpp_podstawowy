f4f067c38efa8d4bf9908aa8ad7a742e33c85fa4
Tal <natalia.nowacka.it@gmail.com>
2020-05-17 16:04:56 +0200

    calculate implementation

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -1,7 +1,39 @@
 #pragma once
 #include <string>
+#include <algorithm>
+
+std::string add(int first, int second) {
+    return std::to_string(first + second);
+}
+
+std::string subtract(int first, int second) {
+    return std::to_string(first - second);
+}
+
+std::string multiply(int first, int second) {
+    return std::to_string(first * second);
+}
+
+std::string divide(int first, int second) {
+    return std::to_string(first / second);
+}
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    if (command == "add") {
+        return add(first, second);
+    }
+
+    if (command == "subtract") {
+        return subtract(first, second);
+    }
+
+    if (command == "multiply") {
+        return multiply(first, second);
+    }
+
+    if (command == "divide") {
+        return divide(first, second);
+    }
+
+    return "Invalid data";
 }
