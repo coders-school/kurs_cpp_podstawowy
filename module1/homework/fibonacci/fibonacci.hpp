6ba62e06c84a666bb59eaf9c612df50a66c731ae
Michal Cam <mcamromer@gmail.com>
2020-05-17 10:12:25 +0200

    Implement functions counting the Fibonacci number recursively and iteratively.

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,28 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int sum = 0;
+    int prev_sum = 0;
+    int prev_prev_sum = 0;
+    for(int i = 0; i <= sequence; i++)
+    {
+        if( i == 1)
+        {
+            prev_prev_sum = 0;
+            prev_sum = 1;
+            sum = prev_sum;
+        }
+        else
+        {
+            sum = prev_sum + prev_prev_sum;
+            prev_prev_sum = prev_sum;
+            prev_sum = sum;
+        }
+    }
+    return sum;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence <= 1) return sequence;
+    else return (fibonacci_recursive(sequence-1) + fibonacci_recursive(sequence-2));
 }
