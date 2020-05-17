f6f6b7c415545af5ea13f632010afe2b376bac35
damixiorek <damixiorek@gmail.com>
2020-05-17 17:21:42 +0200

    recursively works

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
+
+
     return 0;
+
 }
 
+
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+
+     if (sequence <= 1) {
+
+     return sequence;
+
+     }
+
+     return fibonacci_recursive(sequence-1) + fibonacci_recursive(sequence-2);
 }
