41daf25b4da152683ef2accef59396c6f359c5d8
Adrian Baranowicz <adrbaranowicz@gmail.com>
2020-05-17 14:12:32 +0200

    fibonacci_iterative implemented

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,20 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 1) {
+        return sequence;
+    }
+
+    int previousValue{1}, 
+        currentValue{1};
+
+    for (int i = 2; i < sequence; ++i) {
+        int tempValue = currentValue;
+        currentValue += previousValue;
+        previousValue = tempValue;
+    }
+
+    return currentValue;
 }
 
 int fibonacci_recursive(int sequence) {
