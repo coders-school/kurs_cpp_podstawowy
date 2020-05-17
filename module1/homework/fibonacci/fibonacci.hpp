ce4c778801669b37ef5a7d74b4d2a40ac5e1bcb2
KrzysztofJ85 <krzysztof.jucha@gmail.com>
2020-05-17 17:44:55 +0200

    iteratice looks good

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,19 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
+int fibonacci_iterative(int sequence)
+{
     // TODO: Your implementation goes here
-    return 0;
+    int current = 0, next = 1, tmp = 0;
+    for (int i = 0; i < sequence; i++)
+    {
+        tmp = current + next;
+        current = next;
+        next = tmp;
+    }
+    return current;
 }
 
-int fibonacci_recursive(int sequence) {
+int fibonacci_recursive(int sequence)
+{
     // TODO: Your implementation goes here
-    return 0;
 }
