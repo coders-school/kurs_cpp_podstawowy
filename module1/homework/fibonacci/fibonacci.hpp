157b07d994269df6765bd32ed4e8691467eb9a5f
kuchichan <pawel.kucharski@hotmail.com>
2020-05-17 16:16:57 +0200

    Fibo working

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,21 @@
 #pragma once
+#include <cstddef>
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int a = 0;
+    int b = 1;
+    int temp;
+    for (std::size_t i = 0; i < sequence; ++i) {
+      temp = b;
+      b = a + b;
+      a = temp;
+    }
+    return a;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+  if (sequence == 0) return 0;
+  if (sequence == 1) return 1;
+
+  return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
