9e37400d6af8dfc7a3b31db3c57fa5243c0fafeb
jakjan95 <jakubjanusz95@gmail.com>
2020-05-18 15:53:21 +0200

    simplification of last condition

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -10,10 +10,7 @@ std::string calculate(const std::string& command, int first, int second) {
     }else if (command == "multiply"){
         return std::to_string(first * second);
     }else if (command == "divide"){
-        if(second == 0){
-            return "Division by 0";
-        } 
-        return std::to_string(first / second);
+        return (second == 0 ? "Division by 0"  : std::to_string(first / second));
     }
     return "Invalid data";
 }
