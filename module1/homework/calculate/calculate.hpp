e9e23ad966cf75da74099bd19f842c1504ec9094
mateusz-talma <talmamateusz@gmail.com>
2020-05-18 22:07:10 +0200

    Add calculate function

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -1,7 +1,34 @@
 #pragma once
 #include <string>
 
+namespace operations 
+{
+    constexpr auto add = "add";
+    constexpr auto sub = "subtract";
+    constexpr auto mul = "multiply";
+    constexpr auto div = "divide";
+    constexpr auto divByZero = "Division by 0";
+}
+
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+
+    std::string res;
+    if (!command.compare(operations::add))
+        res = std::to_string(first + second);
+    else if (!command.compare(operations::sub))
+        res = std::to_string(first - second);
+    else if (!command.compare(operations::mul))
+        res = std::to_string(first * second);
+    else if (!command.compare(operations::div))
+        {
+            if (second == 0)
+                res = operations::divByZero;
+            else
+                res = std::to_string(first / second);
+        }
+    else
+        res = "Invalid data";
+    
+    return res;
+
 }
