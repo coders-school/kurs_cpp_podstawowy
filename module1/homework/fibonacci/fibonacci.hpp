1b6bd14c089327d9759b13d6fe6f04d1b2d27a28
jakjan95 <jakubjanusz95@gmail.com>
2020-05-18 15:46:45 +0200

    added error handling for negative numbers, simplifies iterative version of fibonacci

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,19 +1,32 @@
 #pragma once
 #include <vector>
+#include <stdexcept>
+
 int fibonacci_iterative(int sequence) {
-    std::vector<int> fibbo{0,1};
-    for(int i = 2 ; i < sequence + 1 ; ++i)
-    {
-        fibbo.push_back(fibbo[i-1] + fibbo[i-2]);
+    if(sequence < 0){
+        throw std::runtime_error("Negative number");
+    }else if(sequence <= 1){
+        return sequence;
     }
-    return fibbo[sequence];
+    
+    int first = 0;
+    int second = 1;
+    
+    for(int i = 2 ; i <= sequence ; ++i){
+        int result = first + second;
+        first = second;
+        second = result;
+    }
+
+    return second;
 }
 
 int fibonacci_recursive(int sequence) {
-    if(sequence == 0){
-        return 0;
-    }else if(sequence == 1){
-        return 1;
+    if(sequence < 0){
+        throw std::runtime_error("Negative number");
+    }else if(sequence <= 1){
+        return sequence;
     }
+
     return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }  
