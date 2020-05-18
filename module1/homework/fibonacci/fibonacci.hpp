3ed94161bc0ad5bc86900ba2e01b269abbd1ede0
marekjanek <marekjanus3@wp.pl>
2020-05-19 00:17:40 +0200

    updated fibonacci.hpp file, fibonacci_recursive and fobocnacci_iterative working

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    
+    if(sequence==0)
+        return 0;
+    if(sequence<=2)
+        return 1;
+
+    int res=1, first=0, second=1;
+
+    for(int i = 0;i<sequence-2;i++){
+
+        first=res;
+        res +=second;
+        second = first;
+
+    }
+    return res;
+    
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+
+    if(sequence==0)
+        return 0;
+    if(sequence<=2)
+        return 1;
+        
+    return fibonacci_recursive(sequence-1) + fibonacci_recursive(sequence-2);    
 }
