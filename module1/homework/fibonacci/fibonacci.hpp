6d02b33ec659be191dcea32a9683c90ff6091a74
Nessy90 <nessy0991@gmail.com>
2020-05-18 21:55:59 +0200

    Add homework - fibonacci

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,23 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    
+    int fib = 0, fib1 = 1, fib2 = 1;
+    if (sequence >= 0 && sequence <= 1) return sequence;
+    for(int i=2; i < sequence; i++)
+    {
+        fib = fib1;
+        fib1 = fib2;
+        fib2 += fib;
+    }
+    return fib2;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+
+    if (sequence >= 0 && sequence <= 1) return sequence;
+    else if (sequence >=2) 
+    {
+        return fibonacci_recursive(sequence-1)+fibonacci_recursive(sequence-2);
+    }
 }
