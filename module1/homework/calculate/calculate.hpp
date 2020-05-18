f993bc6266368bc990a8b3e641e464d2deb9d03a
Bartlomiej Gottfried <bargot@o2.pl>
2020-05-19 00:34:15 +0200

    Calculate function passes ut

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -1,7 +1,31 @@
 #pragma once
 #include <string>
 
+
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+
+    if ("add" == command)
+    {
+        return std::to_string( first + second);
+    }
+    else if ("multiply" == command)
+    {
+        return std::to_string( first * second);
+    }
+    else if("subtract" == command)
+    {
+        return std::to_string( first - second);
+    }
+    else if ("divide" == command)
+    {
+        if( 0 != second)
+        {
+            return std::to_string( first / second);
+        }
+        else
+        {
+            return "Division by 0";
+        }
+    }   
+    return "Invalid data";
 }
