41db00558977c04defbc2cf43530447329e2626c
Bartlomiej Gottfried <bargot@o2.pl>
2020-05-19 00:51:38 +0200

    Fibonacci passes ut

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -2,10 +2,24 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    int fib1 = 0;
+    int fib2 = 1;
+    int retval;
+
+    if(0 == sequence) return 0;
+    if(1 == sequence) return 1;
+    
+    for (int i = 2; i <= sequence; i++){
+        retval = fib1 +  fib2;
+        fib1 = fib2;
+        fib2 = retval;
+    }
+    return retval;
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    if(0 == sequence) return 0;
+    if(1 == sequence) return 1;
+    return (fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1));
 }
