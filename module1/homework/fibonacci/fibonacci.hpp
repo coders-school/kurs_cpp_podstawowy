ce5d0f13f1f543d797d2915344c8a976610af327
mateusz-talma <talmamateusz@gmail.com>
2020-05-18 23:03:34 +0200

    Add fibonacci functions

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,36 @@
 #pragma once
+#include<array>
 
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_iterative(int sequence) 
+{
+    std::array<int, 2> prevFib = {0 ,0};
+    int fib = 0;
+    for(int i = 0; i < sequence; i++)
+    {   
+        if (i <= 0)
+        {
+            prevFib.at(0) = 1;
+            fib = 1;
+            continue;
+        }
+        else if (i == 1)
+        {
+            prevFib.at(1) = 1;
+            continue;
+        }
+        fib = prevFib.at(0) + prevFib.at(1);
+        prevFib.at(0) = prevFib.at(1);
+        prevFib.at(1) = fib;
+    }
+    return fib;
 }
 
-int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_recursive(int sequence) 
+{
+    if (sequence <= 0)
+        return 0;
+    else if (sequence <= 2)
+        return 1;
+    else
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
