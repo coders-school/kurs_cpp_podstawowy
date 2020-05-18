8e55d37343b0dfb5bd8a547dc2783631bdb3811c
marekjanek <marekjanus3@wp.pl>
2020-05-18 23:37:03 +0200

    + updated calculate.hpp - working function calculate

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -1,7 +1,31 @@
 #pragma once
 #include <string>
+#include <vector>
+#include <algorithm>
+
+
 
 std::string calculate(const std::string& command, int first, int second) {
+    
     // TODO: Implement your solution here and return proper value
-    return "";
+    std::vector <std::string> comm{"add", "subtract", "multiply", "divide"};
+    auto res = std::find(std::begin(comm), std::end(comm), command);
+    int menu = std::distance(std::begin(comm), res);
+
+    switch(menu)
+    {
+    case 0:
+        return std::to_string(first+second);
+    case 1:
+        return std::to_string(first-second);
+    case 2:
+        return std::to_string(first*second);
+    case 3:
+        if(second==0)
+            return "Division by 0";
+        return std::to_string(first/second);
+    default:
+        return "Invalid data";
+    }
+	
 }
