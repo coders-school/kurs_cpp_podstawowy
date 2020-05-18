e95cde7cef1f62966b40c73e94a126aefc69a576
m-kop <m.kopertowski@epicspace.eu>
2020-05-18 10:29:18 +0200

    added fibonacci functions

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence < 2) {
+        return sequence;
+    } else {
+        int fib_n_2 = 0;
+        int fib_n_1 = 1;
+        int fib_n;
+
+        for (auto i = 2; i <= sequence; i++) {
+            fib_n = fib_n_1 + fib_n_2;
+
+            fib_n_2 = fib_n_1;
+            fib_n_1 = fib_n; 
+        }
+        return fib_n;
+    }
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    return (sequence < 2 ? sequence : fibonacci_recursive(sequence -2) + 
+                                      fibonacci_recursive(sequence - 1));
 }
