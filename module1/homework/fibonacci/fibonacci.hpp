53c5748897dd1a6f44ee2b1bb4f1a168963e3dbb
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-05-19 13:55:43 +0200

    correcting fibonacci.hpp

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -4,15 +4,11 @@ int fibonacci_iterative(int sequence) {
     if (sequence < 0) {
         return -1;
     }
-    if (sequence == 0) {
-        return 0;
-    }
-    if (sequence == 1) {
-        return 1;
-    }
     int fib[sequence + 1];
     fib[0] = 0;
-    fib[1] = 1;
+    if (sequence > 0) {
+        fib[1] = 1;
+    }
     for (int i = 2; i < sequence + 1; ++i) {
         fib[i] = fib[i - 1] + fib[i - 2];
     }
@@ -20,6 +16,5 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
     return 0;
 }
