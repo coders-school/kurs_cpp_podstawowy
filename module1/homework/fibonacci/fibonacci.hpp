87ef5403cd1da5bb6a25170ebe0e63f24a16ab94
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-05-19 11:00:18 +0200

    Update fibonacci.hpp
    fib iterative - removing long int type of an array; fib recursive removing if of sequence == 2;

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -2,7 +2,7 @@
 
 int fibonacci_iterative(int sequence) {
 
-    long int fibi[23601];
+    int fibi[47];
 
     switch (sequence)
     {
@@ -14,16 +14,15 @@ int fibonacci_iterative(int sequence) {
             return 1;
         }
         default: {
-            fibi[1]=1;
-            fibi[2]=1;
+            fibi[1] = 1;
+            fibi[2] = 1;
 
-            for (int i=3; i<=sequence; i++)
+            for (int i = 3; i <= sequence; i++)
             {
-                fibi[i]=fibi[i-1]+fibi[i-2];    
+                fibi[i] = fibi[i-1] + fibi[i-2];    
             }
             return fibi[sequence];
         }
-
     }
     return 0;
 }
@@ -31,7 +30,7 @@ int fibonacci_iterative(int sequence) {
 int fibonacci_recursive(int sequence) {
     if (sequence == 0) {
         return 0;
-    } else if (sequence == 1 || sequence == 2) {
+    } else if (sequence == 1) {
         return 1;
     } else {
         return fibonacci_recursive(sequence-2) + fibonacci_recursive(sequence-1);
