a39d7a7cae5e3f88152df13624f95b4dc95008ce
ArturGubala <gubala-artur@wp.pl>
2020-05-19 19:13:20 +0200

    Iteretive implementation of exercise

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int fib_tmp = 0, fib_current = 0, fib_next = 1;
+    for(int i = 0; i < sequence; i++)
+    {
+        fib_tmp = fib_current + fib_next;
+        fib_current = fib_next;
+        fib_next = fib_tmp;
+    }
+    return fib_current;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
     return 0;
 }
