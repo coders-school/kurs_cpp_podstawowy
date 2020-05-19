c7e564366a39238bdf7d06826a5bfae27e7c5139
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-05-19 13:52:17 +0200

    adding fibonacci iterative

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,22 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence < 0) {
+        return -1;
+    }
+    if (sequence == 0) {
+        return 0;
+    }
+    if (sequence == 1) {
+        return 1;
+    }
+    int fib[sequence + 1];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i < sequence + 1; ++i) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[sequence];
 }
 
 int fibonacci_recursive(int sequence) {
