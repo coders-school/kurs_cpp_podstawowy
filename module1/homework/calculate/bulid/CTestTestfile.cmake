4cf8775047d77e9e85679e554e89739ccfa8f44f
jakjan95 <jakubjanusz95@gmail.com>
2020-05-19 14:08:37 +0200

    cleaning up

--- module1/homework/calculate/bulid/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jakub/Desktop/github/kurs_cpp_podstawowy/module1/homework/calculate
-# Build directory: /home/jakub/Desktop/github/kurs_cpp_podstawowy/module1/homework/calculate/bulid
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(calculate_test "/home/jakub/Desktop/github/kurs_cpp_podstawowy/module1/homework/calculate/bulid/calculate-ut")
-set_tests_properties(calculate_test PROPERTIES  _BACKTRACE_TRIPLES "/home/jakub/Desktop/github/kurs_cpp_podstawowy/module1/homework/calculate/CMakeLists.txt;46;add_test;/home/jakub/Desktop/github/kurs_cpp_podstawowy/module1/homework/calculate/CMakeLists.txt;0;")
-subdirs("googletest-build")
--- module1/homework/calculate/bulid/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jakub/Desktop/github/kurs_cpp_podstawowy/module1/homework/calculate/bulid/googletest-src
-# Build directory: /home/jakub/Desktop/github/kurs_cpp_podstawowy/module1/homework/calculate/bulid/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
