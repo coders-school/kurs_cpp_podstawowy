5bbe66083d430b721889ea36a45d1c0e047edf70
kawapa <kawapa@outlook.com>
2020-05-19 11:34:37 +0200

    Add exercise 1

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -2,6 +2,24 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    if (command == "add")
+        return std::to_string(first + second);
+    if (command == "subtract")
+        return std::to_string(first - second);
+    if (command == "multiply")
+        return std::to_string(first * second);
+    if (command == "divide")
+    {
+        try
+        {
+            if (second == 0)
+                throw "Division by 0";
+            return std::to_string(first / second);
+        }
+        catch (const char * s)
+        {
+            return s;
+        }
+    }
+    return "Invalid data";
 }
