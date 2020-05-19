84bbf9126c944bc6986a04ab8d69c5af3e103d3f
DawidMalarz <kontakt@dawidmalarz.pl>
2020-05-19 21:36:03 +0200

    iterative works

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,19 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 1) {
+        return sequence;
+    }
+    else {
+        int current = 1, one_before = 1, two_before = 0;
+        for (int i = 2; i <= sequence; ++i)
+            {
+                current = one_before + two_before;
+                two_before = one_before;
+                one_before = current;
+            }
+        return current;
+    }
 }
 
 int fibonacci_recursive(int sequence) {
