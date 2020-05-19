f5dd96fec20e5687dc59e21b6db1967c474fc913
zenon <65047424+ms00780@users.noreply.github.com>
2020-05-19 22:13:34 +0200

    all works

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -2,6 +2,38 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    
+      if (command == "add"){
+         std::string add = std::to_string(first + second);
+         return (add);
+     }
+             
+             
+     if (command == "subtract"){
+         std::string substract = std::to_string(first - second);
+         return (substract);
+     }
+             
+             
+     if (command == "multiply"){
+         std::string mnozenie = std::to_string(first * second);
+         return (mnozenie);
+     }
+             
+     if (command == "divide" && second != 0){
+         
+           std::string divide = std::to_string(first / second);
+           return (divide);
+         }
+     if (command == "divide" && second == 0){
+                     return ("Division by 0");
+                   }
+     
+             
+     else {
+         return ("Invalid data");
+     }
+    
+    
 }
+
