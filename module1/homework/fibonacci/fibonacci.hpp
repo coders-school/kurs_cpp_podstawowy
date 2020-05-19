8823fd9449b2201686cd239027283d3ca21e2a7d
kawapa <kawapa@outlook.com>
2020-05-19 12:27:48 +0200

    Add exercise 2

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int first = 0;
+    int second = 1;
+    int temp;
+
+    if (sequence == 0)
+        return first;
+    if (sequence < 2)
+        return second;
+
+    for (int i = 1; i < sequence; ++i)
+    {
+        temp = first + second;
+        first = second;
+        second = temp;        
+    }
+
+    return temp;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0)
+        return 0;
+    if (sequence < 3)
+        return 1;
+
+    return fibonacci_recursive(sequence - 2) +
+        fibonacci_recursive(sequence - 1);
 }
