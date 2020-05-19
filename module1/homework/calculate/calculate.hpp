6ab4dcf000970926a58ed0f93e8f154ff4de1b1a
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-05-19 13:27:46 +0200

    complete calculate.hpp

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -1,7 +1,28 @@
 #pragma once
+#include <algorithm>
+#include <map>
 #include <string>
+#include <vector>
 
-std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
+std::string calculate(const std::string &command, int first, int second)
+{
+    std::map<std::string, int> options{{"add", 0}, {"subtract", 1}, {"multiply", 2}, {"divide", 3}};
+    if (options.find(command) == options.end()) {
+        return "Invalid data";
+    }
+    switch (options[command]) {
+    case 0: {
+        return std::to_string(first + second);
+    } break;
+    case 1: {
+        return std::to_string(first - second);
+    } break;
+    case 2: {
+        return std::to_string(first * second);
+    } break;
+    case 3: {
+        return (second != 0) ? std::to_string(first / second) : "Invalid data";
+    } break;
+    }
     return "";
 }
