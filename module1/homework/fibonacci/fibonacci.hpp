b36a9e502fb1ab6acfd1ff31ac159d5829384d78
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-05-19 14:11:08 +0200

    fibonacci recursive

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -16,5 +16,18 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
+    if (sequence < 0) {
+        return -1;
+    }
+    if (sequence == 0) {
+        return 0;
+    }
+    if (sequence == 1) {
+        return 1;
+    }
+    if (sequence >= 2) {
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    }
+    
     return 0;
 }
