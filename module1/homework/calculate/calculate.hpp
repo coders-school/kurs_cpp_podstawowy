c4c1509c4a5955fd5cd266b56c298ea55cee6815
DawidMalarz <kontakt@dawidmalarz.pl>
2020-05-19 21:20:37 +0200

    subtraction works

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -6,5 +6,8 @@ std::string calculate(const std::string& command, int first, int second) {
     if(command == "add") {
         result =  first + second;
     }
+    else if(command == "subtract") {
+        result =  first - second;
+    }
     return std::to_string(result);
 }
