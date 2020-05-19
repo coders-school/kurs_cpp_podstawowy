02a74b19048d1dbcfd2566f41698184d4442ea81
PiotrD <dywel321@gmail.com>
2020-05-19 15:07:50 +0200

    added fibonacci

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int fib1 = 0;
+    int fib2 = 1;
+    int result;
+    if(sequence <= 1) 
+        return sequence;
+
+    for (int i = 2; i <= sequence; i++){
+        result = fib1 +  fib2;
+        fib1 = fib2;
+        fib2 = result;
+    }
+
+    return result;
+
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence <= 1) 
+        return sequence;
+    return (fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1));
 }
