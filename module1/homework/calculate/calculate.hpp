e3f2b5a68b97f5cbee6c4e0759ce0b25298a0fa2
KrzysztofJ85 <krzysztof.jucha@gmail.com>
2020-05-19 20:31:58 +0200

    first version was shitty, corrections after review added

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -4,37 +4,19 @@
 std::string calculate(const std::string &command, int first, int second)
 {
     // TODO: Implement your solution here and return proper value
-    int opcja = 0;
     if (command == "add")
-        opcja = 1;
-    if (command == "subtract")
-        opcja = 2;
-    if (command == "multiply")
-        opcja = 3;
-    if (command == "divide")
-        opcja = 4;
-
-    switch (opcja)
-    {
-    case 1:
         return std::to_string(first + second);
-        break;
-    case 2:
+    if (command == "subtract")
         return std::to_string(first - second);
-        break;
-    case 3:
+    if (command == "multiply")
         return std::to_string(first * second);
-        break;
-    case 4:
+    if (command == "divide")
+    {
         if (second == 0)
         {
-           return "Division by 0";
+            return "Division by 0";
         }
         return std::to_string(first / second);
-        break;
-    default:
-        return "Invalid data";
-        break;
     }
     return "Invalid data";
 }
