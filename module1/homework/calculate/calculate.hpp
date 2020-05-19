a679af4447b9aafc0d719bb1016a04e94ff3f7db
DawidMalarz <kontakt@dawidmalarz.pl>
2020-05-19 21:21:40 +0200

    multiplication works

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -9,5 +9,8 @@ std::string calculate(const std::string& command, int first, int second) {
     else if(command == "subtract") {
         result =  first - second;
     }
+    else if(command == "multiply") {
+        result =  first * second;
+    }
     return std::to_string(result);
 }
