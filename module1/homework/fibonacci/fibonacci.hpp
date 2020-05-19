3e82347430126f297e1b61cbee3da5d259fe74af
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-05-19 11:41:50 +0200

    Update fibonacci.hpp

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -28,7 +28,7 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-    if (sequence == 0) || (sequence == 1) {
+    if ((sequence == 0) || (sequence == 1)) {
         return sequence;
     } else {
         return fibonacci_recursive(sequence-2) + fibonacci_recursive(sequence-1);
