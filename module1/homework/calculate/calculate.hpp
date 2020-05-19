bf4d2e4ffbe921ae2a987186773fd91fc1b906ec
DawidMalarz <kontakt@dawidmalarz.pl>
2020-05-19 21:23:57 +0200

    division works

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -12,5 +12,13 @@ std::string calculate(const std::string& command, int first, int second) {
     else if(command == "multiply") {
         result =  first * second;
     }
+    else if(command == "divide") {
+        if(second != 0)
+            result =  first / second;
+        else
+            return "Division by 0";
+    }
+    else
+        return "Invalid data";
     return std::to_string(result);
 }
