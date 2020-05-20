33d2e8773b0b99460ba3dcd28192ecd366f1e509
adrianmasiewicz <adrianmasiewicz@gmail.com>
2020-05-20 18:54:02 +0200

    adding works

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -10,6 +10,11 @@ std::string calculate(const std::string& command, int first, int second) {
   if(command == "add") answer = std::to_string(first + second);
   if(command == "subtract") answer = std::to_string(first - second);
   if(command == "multiply") answer = std::to_string(first * second);
+  if(command == "divide") 
+  {
+	  if(second == 0) answer = "Division by 0";  
+	  else answer = std::to_string(first / second);
+  }
 
   return answer;
 
