612602175064c74d5c354d1520e15cf10f1e8e82
ms00780 <65047424+ms00780@users.noreply.github.com>
2020-05-20 20:58:12 +0200

    calculate [20.05.2020]
    I didn't know I could do it so easily! Thanks a lot for help!

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -2,35 +2,24 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    
-      if (command == "add"){
-         std::string add = std::to_string(first + second);
-         return (add);
-     }
-             
-             
-     if (command == "subtract"){
-         std::string substract = std::to_string(first - second);
-         return (substract);
-     }
-             
-             
-     if (command == "multiply"){
-         std::string multiply = std::to_string(first * second);
-         return (multiply);
-     }
-             
-     if (command == "divide"){
-         if (second == 0){
-                     return ("Division by 0");
-                   }
-         else {
-           std::string divide = std::to_string(first / second);
-           return (divide);
-         }
-     }       
-     else {
-         return ("Invalid data");
-     }
+ if (command == "add"){
+      return (std::to_string(first + second));
+  }
+ if (command == "subtract"){
+      return (std::to_string(first - second));
+  }
+ if (command == "multiply"){
+      return (std::to_string(first * second));
+  }
+ if (command == "divide"){
+    if (second == 0){
+      return ("Division by 0");
+    }
+    else {
+      return (std::to_string(first / second));
+    }
+  }
+    else {
+      return ("Invalid data");
+    }
 }
-
