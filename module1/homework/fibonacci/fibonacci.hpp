6a4bab84bf178f314650497fe1aac16d170de904
Kasper Biegun <kasper@alert.pl>
2020-05-20 22:39:50 +0200

    Changes according to review comments

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -2,30 +2,28 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    int x = 0;
-    int y = 1;
-    int next = 0;
+    int pre_previous = 0;
+    int previous = 1;
+    int current = 0;
     if (sequence == 0) {
-        return x;
+        return pre_previous;
     } else if (sequence == 1) {
-        return y;
+        return previous;
     }
     for (int i = 1; i < sequence; ++i) {
-        next = x + y;
-        x = y;
-        y = next;
+        current = pre_previous + previous;
+        pre_previous = previous;
+        previous = current;
     }
-    return next;
+    return current;
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-    if (sequence == 0) {
-        return 0;
-    } else if (sequence == 1 ) {
-        return 1;  
+    if (sequence <= 1) {
+        return sequence; 
     } else {
         return fibonacci_recursive(sequence - 1) + 
-               fibonacci_recursive(sequence -2);
+               fibonacci_recursive(sequence - 2);
     }
 }
