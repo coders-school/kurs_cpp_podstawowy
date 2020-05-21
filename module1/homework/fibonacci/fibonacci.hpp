aced64607b6ccd65043694a3c37db74ef4425156
Anna Smoter <an.smoter@gmail.com>
2020-05-21 17:31:36 +0200

    adding works

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,27 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence == 0) {
+        return 0;
+    }
+    int tab[sequence];
+    tab[0] = 1;
+    tab[1] = 1;
+    for (int i = 2; i < sequence; i++) {
+        tab[i] = tab[i-1] + tab[i-2];
+    }
+    return tab[sequence-1];
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence == 0) {
+        return 0;
+    }
+    if(sequence == 1) {
+        return 1;
+    }
+    if(sequence == 2) {
+        return 1;
+    }
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
