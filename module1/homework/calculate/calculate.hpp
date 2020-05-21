1c06514e26f33ea13ce891b94c17f0385485a3cf
Tomonike <pgurtowski1@gmail.com>
2020-05-21 13:14:49 +0200

    added fourth  functionality divide

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -15,5 +15,9 @@ std::string calculate(const std::string& command, int first, int second) {
         return std::to_string(first * second);
     }
 
+    if (command == "divide"){
+        return std::to_string(first / second);
+    }
+
     return "Invalid data";
 }
