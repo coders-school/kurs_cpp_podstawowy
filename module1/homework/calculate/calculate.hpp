683ab9f6559ab00a9095377fe5e41ee9ebd47fb5
Tomonike <pgurtowski1@gmail.com>
2020-05-21 13:12:22 +0200

    added thrid  functionality multiply

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -10,6 +10,10 @@ std::string calculate(const std::string& command, int first, int second) {
     if (command == "subtract"){
         return std::to_string(first - second);
     }
-  
+    
+    if (command == "multiply"){
+        return std::to_string(first * second);
+    }
+
     return "Invalid data";
 }
