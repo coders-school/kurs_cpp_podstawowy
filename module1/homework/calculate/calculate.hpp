ca29418440e606c0d0f5a0280b93057245bdd30d
Yoka40 <kontodlayoki@gmail.com>
2020-05-21 09:03:59 +0000

    Powinno byc ok

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -2,6 +2,27 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+
+ std::string ret = "Invalid data";
+ //jak zrobić warunek na int ?
+  if(command=="add")
+    {
+     ret = std::to_string(first+second);
+    }
+  else if(command=="subtract")
+    {
+       ret = std::to_string(first-second);
+    }
+  else if(command=="multiply")
+    {
+      ret = std::to_string(first*second);
+    }
+  else if(command=="divide")
+    {
+      if(second == 0)
+      { ret = "Division by 0"; }
+      else
+      { ret = std::to_string(first/second);}
+    }
+  return ret;
 }
