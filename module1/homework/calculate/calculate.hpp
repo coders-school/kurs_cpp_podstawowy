09ee93300ac726f1a7946593a2df3512f8900ad2
Tomonike <pgurtowski1@gmail.com>
2020-05-21 13:10:05 +0200

    added the second functionality subtract

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -6,6 +6,10 @@ std::string calculate(const std::string& command, int first, int second) {
     if (command == "add"){
         return std::to_string(first + second);
     }
+    
+    if (command == "subtract"){
+        return std::to_string(first - second);
+    }
   
     return "Invalid data";
 }
