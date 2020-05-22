d183ae2f4110fe3771f1d5b9378762c230bfe376
Kuba W <winczukjakub@gmail.com>
2020-05-22 13:58:34 +0200

    done nwdNww

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,29 @@
 #pragma once
+#include <cmath>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int mod;
+
+    if(lhs < 0) {
+        lhs = -lhs;
+    }
+    if(rhs < 0) {
+        rhs = -rhs;
+    }
+
+    while(rhs) {
+        mod = lhs % rhs;
+        lhs = rhs;
+        rhs = mod;
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int nwd = NWD(lhs, rhs);
+    if(!nwd) {
+        return 0;
+    }
+
+    return abs(lhs * rhs) / nwd;
 }
