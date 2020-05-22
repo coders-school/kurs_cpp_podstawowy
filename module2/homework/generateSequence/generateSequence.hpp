3d8c3106c891b77163a50dfa81519662c2a3ce60
Pawellos <pablox377@gmail.com>
2020-05-22 20:26:51 +0200

    generateSequence task complete

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -3,5 +3,13 @@
 
 std::vector<int> generateSequence(int count, int step) {
     // TODO: Implement me :)
-    return {};
+    std::vector<int> vec;
+    int move = step;
+
+    for(int i = 0; i <count; i++){
+        vec.push_back(move);
+        move += step;
+    }
+
+    return vec;
 }
