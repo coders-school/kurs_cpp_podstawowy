9073a899999963e8e6947ac1a3bcc9b28884ccdd
kozlofski <kozlofski.dev@gmail.com>
2020-05-22 16:07:13 +0200

    Calculates maximum value of given vector

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -2,7 +2,16 @@
 #include <vector>
 #include <limits>
 
-int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+int maxOfVector(const std::vector<int>& vec) {    
+    if(vec.size() == 0) {
+        return 0;
+    }
+    int max = std::numeric_limits<int>::min();
+
+    for(auto elem : vec) {
+        if(elem > max) {
+            max = elem;
+        }
+    }
+    return max;
 }
