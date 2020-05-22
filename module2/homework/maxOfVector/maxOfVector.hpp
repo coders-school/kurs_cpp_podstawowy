c724dcea90f70d6c32db3ae10f28c2361209a9c2
Pawellos <pablox377@gmail.com>
2020-05-22 18:52:13 +0200

    maxOfVector task complete

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -4,5 +4,12 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    return {};
+    int max_value = vec.front();
+
+    for(const int& elem : vec)
+    {
+        max_value = max_value <= elem ? elem : max_value;
+    }
+
+    return vec.size() == 0 ? 0 : max_value;
 }
