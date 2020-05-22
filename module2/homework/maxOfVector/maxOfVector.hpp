301994bb5fc7ab176c572b230d854b1489293b21
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-05-23 00:50:35 +0200

    empty lines

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -3,14 +3,11 @@
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
-    
     int maxVec = vec[0];
-
     for (size_t i = 0; i < vec.size(); ++i) {
         if (maxVec < vec[i]) {
             maxVec = vec[i];
         }
     }
-
     return maxVec;
 }
