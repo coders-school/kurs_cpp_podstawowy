39b24c1e7b6dbb808328d67d4b78d9404271dc82
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-05-23 00:08:52 +0200

    update if  condition of maxOfVector
    < instead of <=

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -7,7 +7,7 @@ int maxOfVector(const std::vector<int>& vec) {
     int maxVec = vec[0];
 
     for (size_t i = 0; i < vec.size(); ++i) {
-        if (maxVec <= vec[i]) {
+        if (maxVec < vec[i]) {
             maxVec = vec[i];
         }
     }
