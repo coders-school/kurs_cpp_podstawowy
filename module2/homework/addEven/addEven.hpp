de5558c0e335b3515c4814914cade7b9674780d2
Pawellos <pablox377@gmail.com>
2020-05-22 18:27:38 +0200

    addEven task complete

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -1,8 +1,14 @@
-#pragma once
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
     // TODO: Your implementation goes here
     // Below return is only to make this function compile now
-    return -1;
+    //return std::accumulate(numbers.begin(), numbers.end(), 0, [](int i){i % 2 == 0;});
+     int result = 0; 
+    for(const int& elem : numbers)
+    {
+        int Even_number  = elem % 2 == 0 ? elem : 0;
+        result += Even_number;
+    }
+    return result;
 }
