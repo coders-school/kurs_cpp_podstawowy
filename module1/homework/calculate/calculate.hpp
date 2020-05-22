e7e239f4310f5b8a6c9aa57c186dbf03ca26b82f
lisie-sprawy <magdajankowska6@tlen.pl>
2020-05-23 00:14:09 +0200

    adding fixed calculate

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -2,15 +2,19 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-        if(command == "add")
+        if(command == "add"){
         return std::to_string(first + second);
-        if(command == "subtract")
+        }
+        if(command == "subtract"){
         return std::to_string(first - second);
-        if(command == "multiply")
+        }
+        if(command == "multiply"){
         return std::to_string(first * second);
+        }
         if(command == "divide"){
-    	    if(second == 0)
+    	    if(second == 0){
             return "Division by 0";
+            }
         return std::to_string(first/second);
          }
         return "Invalid data";
