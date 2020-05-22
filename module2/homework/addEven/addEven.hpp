3ed49736778877fe64c65cf355de0e57760f982a
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-05-23 00:51:07 +0200

    empty lines

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -2,14 +2,11 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-
     int summary = 0;
-
     for (const auto& element : numbers) {
         if (element % 2 == 0) {
             summary += element;
         }
     }
-
     return summary;
 }
