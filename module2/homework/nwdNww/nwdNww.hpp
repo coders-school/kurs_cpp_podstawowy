2f3df45233d7dc2e2fd8f99fceaafcfaf3938158
mariomoore <mariusz.murawski84@gmail.com>
2020-05-23 14:02:29 +0200

    nwdNww works

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,29 @@
 #pragma once
+#include <algorithm>
+#include <cstdlib>
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs)
+{
+    int higher = std::max(std::abs(lhs), std::abs(rhs));
+    int lower = std::min(std::abs(lhs), std::abs(rhs));
+    int remainder = 0;
+    while (lower)
+    {
+        remainder = higher % lower;
+        higher = lower;
+        lower = remainder;
+    }
+
+    return higher;
 }
 
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWW(int lhs, int rhs)
+{
+    int nwd = NWD(lhs, rhs);
+    if (nwd)
+    {
+        return std::abs(lhs * rhs) / nwd;
+    }
+
+    return 0;
 }
