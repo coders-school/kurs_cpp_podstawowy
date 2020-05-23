f4b8c2715cfab111df94e4b80e1012831c0aa2bd
jakjan95 <jakubjanusz95@gmail.com>
2020-05-23 17:11:17 +0200

    nwd works

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(rhs == 0){
+        return lhs;
+    }
+    return NWD(rhs, lhs % rhs);
 }
 
 int NWW(int lhs, int rhs) {
