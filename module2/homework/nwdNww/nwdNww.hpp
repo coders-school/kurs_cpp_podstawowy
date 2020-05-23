0d9ad669c008f7f39dd58435b120ff6cbe0bc5da
marekjanek <marekjanus3@wp.pl>
2020-05-23 20:59:16 +0200

    + filled functions NWW and NWD

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,36 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs==0 && rhs==0)
+        return 0;
+   
+    lhs=abs(lhs);
+    rhs=abs(rhs);
+
+    int nwd;
+    if(lhs==0 || lhs==rhs)
+        return rhs;
+    if(rhs==0)
+        return lhs;
+
+    while(lhs!=rhs)
+    {
+        if(lhs>rhs)
+            lhs-=rhs;
+        else
+            rhs-=lhs;               
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    if(lhs==0 && rhs==0)
+        return 0;
+    
+    lhs=abs(lhs);
+    rhs=abs(rhs); 
+
+    return (lhs*rhs)/NWD(lhs,rhs);
+
 }
