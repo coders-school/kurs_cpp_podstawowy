1f76b4f352636d5ec59971f96d7be2481de30209
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-05-23 20:19:13 +0200

    addEven done

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -1,8 +1,17 @@
 #pragma once
 #include <vector>
 
-int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+int addEven(const std::vector<int>& numbers)
+{
+    int result = 0;
+    
+    for(const auto& i : numbers)
+    {
+        if (i % 2 == 0)
+        {
+            result += i;
+        }
+    }
+
+    return result;
 }
