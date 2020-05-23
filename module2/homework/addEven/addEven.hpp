153e16f250c02699073b1ccc8e091dee93b16736
TalNovatzky <natalia.nowacka.it@gmail.com>
2020-05-23 15:23:06 +0200

    adding even numbers instead of counting their amount

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -3,12 +3,15 @@
 
 int addEven(const std::vector<int>& numbers)
 {
-    int evenNumbersAmount = 0;
-    for (auto i = numbers.begin(); i != numbers.end(); ++i) {
-        if ((*i >= 2 || *i <= -2) && (*i % 2 == 0))
-        {
-            evenNumbersAmount++;
+    int addEven(const std::vector<int> & numbers)
+    {
+        int sumOfEvenNumbers = 0;
+        for (auto i = numbers.begin(); i != numbers.end(); ++i) {
+            if ((*i >= 2 || *i <= -2) && *i % 2 == 0)
+            {
+                sumOfEvenNumbers += *i;
+            }
         }
+        return sumOfEvenNumbers;
     }
-    return evenNumbersAmount;
 }
