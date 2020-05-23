5101c8682404d889e1045968ff60694b3228c62c
lisie-sprawy <magda.jankowska6@tlen.pl>
2020-05-23 20:35:36 +0200

    adding fibonacci iterative

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int a = 0, b = 1, c = 1;
+    for(int i=1;i<=sequence;i++)
+    {
+        c = b;
+        b = a;
+        a = b + c;
+    }
+    return a;
 }
 
+
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
-}
+  
+    }
