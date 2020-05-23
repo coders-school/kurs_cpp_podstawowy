5d8c03c11b49181aee03c4bf905bac16e1e9bd73
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-05-23 20:24:29 +0200

    generateSequence done

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -1,7 +1,19 @@
 #pragma once
 #include <vector>
 
-std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
+std::vector<int> generateSequence(int count, int step)
+{
+    if(count > 0)
+    {
+        std::vector<int> result;
+        int value = step;
+
+        for(size_t i = 0; i < count; i++)
+        {
+            result.push_back(value);
+            value += step;
+        }
+        return result;
+    }
     return {};
 }
