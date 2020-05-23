338bf557ea40ccf78a7d92f3ce8d73f3154efcd3
Tomasz Eichler <tomasz.eichler@wp.pl>
2020-05-23 13:00:19 +0200

    Added simple fibonacci recursive function

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
+int fibonacci_iterative(int sequence){
     // TODO: Your implementation goes here
     if(sequence == 0){
         return 0;
@@ -21,7 +21,14 @@ int fibonacci_iterative(int sequence) {
     return fibo_n_1;
 }
 
-int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_recursive(int sequence){
+
+    if(sequence == 0){
+        return 0;
+    }
+    else if(sequence == 1){
+        return 1;
+    }
+
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
