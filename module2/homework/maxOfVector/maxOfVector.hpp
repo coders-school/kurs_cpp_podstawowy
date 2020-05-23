53bee9c9e5ec17f6507b680bcb0b97d04aa77f46
TalNovatzky <natalia.nowacka.it@gmail.com>
2020-05-23 14:58:44 +0200

    Adding maxOfVector implementation

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -2,7 +2,15 @@
 #include <vector>
 #include <limits>
 
-int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+int maxOfVector(const std::vector<int>& vec)
+{
+    int maxValue = *vec.begin();
+    for (auto i = vec.begin(); i != vec.end(); ++i) 
+    {
+        if (maxValue < *i)
+        {
+            maxValue = *i;
+        }
+    }
+    return maxValue;
 }
