df4f0b5afc00ea3a0d3feae5ae73e53013dbb3b0
TalNovatzky <natalia.nowacka.it@gmail.com>
2020-05-23 15:17:25 +0200

    adding check also for signed ints

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -5,7 +5,7 @@ int addEven(const std::vector<int>& numbers)
 {
     int evenNumbersAmount = 0;
     for (auto i = numbers.begin(); i != numbers.end(); ++i) {
-        if (*i >= 2 && *i % 2 == 0)
+        if ((*i >= 2 || *i <= -2) && (*i % 2 == 0))
         {
             evenNumbersAmount++;
         }
