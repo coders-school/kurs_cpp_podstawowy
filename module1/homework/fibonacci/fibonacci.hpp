e02b709bb4bcecc807b032b18a696a26536e843f
lisie-sprawy <magda.jankowska6@tlen.pl>
2020-05-23 20:39:04 +0200

    adding fibonacci recursive

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -13,5 +13,9 @@ int fibonacci_iterative(int sequence) {
 
 
 int fibonacci_recursive(int sequence) {
-  
+    if(sequence == 0)
+        return 0;
+    if(sequence == 1)
+        return 1;
+        return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1);
     }
