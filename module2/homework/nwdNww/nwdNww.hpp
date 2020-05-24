e45afe9861e4e881a5356aa271fc9c705d102a20
MonikaZ <monika.zelechowska@o2.pl>
2020-05-24 14:34:36 +0200

    nwdNww solution

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,18 @@
 #pragma once
+#include <cstdlib>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int max_divider =0;
+    for(int i = 1; (i <= abs(lhs) || i <= abs(rhs)); ++i){
+        if(lhs%i == 0 & rhs%i == 0)
+            max_divider = i;
+    }
+    return max_divider;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (NWD(lhs, rhs)!= 0)
+        return abs(lhs*rhs)/NWD(lhs, rhs);
+    else 
+        return abs(lhs*rhs); 
 }
