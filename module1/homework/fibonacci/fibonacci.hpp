36527e8e437a8772231994bf782d3090b3ef3203
Krzysztof Kucharski <krzysztof85@gmail.com>
2020-05-24 23:26:36 +0200

    working fibonacci

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int fibFirst = 0;
+    int fibSecond = 1;
+    for(int i = 0; i < sequence; i++ ){
+        fibSecond += fibFirst;
+        fibFirst = fibSecond - fibFirst;
+    }
+    return fibFirst;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence == 0) return 0; 
+    if(sequence == 1) return 1; 
+    else return fibonacci_recursive(sequence-1)+fibonacci_recursive(sequence-2); 
 }
