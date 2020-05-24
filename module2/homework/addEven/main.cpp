302fc53c9508bede384ef1b5215c1a3a855308b1
McKucia <macikucia@onet.pl>
2020-05-24 18:03:48 +0200

    addEven-working

--- module2/homework/addEven/main.cpp
+++ module2/homework/addEven/main.cpp
@@ -2,6 +2,16 @@
 #include <vector>
 #include "addEven.hpp"
 
+int addEven( std::vector<int>& vec )
+{
+    int sum= 0;
+    for(const auto& el : vec){
+        if( el%2 == 0)
+            sum+= el;
+    }
+    return sum;
+}
+
 int main() {
     std::vector<int> vec{1, 2, 3, 4, 5};
     std::cout << addEven(vec) << "\n";
