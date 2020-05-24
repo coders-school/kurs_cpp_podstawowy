ac818fe2643bb46bfab31be5f73b110538ab2ff0
duszek <marcin.dus@gmail.com>
2020-05-24 21:46:37 +0200

    Generater seq with std::generate

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -1,7 +1,12 @@
 #pragma once
 #include <vector>
+#include <algorithm>
 
-std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+std::vector<int> generateSequence(int count, int step)
+{
+    if (count < 0)
+        return {};
+    std::vector<int> v(count);
+    std::generate(v.begin(), v.end(), [n = 0, step]() mutable { return n += step; });
+    return v;
 }
