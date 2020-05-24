a52ab7ba8389f433ce1b5cdb0b4284dcbd88ad6b
duszek <marcin.dus@gmail.com>
2020-05-24 15:05:28 +0200

    clang format only

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -3,8 +3,9 @@
 #include <algorithm>
 #include <numeric>
 
-int addEven(const std::vector<int>& numbers) {
+int addEven(const std::vector<int>& numbers)
+{
     std::vector<int> result;
-    std::copy_if(numbers.begin(), numbers.end(), std::back_inserter(result), [](int i){return i%2==0;} );
+    std::copy_if(numbers.begin(), numbers.end(), std::back_inserter(result), [](int i) { return i % 2 == 0; });
     return std::accumulate(result.begin(), result.end(), 0);
 }
