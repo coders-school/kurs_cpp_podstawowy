a243e0f375cd90c16f0118bd7c5353259be1e09a
PiotrD <dywel321@gmail.com>
2020-05-24 17:13:31 +0200

    added nwdNww

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs)
+{
+    if (rhs == 0) {
+        return std::abs(lhs);
+    }
+    return NWD(rhs, lhs % rhs);
 }
 
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWW(int lhs, int rhs)
+{
+    if (lhs == 0 || rhs == 0) {
+        return 0;
+    }
+    return std::abs(lhs * rhs) / NWD(lhs, rhs);
 }
