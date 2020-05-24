6085f0cfea1a8bea8817cb06f9a0d6c413e4fd7b
Michal Cam <mcamromer@gmail.com>
2020-05-24 14:43:18 +0200

    The NWD function is able to return The Greatest common divisor for 2 given positive numbers.

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,8 +1,28 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int dividend, divider, result = 1;
+    if(rhs > lhs)
+    {
+        dividend = rhs;
+        divider = lhs;
+    }
+    else
+    {
+        dividend = lhs;
+        divider = rhs;
+    }
+    
+    for(int i = divider; i >= 1; i--)
+    {
+        if(dividend%i == 0 && divider%i == 0)
+        {
+            result = i;
+            break;
+        }
+    }
+
+    return result;
 }
 
 int NWW(int lhs, int rhs) {
