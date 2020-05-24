ac3690fece837f432239494c0829af4251ff058e
MonikaZ <monika.zelechowska@o2.pl>
2020-05-24 14:50:07 +0200

    maxOfVector solution

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -3,6 +3,10 @@
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int max_vector_value = vec.at(0);
+    for(const auto& element : vec){
+        if(element > max_vector_value)
+            max_vector_value = element;
+    }
+    return max_vector_value;
 }
