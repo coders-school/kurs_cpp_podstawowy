71a9ed5c0af37d68b52d8b00d893bd9986bd556a
DawidJ <dawid.d.jozefczak@gmail.com>
2020-05-24 17:00:12 +0200

    division works

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -1,8 +1,7 @@
 #pragma once
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
+std::string calculate(const std::string& command, int first, int second){
     if(command == "add"){
         return std::to_string(first + second);
     }
@@ -10,7 +9,17 @@ std::string calculate(const std::string& command, int first, int second) {
         return std::to_string(first - second);
     }
     else if(command == "multiply"){
-        return std::to_string(first*second);
+        return std::to_string(first * second);
     }
-    return "";
+    else if(command == "divide"){
+        if(second != 0){
+            return std::to_string(first / second);
+        }
+        else{
+            return "Division by 0";
+        }
+    }
+    else{
+        return "Invalid data";
+    }
 }
