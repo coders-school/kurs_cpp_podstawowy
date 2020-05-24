34cc64851093311f5e6c1a25a620785065c41350
Kamil <pankamilinwestuje@gmail.com>
2020-05-24 22:01:02 +0200

    Update fibonacci.hpp
    Solution

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    
+    if( (sequence == 0) || (sequence == 1) ){
+    return sequence;
+    }
+    
+    int a = 1; 
+    int b = 1;
+    
+    for(int i = 0; i < sequence - 2; i++) {
+        std::swap(a, b);
+        b += a;
+    }
+    return b;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0 || sequence == 1){
+        return sequence;
+        }
+    return (fibonacci_recursive(sequence - 1) + fibonacci_recursive(int sequence - 2));
 }
