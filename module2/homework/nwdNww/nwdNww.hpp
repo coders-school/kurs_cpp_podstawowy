d21586a860cdcc1871f8569a37cd1fde96963b85
m-kop <m.kopertowski@epicspace.eu>
2020-05-24 20:59:51 +0200

    add NWW() implementation

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -30,6 +30,16 @@ int NWD(int lhs, int rhs) {
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    
+    if ((lhs == 0) or (rhs == 0)) {
+        return 0;
+    }
+
+    auto nww = (lhs * rhs) / NWD(lhs, rhs);
+
+    if (nww < 0) {
+        nww = -nww;
+    }
+
+    return nww;
 }
