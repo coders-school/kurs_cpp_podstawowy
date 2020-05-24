55a30ade8a595cadb0432aac95aafc0babe1913d
Andrzej Rafalski <anraf1001@gmail.com>
2020-05-24 12:27:54 +0200

    Changes to generateSequence

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -3,15 +3,14 @@
 
 std::vector<int> generateSequence(int count, int step) {
     std::vector<int> sequence;
+
     if (count <= 0) {
         return sequence;
     }
-    for (int i = 0; i < count; i++) {
-        if (i == 0) {
-            sequence.push_back(step);
-        } else {
-            sequence.push_back(sequence[i - 1] + step);
-        }
+
+    sequence.push_back(step);
+    for (int i = 1; i < count; i++) {
+        sequence.push_back(sequence[i - 1] + step);
     }
 
     return sequence;
