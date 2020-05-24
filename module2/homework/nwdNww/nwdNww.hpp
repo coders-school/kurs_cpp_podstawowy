bae319b3987c4f39a4c0491a568e7d5bc10bd8a3
BreWal <waldemar.brejecki@gmail.com>
2020-05-24 22:40:46 +0200

    nww and nwd work

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,53 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    if (lhs == 0)
+    {
+        return rhs;
+    }
+
+    if (rhs == 0)
+    {
+        return lhs;
+    }
+
+    int bufor;
+    
+    if (rhs>lhs)
+    {
+        int pom = lhs;
+        lhs = rhs;
+        rhs = pom;
+    }
+
+    while (rhs!=0)
+    {
+        bufor =lhs % rhs;
+        lhs = rhs;
+        rhs = bufor;
+    }
+
+    if (lhs<0)
+    {
+        lhs=-lhs;
+    }
+
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    if(lhs==0 || rhs==0)
+    {
+        return 0;
+    }
+    auto nww = (lhs / NWD(lhs, rhs) * rhs);
+
+    if(nww < 0)
+    {
+        nww=-nww;
+    }
+
+    return nww;
 }
