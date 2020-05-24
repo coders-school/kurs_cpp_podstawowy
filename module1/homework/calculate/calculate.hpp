3394483010e1e1f343dfed12c21be2f07da4c94b
Kamil <pankamilinwestuje@gmail.com>
2020-05-24 22:56:20 +0200

    Update calculate.hpp

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -2,28 +2,22 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    int result = 0;
-      
+    
     if (command == "add"){
-        result = first + second;
-        return std::to_string(result);
+        return std::to_string(first + second);
       } 
     else if (command == "multiply"){
-        result = first * second;
-        return std::to_string(result);
+        return std::to_string(first * second);
       }
     else if(command == "subtract"){
         result = first - second;
-        return std::to_string(result);
+        return std::to_string(first - second);
       }
     else if(command == "divide"){
         if (second == 0){
         return std::string("Division by Zero");
         }
-        result = first / second;
-        return std::to_string(result);
+        return std::to_string(first / second);
       }
-
     return "Invalid data";
-
 }
