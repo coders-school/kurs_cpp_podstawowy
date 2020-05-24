4a79a14553df7bfb6e0732713c5b2ae6383382c1
Michal Muszynski <zgredek2318@gmail.com>
2020-05-24 19:45:31 +0200

    everything works

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -2,6 +2,43 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    std::string result("0");
+	if(command == "add" || command == "subtract" || command == "multiply" || command == "divide")
+	{
+    char operation = command[0];
+	
+	switch (operation) 
+	{
+	case 'a':
+		result = std::to_string(first + second);
+		return result;
+		break;
+	case 's':
+		result = std::to_string(first - second);
+		return result;
+		break;
+	case 'm':
+		result = std::to_string(first * second);
+		return result;
+		break;
+	case 'd':
+		if (second != 0)
+		{
+			result = std::to_string(first / second);
+			return result;
+		}
+		else
+		{
+			
+			return "Division by 0"; 
+		}
+		break;
+	
+	}
+	
 }
+
+return "Invalid data";
+}
+
+
