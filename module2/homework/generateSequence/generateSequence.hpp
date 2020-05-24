aabff317165fd758a88ba192be7a9b7087a5a6ee
MonikaZ <monika.zelechowska@o2.pl>
2020-05-24 15:10:43 +0200

    generateSequence solution

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -2,6 +2,9 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> generatedVec {};
+    for(int i = 0; i < count; ++i){
+        generatedVec.emplace_back(step*(i+1));
+    }
+    return generatedVec;
 }
