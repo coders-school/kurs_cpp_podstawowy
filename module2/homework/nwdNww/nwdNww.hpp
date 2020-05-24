4e891dbfaa5acb94af9af1c07a1b1b75c96ca2ba
m-kop <m.kopertowski@epicspace.eu>
2020-05-24 20:52:35 +0200

    add NWD() implementation

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,8 +1,32 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int nwd;
+    int remainder;
+
+    if (lhs == 0) {
+        return rhs;
+    } else if (rhs == 0) {
+        return lhs;
+    }
+
+    do {
+        remainder = lhs % rhs;
+
+        if (remainder == 0) {
+            nwd = rhs;
+        } else {
+            lhs = rhs;
+            rhs = remainder;
+        }
+
+    } while (remainder != 0);
+
+    if (nwd < 0) {
+        nwd = -nwd;
+    }
+
+    return nwd;
 }
 
 int NWW(int lhs, int rhs) {
