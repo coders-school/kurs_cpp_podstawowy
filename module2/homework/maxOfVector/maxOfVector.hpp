862e7c49342f8389abb07e93d7efc76cb5fd0ee6
PiotrD <dywel321@gmail.com>
2020-05-24 16:49:10 +0200

    added maxOfVector

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -1,8 +1,10 @@
 #pragma once
 #include <vector>
 #include <limits>
+#include <set>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    std::set<int> mySet(vec.begin(), vec.end());
+    std::set<int>::iterator it= std::prev(mySet.end());
+    return (*it);
 }
