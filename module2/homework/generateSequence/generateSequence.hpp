909dddac16b5ff717bbd554a3c26c8641210f3c4
Michal Cam <mcamromer@gmail.com>
2020-05-24 21:47:21 +0200

    Define generateSequence function.

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -2,6 +2,14 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    if(count <= 0) return std::vector<int>{};
+    auto next_elem = step;
+    std::vector<int> sequence{};
+    sequence.reserve(count);
+    for(int i = 0; i < count; i++)
+    {
+        sequence.push_back(next_elem);
+        next_elem += step;
+    }
+    return sequence;
 }
