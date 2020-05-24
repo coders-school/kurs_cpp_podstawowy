1b02754fa3c3349fa940f71566ebd79c4df5a727
DarkAriesX <skrzys2103@gmail.com>
2020-05-24 14:48:22 +0200

    NWW & NWD works

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,30 @@
 #pragma once
+#include <cmath>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int buff;
+    if(lhs == 0)
+        return rhs;
+    else if(rhs == 0)
+        return lhs;    
+    while(true){
+        if(lhs%rhs == 0){
+            return std::abs(rhs);
+        }
+        buff = lhs%rhs;
+        lhs = rhs;
+        rhs = buff;
+    }
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if((lhs == 0)||(rhs == 0))
+        return 0;
+    int i = 1;
+    while(true){
+        if((lhs*i)%rhs == 0)
+            return std::abs(lhs*i);
+        else
+            i++;
+    }
 }
