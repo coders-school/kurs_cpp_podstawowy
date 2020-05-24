931031f2baca04faf6d0536a8302ac97850f9ea5
PiotrD <dywel321@gmail.com>
2020-05-24 09:59:17 +0200

    added addEven

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -1,10 +1,22 @@
 #pragma once
+#include <iostream>
 #include <vector>
+#include <numeric>
+
+
+bool checkIfEven(int value){
+    return value%2 ==0 ;
+}
+
+int myBinaryPredicate(int a, int b){
+    return checkIfEven(b) ? a+b : a;
+    
+}
+
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int sum = std::accumulate(numbers.begin(), numbers.end(), 0, myBinaryPredicate);
+    return sum;
 }
 
 //test
