c52e72f44d94d244b6109ff16a1a0278d2d244bd
ArturGubala <gubala-artur@wp.pl>
2020-05-25 17:24:56 +0200

    nwd works fine

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,8 +1,17 @@
 #pragma once
+#include <cstdlib>
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs) 
+{
+    int factor = 0;
+    while (rhs != 0)
+    {
+        factor = rhs;
+        rhs = lhs % rhs;
+        lhs = factor;
+    }
+
+    return abs(lhs);
 }
 
 int NWW(int lhs, int rhs) {
