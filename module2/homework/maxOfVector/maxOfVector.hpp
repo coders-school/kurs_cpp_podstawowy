8bada2eba97e269c8b63c50a3122bf6ff92cd2c5
duszek <marcin.dus@gmail.com>
2020-05-25 13:13:02 +0200

    maxVector with BST + smart pointers recall

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -1,8 +1,63 @@
 #pragma once
-#include <vector>
 #include <limits>
-#include <algorithm>
+#include <memory>
+#include <vector>
 
-int maxOfVector(const std::vector<int>& vec) {
-    return *(std::max_element(std::begin(vec), std::end(vec)));
+struct Node
+{
+    int data;
+    std::shared_ptr<Node> left;
+    std::shared_ptr<Node> right;
+
+    Node(int d)
+    {
+        data = d;
+        left = right = nullptr;
+    }
+};
+
+int findMax(std::shared_ptr<Node> node)
+{
+    std::shared_ptr<Node> current = node;
+    while (current->right != nullptr)
+        current = current->right;
+
+    return (current->data);
+}
+
+struct BinaryTree
+{
+    std::shared_ptr<Node> insert(std::shared_ptr<Node> root, int data)
+    {
+        if (root == nullptr)
+        {
+            return std::shared_ptr<Node>(new Node(data));
+        }
+        else
+        {
+            std::shared_ptr<Node> cur;
+            if (data <= root->data)
+            {
+                cur = insert(root->left, data);
+                root->left = cur;
+            }
+            else
+            {
+                cur = insert(root->right, data);
+                root->right = cur;
+            }
+            return root;
+        }
+    }
+};
+
+int maxOfVector(const std::vector<int>& vec)
+{
+    BinaryTree bt;
+    std::shared_ptr<Node> current = nullptr;
+    for (auto i : vec)
+    {
+        current = bt.insert(current, i);
+    }
+    return findMax(current);
 }
