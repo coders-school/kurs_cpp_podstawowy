730f8d178c092b2789e964ec10323989036e37aa
majkel <michal.sobotka.84@gmail.com>
2020-05-25 23:00:45 +0200

    first methode for NWD algorithm

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,7 +1,25 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
+    if (rhs == 0) {
+        return lhs;
+    }
+    if (lhs == 0) {
+        return rhs;
+    }
+    if (lhs == rhs) {
+        return lhs;
+    }
+    int modulo = lhs > rhs ? abs(lhs % rhs) : abs(rhs % lhs);
+    int higher = lhs > rhs ? abs(rhs) : abs(lhs);
+    int divided = higher;
+    while (modulo != 0) {
+        higher = modulo;
+        modulo = divided % modulo;
+        divided = higher;
+    }
+
+    return divided;
     return -1;
 }
 
