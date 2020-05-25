79d1e05f64b9a9a26998e4fb7a411cddd03f7bfd
SzymonGajewski <szymongaju@gmail.com>
2020-05-25 10:01:44 +0200

    Excercise Calculate done

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -5,10 +5,18 @@ std::string calculate(const std::string& command, int first, int second) {
 std::string result{};    
     if(command == "add")
          result = std::to_string(first+second);
-    if(command == "subtract")
+    else if(command == "subtract")
         result = std::to_string(first-second);
-    if(command == "multiply")
+    else if(command == "multiply")
         result = std::to_string(first*second);
-
+    else if(command == "divide"){
+        if(second == 0)
+            result = "Division by 0";
+        else 
+            result = std::to_string(first/second);
+    }
+    else
+        result = "Invalid data";
+    
     return result;
 }
