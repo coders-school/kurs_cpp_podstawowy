482215ec90a99cfaf8d3ab9eb41316b745229568
ArturGubala <gubala-artur@wp.pl>
2020-05-25 10:28:20 +0200

    function which create sequence work fine

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -1,7 +1,20 @@
 #pragma once
 #include <vector>
 
-std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+std::vector<int> generateSequence(int count, int step) 
+{
+    std::vector<int> sequenceResult;
+
+    if (count <= 0)
+    {
+        return sequenceResult;
+    }
+    else
+    {
+        for (size_t i = 0; i < count; i++)
+        {
+            sequenceResult.push_back(step * (i+1));
+        }
+        return sequenceResult;
+    }
 }
