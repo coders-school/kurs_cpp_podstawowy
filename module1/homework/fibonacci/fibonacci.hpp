636577608b5bd9f53e07850f2578ddad411b1981
Kali Live user <wiktorbigaj@wp.pl>
2020-05-25 19:46:03 +0000

    Fixed code formatting

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,29 +1,29 @@
 #pragma once
 
 int fibonacci_iterative(int sequence){
-  int firstNumber = 0;
-  if(sequence == 0){
-    return firstNumber;
-  }
-  int secondNumber = 1;
-  if(sequence == 1){
-    return secondNumber;
-  }
-  for(int i = 2; i <= sequence; i++){
-    int tmp = firstNumber + secondNumber;
+    int firstNumber = 0;
+    if(sequence == 0){
+        return firstNumber;
+    }
+    int secondNumber = 1;
+    if(sequence == 1){
+        return secondNumber;
+    }
+    for(int i = 2; i <= sequence; i++){
+        int tmp = firstNumber + secondNumber;
     firstNumber = secondNumber;
     secondNumber = tmp;
-  }
-  return secondNumber;
+    }
+    return secondNumber;
 }
 
 int fibonacci_recursive(int sequence){
-  if(sequence == 0){
-    return 0;
-  }
-  if(sequence == 1){
-    return 1;
-  }
-  return fibonacci_recursive(sequence -2) + 
-    fibonacci_recursive(sequence -1);
+    if(sequence == 0){
+        return 0;
+    }
+    if(sequence == 1){
+        return 1;
+    }
+    return fibonacci_recursive(sequence -2) + 
+        fibonacci_recursive(sequence -1);
 }
