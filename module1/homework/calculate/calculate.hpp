d976f5050184720c64d6d67c95f0de602911234e
Kali Live user <wiktorbigaj@wp.pl>
2020-05-25 20:04:58 +0000

    Fixed code formatting

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -2,21 +2,21 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second){
-  if(command == "add"){
-    return std::to_string(first + second);
-  }
-  if(command == "subtract"){
-    return std::to_string(first - second);   
-  }
-  if(command == "multiply"){
-    return std::to_string(first * second);       
-  }
-  if(command == "divide"){
-    if(second == 0){
-      return "Division by 0";
-    } else {
-      return std::to_string(first / second);  
+    if(command == "add"){
+        return std::to_string(first + second);
     }
-  }
-  return  "Invalid data";
+    if(command == "subtract"){
+        return std::to_string(first - second);
+    }
+    if(command == "multiply"){
+        return std::to_string(first * second);
+    }
+    if(command == "divide"){
+        if(second == 0){
+            return "Division by 0";
+        } else {
+            return std::to_string(first / second);
+        }
+    }
+    return  "Invalid data";
 }
