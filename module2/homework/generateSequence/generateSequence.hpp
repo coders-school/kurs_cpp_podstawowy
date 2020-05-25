0f5e9d9967c17d508e76c43c9ac66dc3bb6b452c
majkel <michal.sobotka.84@gmail.com>
2020-05-25 23:07:20 +0200

    genereateSequence metode, all tests passed

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -2,6 +2,13 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    if (count <= 0) {
+        return {};
+    }
+    std::vector<int> vec {step};
+    for (auto it = 1; it < count; it++) {
+        vec.emplace_back(vec[it - 1]+ step);
+    }
+
+    return vec;
 }
