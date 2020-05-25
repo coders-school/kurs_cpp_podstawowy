c0ecbb10714af0ecfc57503046c5d8b79824f411
mike <mike@MacBook-Air-mike.local>
2020-05-25 18:46:17 +0200

    [task] Solution for calculation method

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -3,5 +3,19 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     // TODO: Implement your solution here and return proper value
-    return "";
+    std::string result("");
+    if(command == "add") return std::to_string(first+second);
+    if(command == "subtract") return std::to_string(first-second);
+    if(command == "multiply") return std::to_string(first*second);
+    if(command == "divide"){
+        if(second != 0)
+        {
+            return std::to_string(first/second);
+        }else
+        {
+            return "Division by 0";
+        }
+        
+    }   
+    return "Invalid data";
 }
