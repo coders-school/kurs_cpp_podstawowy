da052175b4a134448aa897bea3adc6590aa451f5
mike <mike@MacBook-Air-mike.local>
2020-05-25 21:15:56 +0200

    [task] Add fibonacci_iterative method

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -2,7 +2,24 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+	int secondValue = 0;
+	int firstValue = 1;
+	int result = 1;
+	if(sequence == 0)
+	{
+		return sequence;
+	}
+	if(sequence == 1)
+	{
+		return sequence;
+	}
+	for(int i = 2; i < sequence; i++)
+	{
+		secondValue = firstValue;
+		firstValue = result;
+		result = firstValue + secondValue; 
+	}
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
