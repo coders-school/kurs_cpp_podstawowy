eae8788eac8a737ed937e2665ff565c5493101bd
Maciej3206 <heilmanmaciej@gmail.com>
2020-05-26 22:34:01 +0200

    working version of homework

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int sub=0;
+    do {
+        (lhs<rhs) ? sub=rhs-lhs : sub=lhs-rhs;
+        (lhs>=sub && lhs>=rhs) ? lhs=sub : rhs=sub;    
+    }
+    while (lhs-rhs!=0);
+    return lhs;
 }
 
-int NWW(int lhs, int rhs) {
+//int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
-    return -1;
-}
+ //   return -1;
+//}
