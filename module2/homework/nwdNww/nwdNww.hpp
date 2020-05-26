3a0fca8a86af39d900d7f9b22d8ee619d4b7c2a7
RyszardH <rysiekh@gmail.com>
2020-05-26 12:24:36 +0200

    NWD & NWW

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,27 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+void ABS_Num( int & lhs, int& rhs)
+{
+    lhs = abs(lhs);
+    rhs = abs(rhs);
 }
-
+int NWD(int lhs, int rhs) { 
+    ABS_Num(lhs,rhs);
+    if (lhs == 0) return rhs;
+    if (rhs == 0) return lhs;
+    
+    while(lhs != rhs)
+    {
+        if(lhs < rhs) rhs-=lhs;
+        else lhs-=rhs;
+    }
+    return lhs;
+}
+ 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
-}
+    ABS_Num(lhs,rhs);
+    if(lhs==rhs and lhs == 0) return 0;
+    return (lhs / NWD(lhs, rhs)) * rhs;
+}   
+
+
