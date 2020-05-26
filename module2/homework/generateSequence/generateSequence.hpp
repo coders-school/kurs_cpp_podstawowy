568cc9d54acbc1eecfbe11606c78b7dde4017e9f
BreWal <waldemar.brejecki@gmail.com>
2020-05-26 20:39:20 +0200

    generateSequence works

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -2,6 +2,20 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+
+    std::vector<int> vec;
+
+    if (count < 0)
+    {
+        return vec;
+    }
+
+    vec.reserve(count);
+
+    for (size_t i = 0; i < count; i++)
+    {
+        vec.push_back(step + step * i);
+    }
+
+    return vec;
 }
