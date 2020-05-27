75362f75219c4c8632cfe82f003d1de57577cc43
Kali Live user <wiktorbigaj@wp.pl>
2020-05-27 19:15:37 +0000

    Fixing variables mismatch

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -4,7 +4,7 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     int max = std::numeric_limits<int>::min();
-    for (auto element : numbers) {
+    for (auto element : vec) {
         if(element > max){
             max = element;
         }
