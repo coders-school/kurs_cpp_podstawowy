dd2c49fe833cf4562214560d776547d312c828d0
kawapa <kawapa@outlook.com>
2020-05-27 12:45:57 +0200

    Exercise maxOfVector

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -2,7 +2,20 @@
 #include <vector>
 #include <limits>
 
-int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+int maxOfVector(const std::vector<int>& vec)
+{
+
+    if (vec.size() == 0)
+        return 0;
+
+    int max = vec[0];
+    for (const auto & i : vec)
+    {
+        if (i > max)
+        {
+            max = i;
+        }
+    }
+    
+    return max;
 }
