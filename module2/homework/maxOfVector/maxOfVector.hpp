09b464987196ed05c77185a02b92b46f33032f1e
Kali Live user <wiktorbigaj@wp.pl>
2020-05-27 19:11:58 +0000

    Zadanie 3 -MaxOfVector

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -3,6 +3,11 @@
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int max = std::numeric_limits<int>::min();
+    for (auto element : numbers) {
+        if(element > max){
+            max = element;
+        }
+    }
+    return max;
 }
