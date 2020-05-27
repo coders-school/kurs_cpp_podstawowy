3d7b13f702b0b68aa85bff767b317956c4dd03ae
Kali Live user <wiktorbigaj@wp.pl>
2020-05-27 18:47:02 +0000

    Zadanie 1 - AddEven

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -1,8 +1,15 @@
 #pragma once
+#include <iostream>
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int sum = 0;
+    for (auto element : numbers) {
+        if(element % 2 == 0){
+            sum += element;
+        }
+    }
+    return sum;
 }
+
+
