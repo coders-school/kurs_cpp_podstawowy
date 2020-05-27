6d2a32ca05f978cfe415a825340a6b10d40e4275
Kali Live user <wiktorbigaj@wp.pl>
2020-05-27 18:56:52 +0000

    Zadanie 2 - NWW i NWD

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,23 @@
 #pragma once
+#include <algorithm>
+#include <stdlib.h>
+#include <vector>
+
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int first = std::max(abs(lhs), abs(rhs));
+    int second = std::min(abs(lhs), abs(rhs));
+    while(second > 0){
+        int tmp = first % second;
+        first = second;
+        second = tmp;
+    }
+    return first;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs == 0 && rhs == 0){
+        return 0;
+    }
+    return abs (lhs * rhs) / NWD(lhs, rhs); 
 }
