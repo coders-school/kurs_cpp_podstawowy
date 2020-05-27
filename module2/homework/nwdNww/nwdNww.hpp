0f3e6fbaf4c6a03ee4412d0c549c4e5a3ffa54b2
kawapa <kawapa@outlook.com>
2020-05-27 12:46:14 +0200

    Exercise NWD & NWD

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,35 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    if (lhs == 0 || rhs == 0)
+        return std::max(lhs, rhs);
+
+    lhs = std::abs(lhs);
+    rhs = std::abs(rhs);
+
+    while (lhs != rhs)
+    {
+        if (lhs > rhs)
+        {
+            lhs = lhs - rhs;
+        }
+        else
+        {
+            rhs = rhs - lhs;
+        }
+    }
+
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    if (lhs == 0 || rhs == 0)
+        return 0;
+
+    lhs = std::abs(lhs);
+    rhs = std::abs(rhs);
+
+    return (lhs / NWD(lhs, rhs)) * rhs;
 }
