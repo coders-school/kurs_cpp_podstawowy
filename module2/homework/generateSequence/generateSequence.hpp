b4ff8d5c2e16a2f05d4a3c68052fd1e3b2845ccd
damixiorek <damixiorek@gmail.com>
2020-05-28 17:39:00 +0200

    generateSequence.hpp works!

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -2,6 +2,23 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
+
+    if (count < 0) {
+
     return {};
+
+    }
+
+    std::vector<int> vec (count);
+
+    int currentNum = step;
+
+    for(auto & element : vec) {
+
+    element = currentNum;
+    currentNum += step;
+
+    }
+
+    return vec;
 }
