3890ddc3e02941f55764999ca33b07d43d7a33cb
damixiorek <damixiorek@gmail.com>
2020-05-28 17:42:55 +0200

    maxOfVector.hpp works!

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -3,6 +3,17 @@
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+
+	int maxNum = vec[0];
+
+	for (auto it = vec.begin(); it != vec.end(); it++) {
+
+		if (*it > maxNum) {
+
+			maxNum = *it;
+		}
+
+	}
+
+	return maxNum;
 }
