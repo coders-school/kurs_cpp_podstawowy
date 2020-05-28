81181efe8685f597379a759e8d4295a5e4c64371
DawidMalarz <kontakt@dawidmalarz.pl>
2020-05-28 21:36:48 +0200

    Removed unnecessary lines

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -1,10 +1,8 @@
 #pragma once
 #include <vector>
 #include <limits>
-#include <algorithm>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
     int max_number = vec[0];
     for (const auto& number : vec) {
         if(number >= max_number) {
