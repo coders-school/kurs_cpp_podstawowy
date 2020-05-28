764ea3d19c902a27b995b89c2e595bec102ab09e
damixiorek <damixiorek@gmail.com>
2020-05-28 17:49:01 +0200

    nwdNww.hpp - nwd works!

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,68 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    if (lhs < 0) { // switch to positive num
+    lhs *= -1;
+
+    }
+
+    if (rhs < 0) { // switch to positive num
+    rhs *= -1;
+
+    }
+
+    if (lhs == rhs) {
+    return lhs;
+
+    }
+
+    if (lhs == 0) {
+    return rhs;
+
+    }
+
+    if (rhs == 0) {
+    return lhs;
+
+    }
+
+
+    int biggerArg = 0;
+    int lesserArg = 0;
+    int howManyTimes = 0;
+    int lastRest = 0;
+    int rest = 0;
+
+    lhs > rhs ? biggerArg = lhs : biggerArg = rhs; // set bigger and lesser argument
+    lhs > rhs ? lesserArg = rhs : lesserArg = lhs;
+
+
+
+    if (!(biggerArg % lesserArg)) { // if greatest common divisor is lesser argument
+    return lesserArg;
+
+    }
+
+    do {
+    lastRest = rest;
+
+    howManyTimes = biggerArg / lesserArg;
+
+    rest = biggerArg - (lesserArg * howManyTimes);
+
+    biggerArg = lesserArg;
+    lesserArg = rest;
+
+    } while (rest != 0);
+
+
+    return lastRest;
+
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    return 0;
+
 }
