170c5ba34a6e283403f22219d493df8fc1651701
damixiorek <damixiorek@gmail.com>
2020-05-28 22:50:11 +0200

    nwdNww.hpp initial code formatting

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,31 +1,26 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    
+int NWD(int lhs, int rhs)  {
+
     int result = 0;
 
     lhs = std::abs(lhs);
     rhs = std::abs(rhs);
 
-	if(rhs > lhs)
-	{
+	if(rhs > lhs) {
         std::swap(lhs,rhs);
 	}
-    
-	while(rhs)
-	{
-		result = rhs;
+
+	while(rhs) {
+        result = rhs;
 		rhs = lhs % rhs;
 		lhs = result;
-		
-	}
-
+    }
 	return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :) 
+
     if(lhs == 0 || rhs == 0) {
         return 0;
     }
@@ -33,9 +28,10 @@ int NWW(int lhs, int rhs) {
     lhs = std::abs(lhs);
     rhs = std::abs(rhs);
 
-   for(int i = 1; i < (lhs * rhs); i++){
-        if((i % lhs == 0)&&(i % rhs == 0)) return i;
+    for(int i = 1; i < (lhs * rhs); i++) {
+        if((i % lhs == 0)&&(i % rhs == 0)) {
+            return i;
+        }
     }
-
     return lhs * rhs;
 }
