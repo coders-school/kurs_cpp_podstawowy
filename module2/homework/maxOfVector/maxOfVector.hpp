e6c8ca7a1b185bb55a9958f517078ad817f015d0
cnuebred <jonatann01ew@gmail.com>
2020-05-28 21:20:31 +0200

    fix maxOfVector function

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -4,9 +4,8 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     if(vec.size() == 0) return 0;
-    if(vec.size() == 1) return vec[0];
-    int tmp = vec[1];
-    for(auto number : vec){
+    int tmp = vec[0];
+    for(const auto &number : vec){
         if(number > tmp)
             tmp = number;
     }
