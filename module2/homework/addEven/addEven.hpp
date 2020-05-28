690afd526c170fb0ddce5465aade04ca9913c309
damixiorek <damixiorek@gmail.com>
2020-05-28 22:59:56 +0200

    addEven.hpp initial code formatting

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -1,11 +1,10 @@
 #include <vector>
 
-int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    int result = 0; 
-    for(const int& element : numbers)
-    {
+int addEven(const std::vector<int> & numbers) {
+
+    int result = 0;
+
+    for(const int& element : numbers) {
         result += element % 2 == 0 ? element : 0;
     }
     return numbers.size() <= 0 ? 0 : result;
