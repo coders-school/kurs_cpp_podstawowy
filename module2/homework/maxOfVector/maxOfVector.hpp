c979796e45a31bfca1473088ab1a53d5bbbd666f
DawidMalarz <kontakt@dawidmalarz.pl>
2020-05-28 15:20:20 +0200

    maxOfVector works

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -1,8 +1,15 @@
 #pragma once
 #include <vector>
 #include <limits>
+#include <algorithm>
 
 int maxOfVector(const std::vector<int>& vec) {
     // TODO: Implement me :)
-    return {};
+    int max_number = vec[0];
+    for (const auto& number : vec) {
+        if(number >= max_number) {
+            max_number = number;
+        }
+    }
+    return max_number;
 }
