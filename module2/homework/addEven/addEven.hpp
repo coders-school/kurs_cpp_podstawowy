4636507434024fc1261198201319dcf07d95b5e9
Pawellos <pablox377@gmail.com>
2020-05-28 21:18:15 +0200

    addEven - task1

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -3,7 +3,6 @@
 int addEven(const std::vector<int>& numbers) {
     // TODO: Your implementation goes here
     // Below return is only to make this function compile now
-    //return std::accumulate(numbers.begin(), numbers.end(), 0, [](int i){i % 2 == 0;});
     int result = 0; 
     for(const int& elem : numbers)
     {
