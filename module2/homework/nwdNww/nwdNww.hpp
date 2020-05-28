39627e82735958e1b80b4116c39d34bc425161d1
danielzwierz <danielzwierz@interia.pl>
2020-05-28 22:32:06 +0200

    nwdNww

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,13 @@
 #pragma once
+#include <algorithm>
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs){
+    	while(rhs)
+		std::swap(lhs %= rhs, rhs);
+	return std::abs(lhs);
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (NWD(lhs, rhs)==0) {return 0;}
+    return std::abs((lhs*rhs)/NWD(lhs, rhs));
 }
