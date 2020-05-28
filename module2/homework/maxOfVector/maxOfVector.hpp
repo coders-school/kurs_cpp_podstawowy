3576d7b78e594fe5a4c5320f163e7ab5bc970ece
DawidMalarz <kontakt@dawidmalarz.pl>
2020-05-28 22:04:49 +0200

    Removed space between if and bracket

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -5,7 +5,7 @@
 int maxOfVector(const std::vector<int>& vec) {
     int max_number = vec[0];
     for (const auto& number : vec) {
-        if(number >= max_number) {
+        if (number >= max_number) {
             max_number = number;
         }
     }
