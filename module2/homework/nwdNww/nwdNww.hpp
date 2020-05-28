ae1bfdcb149aba557a2e69320130dd75e05de890
kyrtaq <kyrtaq13@gmail.com>
2020-05-28 16:59:39 +0200

    made NWD and NWW work!

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,23 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs == 0) return rhs;
+    if(rhs == 0) return lhs;
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+    while(lhs!=rhs){
+        if(lhs>rhs){
+            lhs-=rhs;
+        }else{
+            rhs-=lhs;
+        }
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+    if(lhs == rhs && rhs == 0) return 0;
+    return lhs*rhs/NWD(lhs,rhs);
 }
