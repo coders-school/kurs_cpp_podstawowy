e1b45614be72e623b78fe43c459b062ec8c61b9e
m-kop <m.kopertowski@epicspace.eu>
2020-05-29 23:25:43 +0200

    add prototypes for validation module

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,21 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(const ErrorCode error) {
+    // implement me
+    return "";
+}
+
+bool doesPasswordsMatch(const std::string& password, const std::string& passwordRetyped) {
+    // implement me
+    return false;
+}
+
+ErrorCode checkPasswordRules(const std::string& password) {
+    // implement me
+    return ErrorCode::Ok;
+}
+
+ErrorCode checkPassword(const std::string& password, const std::string& passwordRetyped) {
+    // implement me
+    return ErrorCode::Ok;
+}
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,17 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+#pragma once
+#include <string>
+
+enum class ErrorCode {
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoesNotMatch,
+};
+
+
+std::string getErrorMessage(const ErrorCode error);
+bool doesPasswordsMatch(const std::string& password, const std::string& passwordRetyped);
+ErrorCode checkPasswordRules(const std::string& password);
+ErrorCode checkPassword(const std::string& password, const std::string& passwordRetyped);
