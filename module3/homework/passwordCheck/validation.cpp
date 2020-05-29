3c72f47f7fd6cd489dad9975d38f7f47eecd073b
McKucia <macikucia@onet.pl>
2020-05-29 21:32:22 +0200

    Validation cpp + hpp implementation
    Finito

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,93 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+#include "validation.h"
+#include <iostream>
+
+void getErrorMessage(ErrorCode type){
+    auto code = static_cast<int>(type);
+    switch(code){
+        case 123:
+            std::cout << "Your password is ok. \n";
+            break;
+        case 9:
+            std::cout << "Your password need at least nine characters. \n";
+            break;
+        case 1:
+            std::cout << "Your password need at least one number. \n";
+            break;
+        case 2:
+            std::cout << "Your password need at least one special character. \n";
+            break;
+        case 3:
+            std::cout << "Your password need at least one uppercase letter. \n";
+            break;
+        default:
+            std::cout << "idk what's going on dude \n";
+            break;
+    }
+}
+
+bool doesPasswordsMatch(std::string first, std::string second)
+{
+    return (first == second);
+}
+
+ErrorCode checkPasswordRules(std::string password)
+{
+    //checking if size in at least 9
+    if(password.size() < 9)
+        return ErrorCode::PasswordNeedsAtLeastNineCharacters;
+
+    //checking if string has at least one number, one special character and uppercase letter
+    //default to false
+    bool hasNumber = false;
+    bool hasSpecialCharacter = false;
+    bool hasUppercaseLetter = false;
+
+    //everything in one loop, so the efficiency if better
+    for(size_t i= 0; i < password.size(); i++)
+    {
+        //checking if it has at least one number
+        if( (static_cast<int>(password[i]) > 47 && static_cast<int>(password[i]) < 58 ) ) {
+            hasNumber = true; //bool to true if char on i-index is a number
+        }
+        //if not it is still set to false
+
+        //checking if it has special character
+        if( (static_cast<int>(password[i]) > 32 && static_cast<int>(password[i]) < 48 ) ||
+            (static_cast<int>(password[i]) > 57 && static_cast<int>(password[i]) < 65 ) ) {
+            hasSpecialCharacter = true; //bool to true if char on i-index is a special character
+        }
+
+        if( (static_cast<int>(password[i]) > 64 && static_cast<int>(password[i]) < 91 ) ) {
+            hasUppercaseLetter = true; //bool to true if char on i-index is a number
+        }
+    }
+    if(!hasNumber)
+        return ErrorCode::PasswordNeedsAtLeastOneNumber;
+    if(!hasSpecialCharacter)
+        return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+    if(!hasUppercaseLetter)
+        return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
+
+    return ErrorCode::Ok;
+}
+
+ErrorCode checkPassword(std::string first, std::string second)
+{
+    if(!doesPasswordsMatch(first, second))
+    {
+        return ErrorCode::PasswordsDoesNotMatch;
+    }
+    return checkPasswordRules(first);
+}
+
+int main()
+{
+    std::string password = "abcdefghjk1%&AA";
+    auto error = checkPasswordRules(password);
+    std::cout << static_cast<int>(error) << std::endl;
+    getErrorMessage(error);
+
+    return 0;
+};
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,23 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+#ifndef PASSWORDCHECKO_VALIDATION_H
+#define PASSWORDCHECKO_VALIDATION_H
+
+#include <iostream>
+
+enum class ErrorCode
+{
+    Ok = 123,
+    PasswordNeedsAtLeastNineCharacters = 9,
+    PasswordNeedsAtLeastOneNumber = 1,
+    PasswordNeedsAtLeastOneSpecialCharacter = 2,
+    PasswordNeedsAtLeastOneUppercaseLetter = 3,
+    PasswordsDoesNotMatch = 0,
+};
+
+//Functions
+void getErrorMessage(ErrorCode type);
+bool doesPasswordsMatch(std::string first, std::string second);
+ErrorCode checkPasswordRules(std::string password);
+ErrorCode checkPassword(std::string first, std::string second);
+
+#endif //PASSWORDCHECKO_VALIDATION_H
+
