c4894f81eee098411f01f78e966ea82ed2a5f65c
majkel <michal.sobotka.84@gmail.com>
2020-05-30 00:35:36 +0200

    rand methode corrected

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -7,6 +7,7 @@
 
 std::vector<std::shared_ptr<int>> generate(int count) {
     std::vector<std::shared_ptr<int>> vec {};
+    srand(time(NULL));
     for (auto it = 0; it < count; it++) {
         vec.emplace_back(std::make_shared<int>(generateRandomNumber(count)));
     }
@@ -14,7 +15,12 @@ std::vector<std::shared_ptr<int>> generate(int count) {
 }
 
 int generateRandomNumber(int count) {
-    srand(time(NULL));
     return rand() % count;
+}
+
+void print(std::vector<std::shared_ptr<int>> vec) {
+    for (const auto& it: vec) {
+        std::cout << *it;
+    }
 }
 
--- module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
@@ -4,7 +4,7 @@
 
 std::vector<std::shared_ptr<int>> generate(int count);
 int generateRandomNumber(int count);
-//void print(std::vector<std::shared_ptr<int>> vec);
+void print(std::vector<std::shared_ptr<int>> vec);
 //void add10(std::vector<std::shared_ptr<int>> vec);
 //oid sub10(int* const ptr);
 //void sub10(std::vector<std::shared_ptr<int>> vec);
