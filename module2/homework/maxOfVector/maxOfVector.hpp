f309fd862dc78bc587c84145af0832c110e11616
Yoka40 <lucz.artur@gmail.com>
2020-05-29 21:17:07 +0200

    maxOV_VSC_works

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -2,7 +2,15 @@
 #include <vector>
 #include <limits>
 
-int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+int maxOfVector(const std::vector<int> &vec)
+{
+    auto max = vec[0];
+    for (const auto &num : vec)
+    {
+        if (num > max)
+        {
+            max = num;
+        }
+    }
+    return max;
 }
