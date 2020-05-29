a135f2ec1d5e29a783e5288394dbfefae6d05eb9
majkel <michal.sobotka.84@gmail.com>
2020-05-30 00:18:29 +0200

    generate random, and generate methodes

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -1,2 +1,20 @@
 //TODO 
 //function inplementation
+
+#include<time.h>
+#include <iostream>
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+    std::vector<std::shared_ptr<int>> vec {};
+    for (auto it = 0; it < count; it++) {
+        vec.emplace_back(std::make_shared<int>(generateRandomNumber(count)));
+    }
+    return vec;
+}
+
+int generateRandomNumber(int count) {
+    srand(time(NULL));
+    return rand() % count;
+}
+
--- module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
@@ -3,7 +3,8 @@
 #include <memory>
 
 std::vector<std::shared_ptr<int>> generate(int count);
-void print(std::vector<std::shared_ptr<int>> vec);
-void add10(std::vector<std::shared_ptr<int>> vec);
-void sub10(int* const ptr);
-void sub10(std::vector<std::shared_ptr<int>> vec);
+int generateRandomNumber(int count);
+//void print(std::vector<std::shared_ptr<int>> vec);
+//void add10(std::vector<std::shared_ptr<int>> vec);
+//oid sub10(int* const ptr);
+//void sub10(std::vector<std::shared_ptr<int>> vec);
