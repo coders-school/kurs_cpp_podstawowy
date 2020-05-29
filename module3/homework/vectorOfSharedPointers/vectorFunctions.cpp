a0dc253b4cb1b8ce8898ba863cff416e88c38767
marekjanek <marekjanus3@wp.pl>
2020-05-30 01:31:24 +0200

    functions print(), sub10(), sub10() and add10() added and filled

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -1,5 +1,6 @@
 #include "vectorFunctions.hpp"
 #include <vector>
+#include <iostream>
 
 std::vector<std::shared_ptr<int>> generate(int count){
 
@@ -10,5 +11,44 @@ std::vector<std::shared_ptr<int>> generate(int count){
     }
 
     return SPvec;
+
+}
+
+
+void print(std::vector<std::shared_ptr<int>> pSPvec){
+    // for ranged loop
+    for(auto element : pSPvec){
+        std::cout << element;
+    }
+}
+
+
+void add10(std::vector<std::shared_ptr<int>> addSPvec){
+    
+    
+
+    for(auto element : addSPvec){
+        if(element.get()==nullptr)
+            break;
+        *element+=10;
+    }
+
+}
+void sub10(int * const ptr){
+
+    if(ptr==nullptr)
+        return;
+    *ptr-=10;
+
+
+}
+void sub10(std::vector<std::shared_ptr<int>> subSPvec){
+    
+
+    for(auto element : subSPvec){
+        if(element.get()==nullptr)
+            break;
+        *element-=10;
+    }
 
 }
