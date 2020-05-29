16e9b81dc6a1ffb517b0fc5e3e343745a35f34c9
Tomonike <pgurtowski1@gmail.com>
2020-05-29 14:41:53 +0200

    AddEven begin

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -2,7 +2,14 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
+
+    for (auto i = numbers.begin(); i != numbers.end(); i++){
+        
+
+
+
+    }
+    
+
     return -1;
 }
