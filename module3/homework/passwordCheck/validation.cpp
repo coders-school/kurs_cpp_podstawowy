188f53d91890f77415f1ae22b02942355bc6be1d
jakjan95 <jakubjanusz95@gmail.com>
2020-05-29 15:03:17 +0200

    ErrorCode added and getErrorMessage() implemented

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,25 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+#include <iostream>
+
+
+std::string getErrorMessage(ErrorCode err){
+    switch(err){
+        case ErrorCode::Ok:
+            return "OK";
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+            return "Your password needs at least nine characeters";
+            break;
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+            return "Your password needs at least one special character";
+            break;
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+            return "Your password needs at least one uppercase letter";
+            break;
+        case ErrorCode::PasswordsDoesNotMatch:
+            return "Password does not match";
+            break;
+        default:
+            return "Unknown error";
+            break;
+    }
+}
