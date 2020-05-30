e5dca2ea66b556601616ab89127910e049d02fe7
mariomoore <mariusz.murawski84@gmail.com>
2020-05-30 17:52:33 +0200

    Implemented checkPasswordRules() and checkPassword()

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,4 +1,5 @@
 #include "validation.hpp"
+#include <algorithm>
 
 std::string getErrorMessage(ErrorCode errorCode)
 {
@@ -26,10 +27,53 @@ bool doesPasswordsMatch(std::string password, std::string repeatedPassword)
     return password == repeatedPassword;
 }
 
-ErrorCode checkPasswordRules(std::string password) {
+ErrorCode checkPasswordRules(std::string password)
+{
+    if (password.size() <= 9)
+    {
+        return ErrorCode::PasswordNeedsAtLeastNineCharacters;
+    }
+
+    bool foundNumber = false;
+    for (const auto& ch : password)
+    {
+        if (ch >= '0' && ch <= '9')
+        {
+            foundNumber = true;
+        }
+    }
+    if (!foundNumber)
+    {
+        return ErrorCode::PasswordNeedsAtLeastOneNumber;
+    }
+
+    // https://www.ipvoid.com/password-special-characters/
+    // https://owasp.org/www-community/password-special-characters
+    std::string specialCharacters = " !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~";
+    bool passContainsSpecialChar = std::any_of(specialCharacters.begin(),
+                                                specialCharacters.end(),
+                                                [password](char ch){ return std::any_of(password.begin(),
+                                                                                        password.end(),
+                                                                                        [ch](char c){ return ch == c; }); });
+    if (!passContainsSpecialChar)
+    {
+        return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+    }
+
+    bool passContainsUpperChar = std::any_of(password.begin(), password.end(), ::isupper);
+    if (!passContainsUpperChar)
+    {
+        return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
+    }
+
     return ErrorCode::Ok;
 }
 
-ErrorCode checkPassword(std::string password, std::string repeatedPassword) {
-    return ErrorCode::Ok;
+ErrorCode checkPassword(std::string password, std::string repeatedPassword)
+{
+    if (doesPasswordsMatch(password, repeatedPassword))
+    {
+        return checkPasswordRules(password);
+    }
+    return ErrorCode::PasswordsDoesNotMatch;
 }
