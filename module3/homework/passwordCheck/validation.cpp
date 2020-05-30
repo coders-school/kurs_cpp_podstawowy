9678ff58e786c600dba723777f64813ab0b0e962
duszek <marcin.dus@gmail.com>
2020-05-30 14:54:14 +0200

    fixed compilation, and task 1

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,45 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+#include <string>
+#include <ostream>
+#include <sstream>
+
+std::ostream& operator<<(std::ostream& os, ErrorCode value)
+{
+    switch (value)
+    {
+        case ErrorCode::Ok:
+            return os << "OK";
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+            return os << "PasswordNeedsAtLeastNineCharacters";
+        case ErrorCode::PasswordNeedsAtLeastOneNumber:
+            return os << "PasswordNeedsAtLeastOneNumber";
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+            return os << "PasswordNeedsAtLeastOneSpecialCharacter";
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+            return os << "PasswordNeedsAtLeastOneUppercaseLetter";
+        case ErrorCode::PasswordsDoesNotMatch:
+            return os << "PasswordsDoesNotMatch";
+    }
+    return os;
+}
+
+bool doesPasswordsMatch(std::string& firstPassword, std::string& secondPassword)
+{
+    return true;
+}
+
+ErrorCode checkPasswordRules(std::string& password)
+{
+    return ErrorCode::Ok;
+}
+
+ErrorCode checkPassword(std::string& firstPassword, std::string& secondPassword)
+{
+    return ErrorCode::Ok;
+}
+std::string getErrorMessage(ErrorCode code)
+{
+    std::ostringstream stream;
+    stream << code;
+    return stream.str();
+}
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,18 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+#pragma once
+#include <string>
+
+enum class ErrorCode
+{
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoesNotMatch
+};
+std::ostream& operator << (std::ostream&, ErrorCode);
+
+bool  doesPasswordsMatch(std::string& firstPassword, std::string& secondPassword);
+ErrorCode checkPasswordRules(std::string& password);
+ErrorCode checkPassword(std::string& firstPassword, std::string& secondPassword);
+std::string getErrorMessage(ErrorCode code);
