033b0fec4d0d2a9b73d521e9566b095da1761dcc
McKucia <macikucia@onet.pl>
2020-05-30 18:13:21 +0200

    Adding whole corrections

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,36 +1,39 @@
-#include "validation.hpp"
 #include <iostream>
+#include <array>
 
-std::string getErrorMessage(ErrorCode type){
-    auto code = static_cast<int>(type);
-    switch(code){
-        case 123:
+#include "validation.h"
+
+std::string getErrorMessage(ErrorCode errorType){
+    switch(errorType){
+        case ErrorCode::Ok:
             return "Your password is ok. \n";
             break;
-        case 9:
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
             return "Your password need at least nine characters. \n";
             break;
-        case 1:
+        case ErrorCode::PasswordNeedsAtLeastOneNumber:
             return "Your password need at least one number. \n";
             break;
-        case 2:
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
             return "Your password need at least one special character. \n";
             break;
-        case 3:
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
             return  "Your password need at least one uppercase letter. \n";
             break;
-        default:
-            return "idk what's going on dude \n";
+        case ErrorCode::PasswordsDoesNotMatch:
+            return "Passwords does not match. \n";
             break;
+        default:
+            return "Unknown error \n";
     }
 }
 
-bool doesPasswordsMatch(std::string first, std::string second)
+bool doesPasswordsMatch(std::string& first, std::string& second)
 {
-    return (first == second);
+    return first == second;
 }
 
-ErrorCode checkPasswordRules(std::string password)
+ErrorCode checkPasswordRules(std::string& password)
 {
     //checking if size in at least 9
     if(password.size() < 9)
@@ -46,18 +49,22 @@ ErrorCode checkPasswordRules(std::string password)
     for(size_t i= 0; i < password.size(); i++)
     {
         //checking if it has at least one number
-        if( (static_cast<int>(password[i]) > 47 && static_cast<int>(password[i]) < 58 ) ) {
+        //add "!hasNumber" statement so it doesn't need to check if it's true
+        if( !hasNumber && password[i] >= '0' && password[i] <= '9' ){
             hasNumber = true; //bool to true if char on i-index is a number
         }
         //if not it is still set to false
 
         //checking if it has special character
-        if( (static_cast<int>(password[i]) > 32 && static_cast<int>(password[i]) < 48 ) ||
-            (static_cast<int>(password[i]) > 57 && static_cast<int>(password[i]) < 65 ) ) {
+        if( !hasSpecialCharacter && ( password[i] > ' ' && password[i] < '0' ) ||
+                (password[i] > '9' && password[i] < 'A' ) ) {
             hasSpecialCharacter = true; //bool to true if char on i-index is a special character
         }
 
-        if( (static_cast<int>(password[i]) > 64 && static_cast<int>(password[i]) < 91 ) ) {
+        //hasSpecialCharacter = (std::any_of(password.begin(), password.end(), [](char c){return c == '!' || c == '@';}));
+        // ^this gonna work but i don't know how to check every special character, i think it's bad to write out all symbols
+
+        if( !hasUppercaseLetter && password[i] > 'A' && password[i] < 'Z' ) {
             hasUppercaseLetter = true; //bool to true if char on i-index is a number
         }
     }
@@ -71,7 +78,7 @@ ErrorCode checkPasswordRules(std::string password)
     return ErrorCode::Ok;
 }
 
-ErrorCode checkPassword(std::string first, std::string second)
+ErrorCode checkPassword(std::string& first, std::string& second)
 {
     if(!doesPasswordsMatch(first, second))
     {
@@ -80,3 +87,15 @@ ErrorCode checkPassword(std::string first, std::string second)
     return checkPasswordRules(first);
 }
 
+int main() {
+    std::string password;
+    std::string repeatedPassword;
+    std::cout << "Set new password: ";
+    std::cin >> password;
+    std::cout << "Repeat password: ";
+    std::cin >> repeatedPassword;
+    auto result = checkPassword(password, repeatedPassword);
+    std::cout << getErrorMessage(result) << '\n';
+
+    return 0;
+}
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,23 +1,18 @@
-#ifndef PASSWORDCHECKO_VALIDATION_H
-#define PASSWORDCHECKO_VALIDATION_H
-
-#include <iostream>
+#include <string>
 
 enum class ErrorCode
 {
-    Ok = 123,
-    PasswordNeedsAtLeastNineCharacters = 9,
-    PasswordNeedsAtLeastOneNumber = 1,
-    PasswordNeedsAtLeastOneSpecialCharacter = 2,
-    PasswordNeedsAtLeastOneUppercaseLetter = 3,
-    PasswordsDoesNotMatch = 0,
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoesNotMatch
 };
 
 //Functions
-std::string getErrorMessage(ErrorCode type);
-bool doesPasswordsMatch(std::string first, std::string second);
-ErrorCode checkPasswordRules(std::string password);
-ErrorCode checkPassword(std::string first, std::string second);
-
-#endif //PASSWORDCHECKO_VALIDATION_H
+std::string getErrorMessage(ErrorCode errorType);
+bool doesPasswordsMatch(std::string& first, std::string& second);
+ErrorCode checkPasswordRules(std::string& password);
+ErrorCode checkPassword(std::string& first, std::string& second);
 
