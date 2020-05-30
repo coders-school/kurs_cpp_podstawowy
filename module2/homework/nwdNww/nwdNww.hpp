326f1d0a83f70eb7287ea97dd9467b9921469296
Maciej3206 <heilmanmaciej@gmail.com>
2020-05-30 07:57:50 +0200

    improved algorithm

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,23 +1,24 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    int sub=0;
-    do {
-        (lhs<rhs) ? sub=rhs-lhs : sub=lhs-rhs;
-        (lhs>=sub && lhs>=rhs) ? lhs=sub : rhs=sub;    
+    unsigned int t;
+    while (rhs)
+    {
+        t = rhs ;
+        rhs = lhs % rhs;
+        lhs = t;
     }
-    while (lhs-rhs!=0);
     return lhs;
 }
-
 int NWW(int lhs, int rhs) {
 if (lhs==0 || rhs==0) {return 0;}
 long long mul=lhs*rhs;
-int sub=0;
-    do {
-        (lhs<rhs) ? sub=rhs-lhs : sub=lhs-rhs;
-        (lhs>=sub && lhs>=rhs) ? lhs=sub : rhs=sub;    
+   unsigned int t;
+    while (rhs)
+    {
+        t = rhs ;
+        rhs = lhs % rhs;
+        lhs = t;
     }
-    while (lhs-rhs!=0);
-    return (mul/lhs);
+    return mul/lhs;
 }
