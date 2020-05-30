00d5755813a750fbcf492661dfc8a4b2d1cda8a1
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-05-30 18:18:30 +0200

    implemented getErrorMessage

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,22 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(ErrorCode error)
+{
+    switch(error)
+    {
+        case 1:
+            return "Your password is correct.\n";
+        case 2: 
+            return "Your password needs at least nine characters.\n";
+        case 3:
+            return "Yout password needs at least one number.\n";
+        case 4:
+            return "Your password needs at least one special character.\n";
+        case 5:
+            return "Your password needs at least one upper case letter.\n";
+        case 6:
+            return "Passwords do not match.\n"; 
+        default:
+            return "Your password is incorrect.\n";
+    }
+}
