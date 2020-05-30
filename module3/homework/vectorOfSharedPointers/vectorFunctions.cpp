2461848e182f5606003a092acd176a654ba7f437
mariomoore <mariusz.murawski84@gmail.com>
2020-05-30 20:33:13 +0200

    Project without implementation. Properly builded

---
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -0,0 +1,27 @@
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count)
+{
+    std::vector<std::shared_ptr<int>> vec;
+    return vec;
+}
+
+void print(std::vector<std::shared_ptr<int>>& vec)
+{
+
+}
+
+void add10(std::vector<std::shared_ptr<int>>& vec)
+{
+
+}
+
+void sub10(int* const ptr)
+{
+
+}
+
+void sub10(std::vector<std::shared_ptr<int>>& vec)
+{
+
+}
