57b64d2e52dc95b2332e1bf57509f4f720a1277d
zenon <65047424+ms00780@users.noreply.github.com>
2020-05-30 20:25:37 +0200

    maxofvector & nwd & nww

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -3,6 +3,13 @@
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int max;
+       max = vec.front();
+       for (int i = 1; i < vec.size(); i++){
+           if (vec[i] > vec[i - 1]){
+               max = vec[i];
+       } else
+           i ++;
+     }
+    return max;
 }
--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,22 @@
 #pragma once
+#include <vector>
+#include <algorithm>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+     if (lhs < 0)
+      lhs = lhs * (-1);
+    if (rhs < 0)
+      rhs = rhs * (-1);
+      
+    int sec;
+     while(rhs!=0){
+       sec = rhs;
+       rhs = lhs%rhs;
+       lhs = sec;
+     }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    return lhs/NWD(lhs, rhs)*rhs;
 }
