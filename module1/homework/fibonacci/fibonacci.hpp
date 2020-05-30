3e480912dc634831d8ff5d6b6a0c8f8f70f0263c
whitehorod <whitehorod@gmail.com>
2020-05-30 22:35:23 +0200

    adding works

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,25 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int fCurrent = 1, f1 = 1, f2 = 1;
+    
+    if (sequence == 0)
+      return 0;
+    
+    for (auto iter = 3 ; iter <= sequence ; iter++) {
+      fCurrent = f1 + f2;
+      f1       = f2;
+      f2       = fCurrent;
+    }
+    
+    return fCurrent;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence<0)
+        return -1;
+    else if (sequence<=1)
+        return sequence;
+        
+    return fibonacci_recursive(sequence-1)+fibonacci_recursive(sequence-2);
 }
--- module1/homework/fibonacci/main.cpp
+++ module1/homework/fibonacci/main.cpp
@@ -2,8 +2,18 @@
 #include "fibonacci.hpp"
 
 int main() {
-    std::cout << fibonacci_iterative(10) << "\n";
-    std::cout << fibonacci_recursive(10) << "\n";
+    int nElements = 0;
+//    std::cout << fibonacci_iterative(10) << "\n";
+//    std::cout << fibonacci_recursive(10) << "\n";
+    std::cout << "How many Fibonacci sequence do You want to know?" << "\n";
+    std::cin >> nElements;
+
+    for (int iter = 0 ; iter <= nElements; iter++) {
+        long long fi = fibonacci_iterative(iter);
+        long long fr = fibonacci_recursive(iter);
+        std::cout << "fibonacci n = " << iter << " iterative(n) = " <<  fi << " recursive(n) = " << fr << ((fi==fr)?" OK ":" BAD ") << "\n";
+
+    }
 
     return 0;
 }
