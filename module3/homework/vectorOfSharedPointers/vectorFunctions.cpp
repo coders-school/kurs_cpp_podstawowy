5f8f2cfa92ac40dbe05c8b3577d2a9ff07ee9c89
mariomoore <mariusz.murawski84@gmail.com>
2020-05-30 20:45:13 +0200

    Implemented generate()

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -3,6 +3,10 @@
 std::vector<std::shared_ptr<int>> generate(int count)
 {
     std::vector<std::shared_ptr<int>> vec;
+    for (size_t i = 0; i < count; ++i)
+    {
+        vec.push_back(std::make_shared<int>(i));
+    }
     return vec;
 }
 
