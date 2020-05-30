7ec2903a5bf85f525ad35030a333ad5af9858c15
Kuba W <winczukjakub@gmail.com>
2020-05-30 18:56:55 +0200

    done check password

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,40 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+#include <random>
+
+std::string getErrorMessage(ErrorCode code) {
+    if(code == ErrorCode::Ok) {
+        return "OK";
+    } else if(code == ErrorCode::PasswordNeedsAtLeastNineCharacters) {
+        return "Password Needs At Least Nine Characters";
+    } else if(code == ErrorCode::PasswordNeedsAtLeastOneNumber) {
+        return "Password Needs At Least One Number";
+    } else if(code == ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter) {
+        return "Password Needs At Least One Special Character";
+    } else if(code == ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter) {
+        return "Password Needs At Least One Uppercase Letter";
+    } else if(code == ErrorCode::PasswordsDoesNotMatch) {
+        return "Passwords Does Not Match";
+    }
+}
+
+bool doesPasswordsMatch(const std::string& first_pass, const std::string& second_pass) {
+    auto match = std::mismatch(first_pass.begin(), first_pass.end(), second_pass.begin());
+
+    return (match.first == first_pass.end() && match.second == second_pass.end());
+}
+
+ErrorCode checkPasswordRules(const std::string& password) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(static_cast<int>(ErrorCode::Ok), static_cast<int>(ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter));
+
+    return static_cast<ErrorCode>(dis(gen));
+}
+
+ErrorCode checkPassword(const std::string& first_pass, const std::string& second_pass) {
+    if(doesPasswordsMatch(first_pass, second_pass)) {
+        return checkPasswordRules(first_pass);
+    } else {
+        return ErrorCode::PasswordsDoesNotMatch;
+    }
+}
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,26 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+#pragma once
+#include <string>
+
+enum class ErrorCode{
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoesNotMatch
+};
+
+std::string getErrorMessage(ErrorCode code);
+
+bool doesPasswordsMatch(const std::string& first_pass, const std::string& second_pass);
+
+ErrorCode checkPasswordRules(const std::string& password);
+
+ErrorCode checkPassword(const std::string& first_pass, const std::string& second_pass);
+
+
+
+
+
+
+
