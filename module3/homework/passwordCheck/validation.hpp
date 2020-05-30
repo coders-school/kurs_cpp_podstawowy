a50fc8d4e6780b14984b2a5ee45aa326cc84307a
BreWal <waldemar.brejecki@gmail.com>
2020-05-30 19:52:36 +0200

    added libraries and function definitions and ErrorCode (enum) to validation.hpp

--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,22 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+#pragma once
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <cctype>
+#include <algorithm>
+
+enum ErrorCode
+{
+	Ok = 0,
+	PasswordNeedsAtLeastNineCharacters,
+	PasswordNeedsAtLeastOneNumber,
+	PasswordNeedsAtLeastOneSpecialCharacter,
+	PasswordNeedsAtLeastOneUppercaseLetter,
+	PasswordsDoesNotMatch
+};
+
+std::string getErrorMessage(ErrorCode err);
+bool doesPasswordsMatch(std::string password1, std::string password2);
+ErrorCode checkPasswordRules(std::string password);
+ErrorCode checkPassword(std::string password1, std::string password2);
