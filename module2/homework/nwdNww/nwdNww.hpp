ee80b70abb6fd36f5846877d79334de5bfae3549
adrianmasiewicz <adrianmasiewicz@gmail.com>
2020-05-30 23:35:17 +0200

    adding works

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if ((lhs > 0) && (rhs > 0))
+        while (true)
+        {
+            while(lhs >= rhs){
+                if(lhs == rhs) return rhs;
+                lhs -= rhs;
+            }
+            int number = lhs;
+            lhs = rhs;
+            rhs = number;
+        }
+    
+    return 0;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+      if ((lhs > 0) && (rhs > 0))
+        return lhs * rhs / NWD(lhs, rhs);
+    
+    return 0;
 }
