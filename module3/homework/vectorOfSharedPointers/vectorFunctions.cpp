5889a3a79c95236e6942ae65925e15472850ec72
Kuba C <jakub.cieplinski@outlook.com>
2020-05-31 11:54:42 +0200

    fixed sub10 for vector according to requirements, reserve vector size beforehand

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -3,9 +3,9 @@
 
 
 std::vector<std::shared_ptr<int>> generate(int count) {
-    std::vector<std::shared_ptr<int>> pointers;
+    std::vector<std::shared_ptr<int>> pointers(10);
     for(size_t i = 0; i < count; i++)
-        pointers.push_back(std::make_shared<int>(i));
+        pointers[i] = std::make_shared<int>(i);
     
     return pointers;
 }
@@ -30,6 +30,6 @@ void sub10(int* const number) {
 void sub10(const std::vector<std::shared_ptr<int>>& numbers) {
     for(auto& num : numbers){
         if(num)
-            *num-=10;
+            sub10(num.get());
     }
 }
