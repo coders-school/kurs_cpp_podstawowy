e625f290ac67277613d31b1ca34d00e632e46e2a
Krzysztof Kucharski <krzysztof85@gmail.com>
2020-05-31 23:43:47 +0200

    working NWD

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int temp;
+    while (rhs)
+    {
+        temp = rhs;
+        rhs = lhs % rhs;
+        lhs = temp; 
+    }
+    return abs(lhs);
 }
 
 int NWW(int lhs, int rhs) {
