f412f962b093467b801c020cc71157b71cfd6619
m-kop <m.kopertowski@epicspace.eu>
2020-05-31 21:12:18 +0200

    add vector of shared pointers module
    +vectorFunctions cpp and hpp files
    +vector functions prototypes

---
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -0,0 +1,22 @@
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+    return {};
+}
+
+void print(std::vector<std::shared_ptr<int>> vec) {
+
+}
+
+void add10(std::vector<std::shared_ptr<int>> vec) {
+
+}
+
+void sub10(int * const num) {
+
+}
+
+void sub10(std::vector<std::shared_ptr<int>> vec) {
+
+}
+
