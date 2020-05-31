4e52ae54cdfcad9f326fdd9e5ad2f39ca21f23f6
Michal Muszynski <zgredek2318@gmail.com>
2020-05-31 12:05:48 +0200

    NWD works

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,8 +1,18 @@
 #pragma once
+#include <cmath>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    int temp;
+    while(rhs != 0)
+    {
+        temp = rhs;
+        rhs = lhs%rhs;
+        lhs = temp;
+    }
+    
+    return abs(lhs);
+    
 }
 
 int NWW(int lhs, int rhs) {
