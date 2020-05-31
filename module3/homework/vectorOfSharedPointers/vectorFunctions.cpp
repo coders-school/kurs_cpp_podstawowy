83f445f952ca3aac5c65b00a7f248221914f5292
Kuba C <jakub.cieplinski@outlook.com>
2020-05-31 11:22:06 +0200

    generate function implemented

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -1,2 +1,23 @@
 #include "vectorFunctions.hpp"
 
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+    std::vector<std::shared_ptr<int>> pointers;
+    for(size_t i = 0; i < count; i++)
+        pointers.push_back(std::make_shared<int>(i));
+    
+    return pointers;
+}
+
+void print(std::vector<std::shared_ptr<int>> numbers) {
+
+}
+void add10(std::vector<std::shared_ptr<int>> numbers) {
+
+}
+void sub10(int* const number) {
+
+}
+void sub10(std::vector<std::shared_ptr<int>> numbers) {
+
+}
--- module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <vector>
+#include <memory>
 
 std::vector<std::shared_ptr<int>> generate(int count);
 void print(std::vector<std::shared_ptr<int>> numbers);
