3800f89ee33ad47adf65af6ac9bdec6e8504451e
mtalma <talmamateusz@gmail.com>
2020-05-31 15:20:35 +0200

    Add function generate()

---
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -0,0 +1,17 @@
+#include <memory>
+#include <vector>
+
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count)
+{
+    std::vector<std::shared_ptr<int>> result;
+    result.reserve(count);
+
+    for (size_t i = 0; i < count; i++)
+    {
+        result.emplace_back(std::make_shared<int>(i));
+    }
+
+    return result;
+}
---
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
@@ -0,0 +1,4 @@
+#include<memory>
+#include<vector>
+
+std::vector<std::shared_ptr<int>> generate(int count);
