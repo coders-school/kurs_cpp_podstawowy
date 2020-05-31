7486db42b1c6684966a0745fd812b0ef211f6164
jakjan95 <jakubjanusz95@gmail.com>
2020-05-31 08:59:37 +0200

    vectorFunction cleaned

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -1,38 +1,39 @@
+#include "vectorFunctions.hpp"
 #include <iostream>
 #include <algorithm>
-#include "vectorFunctions.hpp"
+
 
 std::vector<std::shared_ptr<int>> generate(int count) {
     std::vector<std::shared_ptr<int>> vec (count);
-    for (auto it = 0; it < count; it++) {
+    for (auto it = 0; it < count; it++){
         vec[it] = std::make_shared<int>(it);
     }
     return vec;
 }
 
 void print(std::vector<std::shared_ptr<int>> vec) {
-    for (const auto& it: vec) {
+    for (const auto& it: vec){
         std::cout << *it << " ";
     }
     std::cout << '\n';
 }
 
 void add10(std::vector<std::shared_ptr<int>> vec) {
-    for (const auto& it: vec) {
-        if(it != nullptr){
+    for (const auto& it: vec){
+        if(it){
             *it += 10;
         }
     }
 }
 
 void sub10(int* const ptr) {
-    if(ptr != nullptr){
+    if(ptr){
         *ptr -= 10;
     }
 }
 
 void sub10(std::vector<std::shared_ptr<int> > vec) {
-    for (auto& it: vec) {
+    for (auto& it: vec){
         sub10(it.get());
     }
 }
