d6d9d8df00c4e2a4ff4ad41f24e627156fec5a85
DawidJ <dawid.d.jozefczak@gmail.com>
2020-05-31 22:16:17 +0200

    nwdNww implementation

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,19 @@
 #pragma once
 
-int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWD(int lhs, int rhs){
+    int temp = 0;
+    while(rhs != 0){
+        temp = rhs;
+        rhs = lhs % rhs;
+        lhs = temp;
+    }
+    return abs(lhs);   
 }
 
-int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+int NWW(int lhs, int rhs){
+    if(lhs == 0 && rhs == 0){
+        return 0;
+    }
+    int nwd = NWD(lhs, rhs);
+    return abs(lhs / nwd * rhs);
 }
