c6a1861c411c7e47fd043df1ea9cefb36ccb687d
TalNovatzky <natalia.nowacka.it@gmail.com>
2020-05-31 17:01:21 +0200

    validation files skeleton'

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,21 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(ErrorCode errorCode)
+{
+    return std::string();
+}
+
+bool doesPasswordsMatch(std::string password, std::string confirmedPassword)
+{
+    return false;
+}
+
+ErrorCode checkPassword(std::string password, std::string confirmedPassword)
+{
+    return ErrorCode();
+}
+
+ErrorCode checkPasswordRules(std::string password)
+{
+    return ErrorCode();
+}
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,19 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+#include <string>
+
+enum class ErrorCode 
+{
+	Ok,
+	PasswordNeedsAtLeastNineCharacters,
+	PasswordNeedsAtLeastOneNumber,
+	PasswordNeedsAtLeastOneSpecialCharacter,
+	PasswordNeedsAtLeastOneUppercaseLetter,
+	PasswordsDoesNotMatch
+};
+
+std::string getErrorMessage(ErrorCode errorCode);
+
+bool doesPasswordsMatch(std::string password, std::string confirmedPassword);
+
+ErrorCode checkPasswordRules(std::string password);
+
+ErrorCode checkPassword(std::string password, std::string confirmedPassword);
