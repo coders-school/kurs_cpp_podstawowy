1e202eaa34656be8e9dfc68c0555eac38ec3f6aa
DawidJ <dawid.d.jozefczak@gmail.com>
2020-05-31 22:44:35 +0200

    generateSequence implementation

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -1,7 +1,14 @@
 #pragma once
 #include <vector>
 
-std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+std::vector<int> generateSequence(int count, int step){
+    if(count <= 0){
+        return{};
+    }
+    std::vector<int> vec;
+    vec.reserve(count);
+    for(auto i = 1; i <= count; i++){
+        vec.push_back(step * i);
+    }
+    return vec;
 }
