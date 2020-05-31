fe625dc5665129957595c295f0b94c1c985f51cf
MonikaZ <monika.zelechowska@o2.pl>
2020-05-31 20:38:02 +0200

    Add sub10 and add 10 functions

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -13,3 +13,23 @@ void print(const std::vector<std::shared_ptr<int>>& vec){
         std::cout<<*element<<" ";
     std::cout<<'\n';
 }
+
+void add10(std::vector<std::shared_ptr<int>>& vec){
+    for(auto& element : vec){
+        if(element != nullptr)
+           *element += 10;
+    }
+}
+
+void sub10(int* const ptr){
+    if(ptr != nullptr)
+        *ptr -= 10;
+}
+
+void sub10(const std::vector<std::shared_ptr<int>>& vec){
+    for(const auto& element : vec)
+    {
+        if(element != nullptr)
+            *element -= 10;
+    }
+}
