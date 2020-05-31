1f8f88007b8d3bcf5c53bf4a9e9ad2297412b99a
Tomasz Eichler <tomasz.eichler@wp.pl>
2020-05-31 21:21:30 +0200

    nwd works, started with nww

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,12 +1,25 @@
 #pragma once
+#include <cmath>
 
 int NWD(int lhs, int rhs) {
-    if(!lhs){
+    if(lhs == 0){
         return rhs;
     }
-    return -1;
+    else if(rhs == 0){
+        return lhs;
+    }
+
+    int temp;
+    while(rhs != 0){
+        temp = std::abs(rhs);
+        rhs = std::abs(lhs) % temp;
+        lhs = temp;
+    }
+
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
+    
     return -1;
 }
