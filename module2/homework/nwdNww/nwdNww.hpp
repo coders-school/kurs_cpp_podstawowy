544fee55ca852ea325cb84c7c015b63b7dfaf678
Tomasz Eichler <tomasz.eichler@wp.pl>
2020-05-31 21:11:05 +0200

    started with nwd

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
+    if(!lhs){
+        return rhs;
+    }
     return -1;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
     return -1;
 }
