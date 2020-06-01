9a293325c41cf7cb056a761406dd9873f3827c0c
whitehorod <whitehorod@gmail.com>
2020-06-01 19:11:10 +0200

    adding works

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -4,21 +4,18 @@
 std::string calculate(const std::string& command, int first, int second) {
     std::string result = "Invalid data";
 
-    if( command == "add" ) { 
-      result = std::to_string(first+second);
-    } else
-        if( command == "subtract" ) { 
-      result = std::to_string(first-second);
-    } else
-        if( command == "multiply" ) { 
-      result = std::to_string(first*second);
-    } else
-        if( command == "divide" ) { 
-          if(second!=0)
-            result = std::to_string(first/second);
-          else
-            result = "Division by 0";
+    if (command == "add") { 
+      result = std::to_string(first + second);
+    } else if (command == "subtract") { 
+      result = std::to_string(first - second);
+    } else if (command == "multiply") { 
+      result = std::to_string(first * second);
+    } else if (command == "divide") { 
+        if (second != 0)
+          result = std::to_string(first / second);
+        else
+          result = "Division by 0";
     } 
 
-    return result;
+  return result;
 }
