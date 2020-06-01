16bbf5f7ed3ac76b0d3c539981f7c5a8d0052887
Anna Smoter <an.smoter@gmail.com>
2020-06-01 15:28:05 +0200

    nwdNww added

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs == 0) {
+        return rhs;
+    }
+    if(rhs == 0) {
+        return lhs;
+    }
+    if(lhs < 0) {
+        lhs *= -1;
+    }
+    if(rhs < 0) {
+        rhs *= -1;
+    }
+    while(rhs != lhs) {
+        if(rhs > lhs) {
+            rhs -= lhs;
+        } else {
+            lhs -= rhs;
+        }
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs == 0 || rhs == 0) {
+        return 0;
+    }
+    return std::abs(lhs * rhs / NWD(lhs, rhs));
 }
