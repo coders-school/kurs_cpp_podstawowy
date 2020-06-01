2cf1346cbc740ba46fcf6b1ce35691b676bbfc95
whitehorod <whitehorod@gmail.com>
2020-06-01 19:32:57 +0200

    adding works

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,25 +1,25 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    int fCurrent = 1, f1 = 1, f2 = 1;
+    int fibonacciCurrent = 1, fibonacciN1 = 1, fibonacciN2 = 1;
     
     if (sequence == 0)
       return 0;
     
     for (auto iter = 3 ; iter <= sequence ; iter++) {
-      fCurrent = f1 + f2;
-      f1       = f2;
-      f2       = fCurrent;
+      fibonacciCurrent = fibonacciN1 + fibonacciN2;
+      fibonacciN2      = fibonacciN1;
+      fibonacciN1      = fibonacciCurrent;
     }
     
-    return fCurrent;
+    return fibonacciCurrent;
 }
 
 int fibonacci_recursive(int sequence) {
-    if(sequence<0)
+    if(sequence < 0)
         return -1;
-    else if (sequence<=1)
+    else if ( sequence <= 1)
         return sequence;
         
-    return fibonacci_recursive(sequence-1)+fibonacci_recursive(sequence-2);
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
