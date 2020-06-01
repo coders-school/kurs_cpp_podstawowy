d90ee03808ba126625cd1824826738396fc5f692
kuchichan <pawel.kucharski@hotmail.com>
2020-06-01 20:06:04 +0200

    headers + .gitignore

--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,18 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+#include <string>
+
+#pragma once
+
+enum class ErrorCode {
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacters,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PassswordsDoesNotMatch,
+};
+
+std::string getErrorMessage(const ErrorCode& errorCode);
+bool doesPasswordsMatch(const std::string& firstPwd, const std::string& secondPwd);
+ErrorCode checkPasswordRules(const std::string& password);
+ErrorCode checkPassword(const std::string& firstPwr, const std::string& secondPwd);
+
