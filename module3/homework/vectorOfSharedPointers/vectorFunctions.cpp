5c3c5b88b5d27f2746f08fc9f92dc94e3bc793f8
kyrtaq <kyrtaq13@gmail.com>
2020-06-02 16:21:20 +0200

    added generate

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -12,3 +12,10 @@ void sub10(int* const ptr){
     }
 }
 
+std::vector<std::shared_ptr<int>> generate(int count){
+    std::vector<std::shared_ptr<int>> result;
+    for(int i=0;i<count;++i){
+        result.emplace_back(std::make_shared<int>(i));
+    }
+    return result;
+}
