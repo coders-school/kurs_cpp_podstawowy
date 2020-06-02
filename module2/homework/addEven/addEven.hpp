c46432c7b3f631ab44eff16ce38ce23ea125fed0
Kali Live user <wiktorbigaj@wp.pl>
2020-06-02 19:15:44 +0000

    applied sugestions

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -4,8 +4,8 @@
 
 int addEven(const std::vector<int>& numbers) {
     int sum = 0;
-    for (auto element : numbers) {
-        if(element % 2 == 0){
+    for (const auto& element : numbers) {
+        if (element % 2 == 0){
             sum += element;
         }
     }
