fbad457f4bf6dde5c9b21db6945d97b4a0d4fc36
Andrzej Rafalski <anraf1001@gmail.com>
2020-06-02 14:42:42 +0200

    Deleted unnecessary

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -4,22 +4,16 @@ std::string getErrorMessage(ErrorCode codeToShow) {
     switch (codeToShow) {
     case ErrorCode::Ok:
         return "OK";
-        break;
     case ErrorCode::PasswordNeedsAtLeastNineCharacters:
         return "Password needs at least nine characters";
-        break;
     case ErrorCode::PasswordNeedsAtLeastOneNumber:
         return "Password needs at least one number";
-        break;
     case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
         return "Password needs at least one special character";
-        break;
     case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
         return "Password needs at least one uppercase letter";
-        break;
     case ErrorCode::PasswordsDoesNotMatch:
         return "Passwords don't match";
-        break;
     default:
         return "Unknown error code";
     }
