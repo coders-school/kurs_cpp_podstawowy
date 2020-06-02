186ac1dc1fc1f5060d22acdb472691ed7c2278a3
Kasper Biegun <kasper@alert.pl>
2020-06-02 17:42:18 +0200

    Revert "Exercise tasks completed"
    This reverts commit e23d1c8e4117d784bc5195448e01782429200891.

--- module3/task1.cpp
+++ module3/task1.cpp
@@ -3,9 +3,6 @@
 
 // TODO: Implement foo()
 // It should modify passed string to text "Other string"
-void foo(std::string& str) {
-    str = "Other string";
-}
 
 int main() {
     std::string str("Some string");
