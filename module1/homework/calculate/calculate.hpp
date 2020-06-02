fd37eff5adb39688737467089583e791ff6810ee
danielzwierz <danielzwierz@interia.pl>
2020-06-02 09:48:55 +0200

    some corrections

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -1,20 +1,25 @@
 #pragma once
-#include <string>
 #include <iostream>
+#include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
 
- std::string result = "Invalid data";
+  std::string result = "Invalid data";
 
-  if(command == "add") result = std::to_string(first + second);
-  if(command == "subtract") result = std::to_string(first - second);
-  if(command == "multiply") result = std::to_string(first * second);
-  if(command == "divide") 
-  {
-	  if(second == 0) result = "Division by 0";  
-	  else result = std::to_string(first / second);
+  if(command == "add") {
+    result = std::to_string(first + second);
   }
-
-  return result;
-
+  if(command == "subtract") {
+    result = std::to_string(first - second);
+  }
+  if(command == "multiply") {
+    result = std::to_string(first * second);
+  }
+  if(command == "divide") {
+    result = "Division by 0";
+    if(second != 0) {
+      result = std::to_string(first / second);    
+    }   
+  }
+return result;
 }
