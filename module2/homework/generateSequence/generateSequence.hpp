cfc6deb4a0a9d4a6f7d482fd049609e69d28f4de
Kali Live user <wiktorbigaj@wp.pl>
2020-06-02 19:47:09 +0000

    applied sugestions

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -2,9 +2,14 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    std::vector<int> result;
-    for(int i = 1;i <= count;i++){
-        result.push_back(step * i);
+    if (count <= 0){
+        return std::vector<int>{};
+    }
+    std::vector<int> result(count);
+    int sum = 0;
+    for (int i = 1;i <= count;i++){
+        sum += step;
+        result[i - 1] = sum;
     }
     return result;
 }
