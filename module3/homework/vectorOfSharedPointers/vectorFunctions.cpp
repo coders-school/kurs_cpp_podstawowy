60ae0b12032aa84c938437fba0b1fdb3bcc2c08b
NG90 <glowka.natalia@gmail.com>
2020-06-02 23:07:25 +0200

    vectorOfSharedPointers - functions stubs added

---
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -0,0 +1,18 @@
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+    std::vector<std::shared_ptr<int>> resultVector{};
+    return resultVector;
+}
+
+void print(const std::vector<std::shared_ptr<int>>& sharedPointersVector) {
+}
+
+void add10(const std::vector<std::shared_ptr<int>>& sharedPointersVector) {
+}
+
+void sub10(const std::vector<std::shared_ptr<int>>& sharedPointersVector) {
+}
+
+void sub10(int* const element) {
+}
