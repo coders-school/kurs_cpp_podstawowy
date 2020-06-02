3b291d23786a3a9622c7a612d2c478147e4978ca
Kali Live user <wiktorbigaj@wp.pl>
2020-06-02 19:20:19 +0000

    applied sugestions

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -4,8 +4,8 @@
 
 int maxOfVector(const std::vector<int>& vec) {
     int max = std::numeric_limits<int>::min();
-    for (auto element : vec) {
-        if(element > max){
+    for (const auto& element : vec) {
+        if (element > max){
             max = element;
         }
     }
