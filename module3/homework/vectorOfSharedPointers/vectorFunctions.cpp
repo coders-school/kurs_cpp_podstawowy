b60baff1d01740087269e7618236f6360a56a139
ArturGubala <gubala-artur@wp.pl>
2020-06-02 15:30:34 +0200

    added implementation for function which print each element of vector

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -12,17 +12,19 @@ std::vector<std::shared_ptr<int>> generate(int count) {
 }
 
 void print(std::vector<std::shared_ptr<int>>& vector) {
-
+    for (const auto& el : vector) {
+        std::cout << *el << "\n";
+    }
 }
 
 void add10(std::vector<std::shared_ptr<int>>& vector) {
-
+    
 }
 
 void sub10(int* const ptr) {
-
+    
 }
 
 void sub10(std::vector<std::shared_ptr<int>>& vector) {
-
+    
 }
