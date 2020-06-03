0d72566837c9b7483293997459ea20082ba3a230
Krzysztof Kucharski <krzysztof85@gmail.com>
2020-06-03 10:26:06 +0200

    Changes have been made to the formatting of the code

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -6,7 +6,7 @@ int maxOfVector(const std::vector<int>& vec) {
     int max = vec.front();
     for (const auto& element : vec) {
         if (max < element) {
-        max = element;
+            max = element;
         }
     }
     return max;
