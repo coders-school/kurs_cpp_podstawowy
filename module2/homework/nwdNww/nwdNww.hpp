79533bbba9c5d42ccddc37816072e36dc86d628f
Krzysztof Kucharski <krzysztof85@gmail.com>
2020-06-03 09:58:21 +0200

    Revert "working NWW"
    This reverts commit ef7a90ed2b686851dc720155603034473465c449.

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -12,10 +12,6 @@ int NWD(int lhs, int rhs) {
 }
 
 int NWW(int lhs, int rhs) {
-    if (lhs == 0 || rhs == 0) {
-        return 0;
-    }
-    lhs = abs(lhs);
-    rhs = abs(rhs);
-    return lhs * rhs / NWD(lhs, rhs);
+    // TODO: Implement me :)
+    return -1;
 }
