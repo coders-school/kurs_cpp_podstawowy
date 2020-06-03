34ed0333babc858160f377f7fad210d7147808cb
Mateusz Kwiecień <mateo_kw@outlook.com>
2020-06-03 23:32:47 +0200

    Both recursive and iterative function work

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,27 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int t0 = 0, t1 = 1, nextTerm = t0+t1;
+    if(sequence == 0){
+        return 0;
+    }
+    if(sequence == 1 || sequence == 2){
+        return 1;
+    }
+    for(int i = 1; i<sequence; i++){
+        t0 = t1;
+        t1 = nextTerm;
+        nextTerm = t0 + t1;
+    }
+    return t1;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence == 0){
+        return 0;
+    }
+    if(sequence == 1 || sequence == 2){
+        return 1;
+    }
+    return fibonacci_recursive(sequence-1)+fibonacci_recursive(sequence-2);
 }
