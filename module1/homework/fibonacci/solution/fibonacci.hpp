61c09d038ac0f1f6cf92a29f187ae0025d174489
kyrtaq <53411189+kyrtaq@users.noreply.github.com>
2020-06-03 19:08:00 +0200

    update the way to initialize variable
    it`s a really small thing but it bugs me,
    
    in every other code example you use initialization list but in this file you didn`t do that, it would look better if you sticked to one way of initialization

--- module1/homework/fibonacci/solution/fibonacci.hpp
+++ module1/homework/fibonacci/solution/fibonacci.hpp
@@ -12,8 +12,8 @@ int fibonacci_iterative(int sequence) {
     if (sequence == 0 || sequence == 1)
         return sequence;
 
-    int first = 0;
-    int second = 1;
+    int first{0};
+    int second{1};
     for (size_t i = 1; i < sequence; ++i) {
         int tmp = second;
         second = first + second;
