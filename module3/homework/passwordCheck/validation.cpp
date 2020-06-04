06a3cc89fc4528a9ec94c897f715867a76a5b1cc
damixiorek <damixiorek@gmail.com>
2020-06-04 21:42:05 +0200

    validation works!

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,99 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+#include <algorithm>
+#include <ctime>
+
+
+std::string getErrorMessage(ErrorCode e) {
+
+    std::string errMsg = "";
+
+    switch (e) {
+
+        case ErrorCode::Ok: {
+
+            errMsg = "OK";
+            break;
+        }
+
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters: {
+
+            errMsg = "PasswordNeedsAtLeastNineCharacters";
+            break;
+        }
+
+        case ErrorCode::PasswordNeedsAtLeastOneNumber: {
+
+            errMsg = "PasswordNeedsAtLeastOneNumber";
+            break;
+        }
+
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter: {
+
+            errMsg = "PasswordNeedsAtLeastOneSpecialCharacter";
+            break;
+        }
+
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter: {
+
+            errMsg = "PasswordNeedsAtLeastOneUppercaseLetter";
+            break;
+        }
+
+        case ErrorCode::PasswordsDoesNotMatch: {
+
+            errMsg = "PasswordsDoesNotMatch";
+        }
+
+    }
+
+    return errMsg;
+}
+
+
+
+bool doesPasswordsMatch(const std::string&pass, const std::string& repPass) {
+
+    return pass == repPass;
+}
+
+
+
+ErrorCode checkPasswordRules(const std::string& pass) {
+
+    if (pass.size() < 9) {
+
+        return ErrorCode::PasswordNeedsAtLeastNineCharacters;
+    }
+
+    if (std::any_of(pass.cbegin(), pass.cend(), [](const char& c){if (c > 47 && c < 58) {return true;} else {return false;};})) {
+
+        return ErrorCode::PasswordNeedsAtLeastOneNumber;
+    }
+
+    if (std::any_of(pass.cbegin(), pass.cend(), [](const char& c){if ((c > 32 && c < 48) && (c > 57 && c < 65)) {return true;} else {return false;};})) {
+
+        return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+    }
+
+    if (std::any_of(pass.cbegin(), pass.cend(), [](const char& c){if (c > 64 && c < 91) {return true;} else {return false;};})) {
+
+        return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
+    }
+
+    return ErrorCode::Ok;
+}
+
+
+
+ErrorCode checkPassword(const std::string& pass, const std::string& repPass) {
+
+    if (doesPasswordsMatch(pass, repPass) == false) {
+
+        return ErrorCode::PasswordsDoesNotMatch;
+    }
+
+    return checkPasswordRules(pass);
+}
+
+
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,24 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+#include <string>
+
+enum class ErrorCode {
+
+Ok,
+PasswordNeedsAtLeastNineCharacters,
+PasswordNeedsAtLeastOneNumber,
+PasswordNeedsAtLeastOneSpecialCharacter,
+PasswordNeedsAtLeastOneUppercaseLetter,
+PasswordsDoesNotMatch
+
+};
+
+
+std::string getErrorMessage(ErrorCode e);
+
+bool doesPasswordsMatch(const std::string& pass, const std::string& repPass);
+
+ErrorCode checkPasswordRules(const std::string& pass);
+
+ErrorCode checkPassword(const std::string& pass, const std::string& repPass);
+
+ErrorCode checkPasswordRules(const std::string& pass, const std::string& repPass);
+
