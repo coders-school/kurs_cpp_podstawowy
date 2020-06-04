e7b2737f2aef91644149c806b0962fdc24892c43
Mateusz Kwiecień <mateo_kw@outlook.com>
2020-06-05 00:48:14 +0200

    VectorOfSharedPointers - Fixed

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -4,7 +4,7 @@
 std::vector<std::shared_ptr<int>> generate(int count){
     std::vector<std::shared_ptr<int>> vec{};
     vec.reserve(count);
-    for(size_t i; i < count; i++){
+    for(size_t i = 0; i < count; i++){
         vec.emplace_back(std::make_shared<int>(i));
     }
     return vec;
@@ -32,8 +32,8 @@ void sub10 (int* const ptr){
 
 void sub10 (std::vector<std::shared_ptr<int>> vec){
     for(const auto& ptr : vec){
-        if(ptr){
-            return sub10(ptr.get());
+        if (ptr) {
+            sub10(ptr.get());
         }
     }
 }
