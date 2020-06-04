8ae015ef224493a54bb5702b4d6b138dc351157c
damixiorek <damixiorek@gmail.com>
2020-06-04 21:46:02 +0200

    vectorFunctions work!

---
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+std::vector<std::shared_ptr<int>> generate(int count);
+
+void print(std::vector<std::shared_ptr<int>> vecOfSharedPtr);
+
+void add10(std::vector<std::shared_ptr<int>> vecOfSharedPtr);
+
+void sub10(int * const ptrToIntValue);
+
+void sub10(std::vector<std::shared_ptr<int>> vecOfSharedPtr);
