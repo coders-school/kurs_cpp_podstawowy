55076b07affd0fa2ae0666ad48015316495ac88d
ArturGubala <gubala-artur@wp.pl>
2020-06-04 12:38:04 +0200

    added new tests for checkPasswordRules and getErrorMessage functions, changed any_of for none_of and added new line char in validation.hpp file

--- module3/homework/passwordCheck/test.cpp
+++ module3/homework/passwordCheck/test.cpp
@@ -4,8 +4,15 @@
 std::string EMPTY_PASSWORD = "";
 std::string PROPER_PASSWORD = "abcABC123!@#";
 std::string TOO_SHORT_PASSWORD = "12345678";
+std::string PASSWORD_WITHOUT_DIGITS = "abcDEF!@#GhI";
+std::string PASSWORD_WITHOUT_SPECIAL_CHARACTERS = "abcDEF0123GhI";
+std::string PASSWORD_WITHOUT_UPPER_CHARACTERS = "abc#de01fg%$23";
 auto MIN_ALLOWED_ERROR_CODE = ErrorCode::Ok;
 auto MAX_ALLOWED_ERROR_CODE = ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
+auto ERROR_CODE_TOO_SHORT = ErrorCode::PasswordNeedsAtLeastNineCharacters;
+auto ERROR_CODE_NO_DIGITS = ErrorCode::PasswordNeedsAtLeastOneNumber;
+auto ERROR_CODE_NO_SPECIAL_CHARACTERS = ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+auto ERROR_CODE_NO_UPPER_CHARACTERS = ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
 
 TEST(DoesPasswordsMatchTests, returnsTrueForIdenticalPasswords) {
     EXPECT_TRUE(doesPasswordsMatch(EMPTY_PASSWORD, EMPTY_PASSWORD));
@@ -19,11 +26,12 @@ TEST(DoesPasswordsMatchTests, returnsFalseForDifferentPasswords) {
     EXPECT_FALSE(doesPasswordsMatch(PROPER_PASSWORD, TOO_SHORT_PASSWORD));
 }
 
-TEST(checkPasswordRulesTests, returnsValuesBetweenOkAndPasswordNeedsAtLeastOneUppercaseLetter) {
-    EXPECT_GE(checkPasswordRules(EMPTY_PASSWORD), MIN_ALLOWED_ERROR_CODE);  // greater or equal >=
-    EXPECT_LE(checkPasswordRules(EMPTY_PASSWORD), MAX_ALLOWED_ERROR_CODE);  // less or equal <=
-    EXPECT_GE(checkPasswordRules(TOO_SHORT_PASSWORD), MIN_ALLOWED_ERROR_CODE);
-    EXPECT_LE(checkPasswordRules(TOO_SHORT_PASSWORD), MAX_ALLOWED_ERROR_CODE);
+TEST(checkPasswordRulesTests, returnsPropersValuesForDiferentsPasswords) {
+    EXPECT_EQ(checkPasswordRules(EMPTY_PASSWORD), ERROR_CODE_TOO_SHORT);
+    EXPECT_EQ(checkPasswordRules(TOO_SHORT_PASSWORD), ERROR_CODE_TOO_SHORT);
+    EXPECT_EQ(checkPasswordRules(PASSWORD_WITHOUT_DIGITS), ERROR_CODE_NO_DIGITS);
+    EXPECT_EQ(checkPasswordRules(PASSWORD_WITHOUT_SPECIAL_CHARACTERS), ERROR_CODE_NO_SPECIAL_CHARACTERS);
+    EXPECT_EQ(checkPasswordRules(PASSWORD_WITHOUT_UPPER_CHARACTERS), ERROR_CODE_NO_UPPER_CHARACTERS);
 }
 
 TEST(checkPasswordTests, returnsValuesBetweenOkAndPasswordNeedsAtLeastOneUppercaseLetter) {
@@ -32,12 +40,16 @@ TEST(checkPasswordTests, returnsValuesBetweenOkAndPasswordNeedsAtLeastOneUpperca
 }
 
 TEST(checkPasswordTests, returnsPasswordsDoesNotMatchForDifferentPasswords) {
-    EXPECT_EQ(checkPassword(PROPER_PASSWORD, EMPTY_PASSWORD), ErrorCode::PasswordsDoesNotMatch); // equal ==
+    EXPECT_EQ(checkPassword(PROPER_PASSWORD, EMPTY_PASSWORD), ErrorCode::PasswordsDoesNotMatch);
     EXPECT_EQ(checkPassword(EMPTY_PASSWORD, PROPER_PASSWORD), ErrorCode::PasswordsDoesNotMatch);
     EXPECT_EQ(checkPassword(TOO_SHORT_PASSWORD, PROPER_PASSWORD), ErrorCode::PasswordsDoesNotMatch); 
 }
 
 TEST(getErrorMessageTests, returnsErrorCodeAsString) {
     EXPECT_EQ(getErrorMessage(ErrorCode::Ok), "OK");
-    // Add other tests for getErrorMessage if you wish
+    EXPECT_EQ(getErrorMessage(ErrorCode::PasswordNeedsAtLeastNineCharacters), "Password is too short");
+    EXPECT_EQ(getErrorMessage(ErrorCode::PasswordNeedsAtLeastOneNumber), "Password need to contain at least one digit");
+    EXPECT_EQ(getErrorMessage(ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter), "Password need to contain at least one special character (like: [#],[%],[,],[$],[/]");
+    EXPECT_EQ(getErrorMessage(ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter), "Password need to contain at least one uppercase letter");
+    EXPECT_EQ(getErrorMessage(ErrorCode::PasswordsDoesNotMatch), "Passwords do not match");
 }
--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -22,11 +22,11 @@ std::string getErrorMessage(ErrorCode code) {
 ErrorCode checkPasswordRules(std::string password) {
     if (password.length() < 9) {
         return ErrorCode::PasswordNeedsAtLeastNineCharacters;
-    } else if (!std::any_of(password.begin(), password.end(), ::isdigit)) {
+    } else if (std::none_of(password.begin(), password.end(), ::isdigit)) {
         return ErrorCode::PasswordNeedsAtLeastOneNumber;
     } else if (std::all_of(password.begin(), password.end(), ::isalnum)) {
         return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
-    }else if (!std::any_of(password.begin(), password.end(), ::isupper)) {
+    }else if (std::none_of(password.begin(), password.end(), ::isupper)) {
         return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
     } else {
         return ErrorCode::Ok;
