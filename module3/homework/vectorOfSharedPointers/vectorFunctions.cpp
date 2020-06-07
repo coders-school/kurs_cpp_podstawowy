61157c8ee761471fbb63f5435c118c77f41d757a
Michal Muszynski <zgredek2318@gmail.com>
2020-06-07 23:52:33 +0200

    No reference in print() fixed

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -10,7 +10,7 @@ std::vector<std::shared_ptr<int>> generate(int count){
     }
     return vec;
 }
-void print(std::vector<std::shared_ptr<int>> vec){
+void print(std::vector<std::shared_ptr<int>>& vec){
     for(auto el : vec){
         std::cout << *el << "\n";
     }
