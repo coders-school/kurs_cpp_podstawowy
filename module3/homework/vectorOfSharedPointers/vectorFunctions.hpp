c5aae79c21fb5dfc2bd8f32ff4d2316166ae0f11
DawidMalarz <kontakt@dawidmalarz.pl>
2020-06-07 09:06:33 +0200

    hpp implemented

---
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <memory>
+#include <vector>
+
+std::vector<std::shared_ptr<int>> generate(const int count);
+void print(const std::vector<std::shared_ptr<int>>& vec);
+void add10(std::vector<std::shared_ptr<int>>& vec);
+void sub10(int* const ptr);
+void sub10(const std::vector<std::shared_ptr<int>>& vec)
