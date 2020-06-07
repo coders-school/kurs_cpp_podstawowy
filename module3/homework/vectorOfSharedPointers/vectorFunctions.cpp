7b3f398b28d1ed6be4ee8d98dd8b66c048a33231
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-06-07 23:36:22 +0200

    vector of shared_ptrs

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -2,43 +2,41 @@
 
 #include <iostream>
 
-std::vector<std::shared_ptr<int>> generate(int count){
-    std::vector<std::shared_ptr<int>> vec;
-    for(size_t i = 0; i < count; ++i){
+std::vector<std::shared_ptr<int>> generate(int count) {
+    if (count < 0) {
+        return std::vector<std::shared_ptr<int>>{};
+    }
+    std::vector<std::shared_ptr<int>> vec {};
+    vec.reserve(count);
+    for (int i = 0; i < count; ++i) {
         vec.emplace_back(std::make_shared<int>(i));
     }
     return vec;
 }
-void print(std::vector<std::shared_ptr<int>> vec){
-    for(auto el : vec){
-        std::cout << *el << "\n";
-    }
 
-}
-void add10(std::vector<std::shared_ptr<int>>& vec){
-    for(size_t i = 0; i < vec.size(); ++i){
-       if(vec[i]){
-        *vec[i] += 10;
-       }
-        else{
-            std::cout << "Pointer is NullPtr";
-        }
-        
+void print(std::vector<std::shared_ptr<int>>& vec) {
+    for (const auto & sptr: vec) {
+        std::cout << *sptr << '\n';
     }
- 
 }
-void sub10(std::vector<std::shared_ptr<int>>& vec){
-    for(size_t i = 0; i < vec.size(); ++i){
-        if(vec[i]){
-        auto temp = vec[i];
-        sub10(temp.get());
-        }
-        else{
-            std::cout << "Pointer is a NullPtr";
-        }
-    }
 
+void add10(std::vector<std::shared_ptr<int>>& vec) {
+    for (auto& elem : vec) {
+        if(elem.get()) {
+            *elem += 10;
+        }
+    }
 }
-void sub10(int* const ptr){
+
+void sub10(int * const ptr) {
+    if (!ptr) {
+        return;
+    }
     *ptr -= 10;
+}
+
+void sub10(std::vector<std::shared_ptr<int>>& vec) {
+    for (auto& elem : vec) {
+        sub10(elem.get());
+    }
 }
