c157eaa55d6b6e3bbb5e7f53f3615dfd48b6890f
Michal Muszynski <zgredek2318@gmail.com>
2020-06-07 21:08:17 +0200

    Generate implemented

---
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -0,0 +1,22 @@
+#include "vectorFunctions.hpp"
+
+
+std::vector<std::shared_ptr<int>> generate(int count){
+    std::vector<std::shared_ptr<int>> vec;
+    for(size_t i = 0; i < (count); ++i){
+        vec.emplace_back(std::make_shared<int>(i));
+    }
+    return vec;
+}
+void print(std::vector<std::shared_ptr<int>> vec){
+
+}
+void add10(std::vector<std::shared_ptr<int>> vec){
+
+}
+void sub10(std::vector<std::shared_ptr<int>> vec){
+
+}
+void sub10(int* const ptr){
+
+}
