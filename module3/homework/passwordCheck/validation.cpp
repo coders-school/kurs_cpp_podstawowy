9cdd545889446c0425637853c2a50ab6b1fe84e9
Kali Live user <wiktorbigaj@wp.pl>
2020-06-07 12:00:23 +0000

    Changed std::string to enum

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -29,15 +29,15 @@ bool doesPasswordsMatch(const std::string input, const std::string password) {
     return input == password;
 }
 
-std::string checkPasswordRules(const std::string& password) {
+ErrorCode checkPasswordRules(const std::string& password) {
     srand(time(NULL));
-    int random = rand() % 6;
-    return getErrorMessage(ErrorCode(random));
+    int random = ( rand() % 5 ) + 1;
+    return ErrorCode(random);
 }
 
-std::string checkPassword(std::string input, std::string password) {
+ErrorCode checkPassword(std::string input, std::string password) {
     if (!doesPasswordsMatch(input, password)) {
-        return getErrorMessage(ErrorCode::PasswordsDoesNotMatch);
+        return ErrorCode::PasswordsDoesNotMatch;
     }
     return checkPasswordRules(input);
 }
