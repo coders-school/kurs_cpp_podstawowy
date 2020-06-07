5cbec41e5304b92e614d7ed25dbe5281c4d254df
mtalma <talmamateusz@gmail.com>
2020-06-07 19:48:23 +0200

    Formatted files

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -1,50 +1,42 @@
-#include <memory>
-#include <vector>
-#include <iostream>
 #include "vectorFunctions.hpp"
 
-std::vector<std::shared_ptr<int>> generate(int count)
-{
+#include <iostream>
+#include <memory>
+#include <vector>
+
+std::vector<std::shared_ptr<int>> generate(int count) {
     std::vector<std::shared_ptr<int>> result;
     result.reserve(count);
 
-    for (size_t i = 0; i < count; i++)
-    {
+    for (size_t i = 0; i < count; i++) {
         result.emplace_back(std::make_shared<int>(i));
     }
 
     return result;
 }
 
-void sub10(int* const elem)
-{
-    if (elem)
-    {
+void sub10(int* const elem) {
+    if (elem) {
         *elem -= 10;
     }
 }
 
-void print(const std::vector<std::shared_ptr<int>> vec)
-{
-    for(auto el : vec)
-    {
+void print(const std::vector<std::shared_ptr<int>>& vec) {
+    for (auto el : vec) {
         std::cout << *el << std::endl;
     }
 }
 
-void add10(const std::vector<std::shared_ptr<int>> vec)
-{
-    for(auto elem : vec)
-    {
-        if(elem)
-       *elem += 10;     
+void add10(const std::vector<std::shared_ptr<int>>& vec) {
+    for (auto elem : vec) {
+        if (elem) {
+            *elem += 10;
+        }
     }
 }
 
-void sub10(const std::vector<std::shared_ptr<int>> vec)
-{
-    for( auto el : vec)
-    {
+void sub10(const std::vector<std::shared_ptr<int>>& vec) {
+    for (auto el : vec) {
         sub10(el.get());
     }
 }
--- module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
@@ -1,8 +1,8 @@
-#include<memory>
-#include<vector>
+#include <memory>
+#include <vector>
 
 std::vector<std::shared_ptr<int>> generate(int count);
-void add10(const std::vector<std::shared_ptr<int>> vec);
-void print(const std::vector<std::shared_ptr<int>> vec);
+void add10(const std::vector<std::shared_ptr<int>>& vec);
+void print(const std::vector<std::shared_ptr<int>>& vec);
 void sub10(int* const elem);
-void sub10(const std::vector<std::shared_ptr<int>> vec); 
+void sub10(const std::vector<std::shared_ptr<int>>& vec);
