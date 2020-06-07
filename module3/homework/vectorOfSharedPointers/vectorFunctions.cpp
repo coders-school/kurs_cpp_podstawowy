6494cb382bc1d5fa27c880cd9232a3afe8d59a5a
Mateusz Jaworski <mathew.jaworski@gmail.com>
2020-06-07 13:31:27 +0200

    vector functions implemented

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -5,6 +5,7 @@
 std::vector<std::shared_ptr<int>> generate(int count)
 {
 	std::vector<std::shared_ptr<int>> vectorToReturn;
+	vectorToReturn.reserve(count);
 
 	for (size_t i = 0; i < count; i++) 
 	{
@@ -16,9 +17,40 @@ std::vector<std::shared_ptr<int>> generate(int count)
 
 void print(const std::vector<std::shared_ptr<int>>& vectorToPrint)
 {
-	for (size_t i = 0, count = vectorToPrint.size(); i < count; i++)
+	for(auto const& it : vectorToPrint)
 	{
-		std::cout << *vectorToPrint[i];
+		std::cout << *it;
+	}
+}
+
+void add10(const std::vector<std::shared_ptr<int>> vector)
+{
+	for(auto& it : vector)
+	{
+		if(!it)
+		{
+			continue;
+		}
+
+		*it += 10;
+	}
+}
+
+void sub10(int * const ptr)
+{
+	if(!ptr)
+	{
+		return;
+	}
+
+	*ptr -= 10;
+}
+
+void sub10(const std::vector<std::shared_ptr<int>> vector)
+{
+	for(auto& it : vector)
+	{
+		sub10(it.get());
 	}
 }
 
--- module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
@@ -6,7 +6,8 @@ std::vector<std::shared_ptr<int>> generate(int count);
 
 void print(const std::vector<std::shared_ptr<int>>& vectorToPrint);
 
-//TODO 
-//void add10() która przyjmie wektor i doda do ka¿dej liczby 10
-//void sub10() która przyjmie sta³y wskaŸnik na int i odejmie od tego elementu 10
-//void sub10(); Która przyjmie wektor wspó³dzielonych wskaŸników i wywo³a dla ka¿dego elementu powy¿sze przeci¹¿enie funkcji sub10()
+void add10(const std::vector<std::shared_ptr<int>> vector);
+
+void sub10(int * const number);
+
+void sub10(const std::vector<std::shared_ptr<int>> vector);
