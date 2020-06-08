4cbf35ce6e36a4b69f54be03b2bad0fb9a2244fa
Bartlomiej Gottfried <bargot@o2.pl>
2020-06-09 00:02:53 +0200

    Done max of vector.

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -2,7 +2,16 @@
 #include <vector>
 #include <limits>
 
+/**
+ * Warning: 32bit architecture assumed.
+ */
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int max = INT32_MIN;
+    
+    for (auto elem : vec){
+        if(elem > max){
+            max = elem;
+        }
+    }
+    return max;
 }
