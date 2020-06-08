5873cfa51d48151808eeb0c5d3dd42b0ad9626c8
DawidJ <dawid.d.jozefczak@gmail.com>
2020-06-08 19:47:39 +0200

    Full functionality of PasswordCheck

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,9 +1,10 @@
 #include "validation.hpp"
+#include <algorithm>
 
 std::string getErrorMessage(const ErrorCode error){
     switch (error){
     case Ok:
-        return "Ok";
+        return "OK";
     case PasswordNeedsAtLeastNineCharacters:
         return "Password needs at least nine characters";
     case PasswordNeedsAtLeastOneNumber:
@@ -12,23 +13,35 @@ std::string getErrorMessage(const ErrorCode error){
         return "Password needs at least one special character";
     case PasswordNeedsAtLeastOneUppercaseLetter:
         return "Password needs at least one uppercase letter";
-    case PasswordDoesNotMatch:
+    case PasswordsDoesNotMatch:
         return "Password does not match";        
     }
     return "Unknown error";
 }
 
-bool doesPasswordMatch(const std::string& lhs, const std::string& rhs){
+bool doesPasswordsMatch(const std::string& lhs, const std::string& rhs){
     return lhs == rhs;
 }
 
 ErrorCode checkPasswordRules(const std::string& str){
+    if(str.length() < MinPasswordLength){
+        return PasswordNeedsAtLeastNineCharacters;
+    }
+    else if(std::none_of(str.begin(), str.end(), ::isdigit)){
+        return PasswordNeedsAtLeastOneNumber;
+    }
+    else if(std::all_of(str.begin(), str.end(), ::isalnum)){
+        return PasswordNeedsAtLeastOneSpecialCharacter;
+    }
+    else if(std::none_of(str.begin(),str.end(), ::isupper)){
+        return PasswordNeedsAtLeastOneUppercaseLetter;
+    }
     return Ok;
 }
 
 ErrorCode checkPassword(const std::string& lhs, const std::string& rhs){
-    if(!doesPasswordMatch(lhs, rhs)){
-        return PasswordDoesNotMatch;
+    if(!doesPasswordsMatch(lhs, rhs)){
+        return PasswordsDoesNotMatch;
     }
     return checkPasswordRules(lhs);
 }
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,16 +1,18 @@
 #pragma once
 #include <string>
 
+constexpr size_t MinPasswordLength = 9;
+
 enum ErrorCode {
     Ok,
     PasswordNeedsAtLeastNineCharacters,
     PasswordNeedsAtLeastOneNumber,
     PasswordNeedsAtLeastOneSpecialCharacter,
     PasswordNeedsAtLeastOneUppercaseLetter,
-    PasswordDoesNotMatch
+    PasswordsDoesNotMatch
 };
 
 std::string getErrorMessage(const ErrorCode);
-bool doesPasswordMatch(const std::string&, const std::string&);
+bool doesPasswordsMatch(const std::string&, const std::string&);
 ErrorCode checkPasswordRules(const std::string&);
 ErrorCode checkPassword(const std::string&, const std::string&);
