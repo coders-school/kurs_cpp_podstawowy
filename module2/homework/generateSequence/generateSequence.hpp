615b2d6fc41312eb2fcd96a83ab8eeb2f5c7d505
Bartlomiej Gottfried <bargot@o2.pl>
2020-06-09 00:19:17 +0200

    generate sequence done

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -2,6 +2,13 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> sequence;
+    int sum = step;
+    if(count > 0){
+        for(int i= 0; i < count; i++){   
+            sequence.push_back(sum);
+            sum += step;
+        }
+    }
+    return sequence;
 }
