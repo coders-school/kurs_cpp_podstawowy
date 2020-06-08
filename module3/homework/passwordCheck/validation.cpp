08454deb3d0ca404ea204183e3092c1efde1e15e
DawidJ <dawid.d.jozefczak@gmail.com>
2020-06-08 17:24:17 +0200

    Basic functionality of PasswordCheck

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,34 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(const ErrorCode error){
+    switch (error){
+    case Ok:
+        return "Ok";
+    case PasswordNeedsAtLeastNineCharacters:
+        return "Password needs at least nine characters";
+    case PasswordNeedsAtLeastOneNumber:
+        return "Password needs at least one number";
+    case PasswordNeedsAtLeastOneSpecialCharacter:
+        return "Password needs at least one special character";
+    case PasswordNeedsAtLeastOneUppercaseLetter:
+        return "Password needs at least one uppercase letter";
+    case PasswordDoesNotMatch:
+        return "Password does not match";        
+    }
+    return "Unknown error";
+}
+
+bool doesPasswordMatch(const std::string& lhs, const std::string& rhs){
+    return lhs == rhs;
+}
+
+ErrorCode checkPasswordRules(const std::string& str){
+    return Ok;
+}
+
+ErrorCode checkPassword(const std::string& lhs, const std::string& rhs){
+    if(!doesPasswordMatch(lhs, rhs)){
+        return PasswordDoesNotMatch;
+    }
+    return checkPasswordRules(lhs);
+}
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,16 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+#pragma once
+#include <string>
+
+enum ErrorCode {
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordDoesNotMatch
+};
+
+std::string getErrorMessage(const ErrorCode);
+bool doesPasswordMatch(const std::string&, const std::string&);
+ErrorCode checkPasswordRules(const std::string&);
+ErrorCode checkPassword(const std::string&, const std::string&);
