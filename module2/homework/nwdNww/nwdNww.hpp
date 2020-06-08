d37416130d3ff46d39e377c688e8766064606711
Bartlomiej Gottfried <bargot@o2.pl>
2020-06-08 23:54:35 +0200

    Done nwd, nww

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,46 @@
 #pragma once
 
+
+/**
+ * Zoptymalizowany algorytm Euklidesa
+ * From: http://www.algorytm.edu.pl/algorytmy-maturalne/algorytm-eulkidesa.html
+ * (I did it 20 years ago.. I promise.)
+ */
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int buf;
+    
+	if(lhs < 0){
+        lhs *= -1U;
+    }
+	if(rhs < 0){
+        rhs *= -1U;
+    }
+    while(rhs != 0)
+    {
+		buf = rhs;
+		rhs = lhs % rhs;
+		lhs = buf;	
+	}	
+    return lhs;
 }
 
+/**
+ * From: http://www.algorytm.edu.pl/algorytmy-maturalne/nww.html
+ */ 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+	int buf;
+	
+    if(lhs < 0){
+        lhs *= -1U;
+    }
+	if(rhs < 0){
+        rhs *= -1U;
+    }
+
+    buf = NWD(lhs, rhs);
+    if(buf){
+        return lhs / buf * rhs;
+    }else{
+        return 0;
+    }
 }
