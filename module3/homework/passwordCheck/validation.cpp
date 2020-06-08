65e0adc942884112abfa7693fb982a15a8cf34cd
Kali Live user <wiktorbigaj@wp.pl>
2020-06-08 14:04:01 +0000

    restored lambas to make program work

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -31,13 +31,13 @@ ErrorCode checkPasswordRules(const std::string& password) {
     if (password.size() < minPasswordSize) {
         return ErrorCode::PasswordNeedsAtLeastNineCharacters;
     }
-    if (std::none_of(password.cbegin(), password.cend(), isdigit())) {
+    if (std::none_of(password.cbegin(), password.cend(), [](char i){ return isdigit(i); })) {
         return ErrorCode::PasswordNeedsAtLeastOneNumber;
     }
-    if (std::none_of(password.cbegin(), password.cend(), ispunct())) {
+    if (std::none_of(password.cbegin(), password.cend(), [](char i){ return ispunct(i); })) {
         return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
     }
-    if (std::none_of(password.cbegin(), password.cend(), isupper(i))) {
+    if (std::none_of(password.cbegin(), password.cend(), [](char i){ return isupper(i); })) {
         return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
     }
     return ErrorCode::Ok;
