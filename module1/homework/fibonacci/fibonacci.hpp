3e9dc35d88a7785146575d838f602481e248bfd5
Papierkowy <matbaniu@gmail.com>
2020-06-09 12:54:45 -0700

    recursive works

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -2,10 +2,12 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+      return 0;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence < 3){
+		return 1;
+    }
+	return fibonacci_recursive(sequence-2)+fibonacci_recursive(sequence-1);
 }
