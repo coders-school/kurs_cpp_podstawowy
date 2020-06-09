62ae26b6ab36c0e1678d60618ed032f044014203
Papierkowy <matbaniu@gmail.com>
2020-06-09 12:37:15 -0700

    dividing works

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -11,6 +11,11 @@ std::string calculate(const std::string& command, int first, int second) {
     } 
      if (command == "multiply"){
         return std::to_string(first * second);
+    }  if (command == "divide"){
+        if(second == 0){
+             return "Division by 0";;
+        }
+        return std::to_string(first/second);
     } else
     return "Invalid data";
 }
