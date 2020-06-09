4eeec23b3a47dc729e721a4e9a8a4ce2592eae68
Papierkowy <matbaniu@gmail.com>
2020-06-09 13:23:30 -0700

    iterative works that was more difficult

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,29 @@
 #pragma once
+#include <iostream>
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-      return 0;
+    int beforePrevious = 0; 
+    int previous = 1; 
+    int temporarySumResult = 0;
+
+    if(sequence == 0){
+		return 0;
+    }
+   
+    for (int i = 0; i < sequence; i++) {
+      temporarySumResult = beforePrevious + previous;
+      beforePrevious = previous;
+      previous = temporarySumResult;
+   }
+   return beforePrevious;
 }
 
 int fibonacci_recursive(int sequence) {
+
+    if(sequence == 0){
+		return 0;
+    }
     if(sequence < 3){
 		return 1;
     }
