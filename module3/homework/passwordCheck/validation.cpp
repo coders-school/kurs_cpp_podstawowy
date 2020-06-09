f9ec8d26cc4379068aa21732d362b20fd13a0aef
marekjanek <marekjanus3@wp.pl>
2020-06-09 21:21:54 +0200

    Another changes requestet on PR, code formatting etc.

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,61 +1,47 @@
 #include "validation.hpp"
+
 #include <algorithm>
 #include <cctype>
 
-std::string getErrorMessage(ErrorCode error)
-{
-    switch(error)
-    {
-        case ErrorCode::Ok:
-            return "Your password is correct.\n";
-        case ErrorCode::PasswordNeedsAtLeastNineCharacters: 
-            return "Your password needs at least nine characters.\n";
-        case ErrorCode::PasswordNeedsAtLeastOneNumber:
-            return "Your password needs at least one number.\n";
-        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
-            return "Your password needs at least one special character.\n";
-        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
-            return "Your password needs at least one upper case letter.\n";
-        case ErrorCode::PasswordsDoesNotMatch:
-            return "Passwords do not match.\n"; 
-        default:
-            return "Your password is incorrect.\n";
+std::string getErrorMessage(ErrorCode error) {
+    switch (error) {
+    case ErrorCode::Ok:
+        return "Your password is correct.\n";
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+        return "Your password needs at least nine characters.\n";
+    case ErrorCode::PasswordNeedsAtLeastOneNumber:
+        return "Your password needs at least one number.\n";
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+        return "Your password needs at least one special character.\n";
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+        return "Your password needs at least one upper case letter.\n";
+    case ErrorCode::PasswordsDoesNotMatch:
+        return "Passwords do not match.\n";
+    default:
+        return "Your password is incorrect.\n";
     }
 }
 
-bool doesPasswordsMatch(const std::string& firstPass, const std::string& secondPass)
-{
+bool doesPasswordsMatch(const std::string& firstPass, const std::string& secondPass) {
     return firstPass == secondPass;
 }
 
-ErrorCode checkPasswordRules(const std::string& password)
-{
-    if(password.size() < 9)
-    {
+ErrorCode checkPasswordRules(const std::string& password) {
+    if (password.size() < 9) {
         return ErrorCode::PasswordNeedsAtLeastNineCharacters;
-    }
-    else if (!std::any_of(password.begin(), password.end(), [](char letter){ return isdigit(letter); }))
-    {
+    } else if (!std::any_of(password.begin(), password.end(), [](char letter) { return isdigit(letter); })) {
         return ErrorCode::PasswordNeedsAtLeastOneNumber;
-    }
-    else if (!std::any_of(password.begin(), password.end(), [](char letter){ return ispunct(letter); }))
-    {
+    } else if (!std::any_of(password.begin(), password.end(), [](char letter) { return ispunct(letter); })) {
         return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
-    }
-    else if (!std::any_of(password.begin(), password.end(), [](char letter){ return isupper(letter); }))
-    {
+    } else if (!std::any_of(password.begin(), password.end(), [](char letter) { return isupper(letter); })) {
         return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
     }
-    
     return ErrorCode::Ok;
 }
 
-ErrorCode checkPassword(const std::string& firstPass, const std::string& secondPass)
-{
-    if (doesPasswordsMatch(firstPass, secondPass))
-    {
+ErrorCode checkPassword(const std::string& firstPass, const std::string& secondPass) {
+    if (doesPasswordsMatch(firstPass, secondPass)) {
         return checkPasswordRules(firstPass);
     }
-    
     return ErrorCode::PasswordsDoesNotMatch;
 }
--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -1,44 +1,37 @@
 #include "vectorFunctions.hpp"
+
 #include <iostream>
 
-std::vector<std::shared_ptr<int>> generate(int count){
-
-    std::vector <std::shared_ptr<int>> SPvec;
-    for(int i=0; i<count; i++){
+std::vector<std::shared_ptr<int>> generate(int count) {
+    std::vector<std::shared_ptr<int>> SPvec;
+    for (int i = 0; i < count; i++) {
         SPvec.push_back(std::make_shared<int>(i));
     }
     return SPvec;
 }
 
-
-void print(const std::vector<std::shared_ptr<int>>& pSPvec){
-
-    for(const auto& element : pSPvec){
+void print(const std::vector<std::shared_ptr<int>>& pSPvec) {
+    for (auto element : pSPvec) {
         std::cout << element;
     }
 }
 
-
-void add10(const std::vector<std::shared_ptr<int>>& addSPvec){
-    
-    for(auto element : addSPvec){
-        if(!element.get())
+void add10(const std::vector<std::shared_ptr<int>>& addSPvec) {
+    for (auto element : addSPvec) {
+        if (!element.get())
             continue;
-        *element+=10;
+        *element += 10;
     }
-
 }
-void sub10(int * const ptr){
-
-    if(ptr==nullptr)
+void sub10(int* const ptr) {
+    if (ptr == nullptr)
         return;
-    *ptr-=10;
+    *ptr -= 10;
 }
 
-void sub10(const std::vector<std::shared_ptr<int>>& subSPvec){
-    
-    for(auto element : subSPvec){
-        if(!element.get())
+void sub10(const std::vector<std::shared_ptr<int>>& subSPvec) {
+    for (auto element : subSPvec) {
+        if (!element.get())
             continue;
         sub10(element.get());
     }
