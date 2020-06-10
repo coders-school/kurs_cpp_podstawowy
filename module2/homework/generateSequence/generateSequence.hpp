3ba2b7a8263fc0c78532682320dee5f8cdf5b02d
szneler <szneler@gmail.com>
2020-06-11 00:46:55 +0200

    Task4 done

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -2,6 +2,15 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    
+    std::vector<int> result{};
+    auto counter {0};
+    auto currentElement {0};
+
+    while(count>counter){
+        currentElement+=step;
+        result.push_back(currentElement);
+        counter++;
+    }
+    return result;
 }
