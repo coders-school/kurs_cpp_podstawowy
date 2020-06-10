affaa74f068f2ffa31b035e9d47aa40ba19dc835
szneler <szneler@gmail.com>
2020-06-10 23:30:52 +0200

    Task2 done

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+        int tempVal;
+    
+	while(rhs!=0)
+    {
+		tempVal = rhs;
+		rhs = lhs%rhs;
+		lhs = tempVal;	
+	}
+	if(lhs<0)
+    return lhs*(-1);
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    return lhs*rhs/NWD(lhs, rhs);
+    
 }
