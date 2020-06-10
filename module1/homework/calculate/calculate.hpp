427ae21d3a884e383817b5d2422ae79598ddb61c
szneler <szneler@gmail.com>
2020-06-10 16:50:43 +0200

    Adding solved task

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -2,6 +2,25 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+
+    if (command.compare("add") == 0)
+    {
+        return std::to_string(first + second);
+    }
+    else if (command.compare("subtract") == 0)
+    {
+        return std::to_string(first - second);
+    }
+    else if (command.compare("multiply") == 0)
+    {
+        return std::to_string(first * second);
+    }
+    else if (command.compare("divide") == 0)
+    {
+        return std::to_string(first / second);
+    }
+    else
+    {
+        return "Invalid data";
+    }
 }
--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,41 @@
 #pragma once
+#include <vector>
+#include <iostream>
 
-int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_iterative(int sequence)
+{
+    if (sequence == 0)
+    {
+        return 0;
+    }
+    if (sequence == 1 || sequence == 2)
+    {
+        return 1;
+    }
+
+    std::vector<int> elements(2, 1);
+
+    for (int i = 2; i < sequence; i++)
+    {
+        std::vector<int>::iterator it = elements.end();
+        it--;
+        auto lastVal = *it;
+        it--;
+        auto nextToLastVal = *it;
+        auto sum = lastVal + nextToLastVal;
+        elements.emplace_back(sum);
+    }
+    return elements.back();
 }
 
-int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+int fibonacci_recursive(int sequence)
+{
+    if (sequence == 1 || sequence == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    }
 }
