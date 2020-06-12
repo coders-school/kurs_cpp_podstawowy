7f3759265fa6d034fbe1865b164c5fab6a9b71ff
Bartlomiej Gottfried <bargot@o2.pl>
2020-06-12 13:10:24 +0200

    Optimization improvements. Using more 'auto'.

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -7,7 +7,7 @@ std::vector<std::shared_ptr<int>> generate(int count) {
     std::vector<std::shared_ptr<int>> result;
     result.reserve(count);
 
-    for (int i = 0; i< count; i++) {
+    for (auto i = 0; i< count; i++) {
         result.push_back(std::make_shared<int>(i));
     }
     return result;
@@ -15,16 +15,16 @@ std::vector<std::shared_ptr<int>> generate(int count) {
 
 void print(const std::vector<std::shared_ptr<int>>& vec)
 {
-    for (const std::shared_ptr<int> ptr : vec)
+    for (auto ptr : vec)
     {
         std::cout<<*ptr<<", ";
     }
     std::cout<<'\n';
 }
 
-void add10(std::vector<std::shared_ptr<int>>& vec)
+void add10(const std::vector<std::shared_ptr<int>>& vec)
 {
-    for (std::shared_ptr<int> ptr : vec)
+    for (auto ptr : vec)
     {
         if(ptr != nullptr)
         {
@@ -35,15 +35,15 @@ void add10(std::vector<std::shared_ptr<int>>& vec)
 
 void sub10(int * const ptr)
 {
-    if(ptr != nullptr)
+    if(ptr)
     {
         *ptr -= 10;
     }
 }
 
-void sub10(std::vector<std::shared_ptr<int>>& vec)
+void sub10(const std::vector<std::shared_ptr<int>>& vec)
 {
-    for (std::shared_ptr<int> ptr : vec)
+    for (auto ptr : vec)
     {
         sub10(ptr.get());
     }
