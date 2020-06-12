f452ff86b0c2c8086e52c3a129fcc5d10e9a8ec3
Bartlomiej Gottfried <bargot@o2.pl>
2020-06-12 02:38:57 +0200

    Task solved

---
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -0,0 +1,44 @@
+#include "vectorFunctions.hpp"
+#include <iostream>
+
+
+
+std::vector<std::shared_ptr<int>> generate(int count){
+    std::vector<std::shared_ptr<int>> result;
+    result.reserve(count);
+
+    for (int i = 0; i< count; i++){
+        result.push_back(std::make_shared<int>(i));
+    }
+    return result;
+} 
+    
+void print(const std::vector<std::shared_ptr<int>>& vec)
+{
+    for (const std::shared_ptr<int> ptr : vec)
+    {
+        std::cout<<*ptr<<", ";
+    }
+    std::cout<<'\n';
+}    
+    
+void add10(std::vector<std::shared_ptr<int>>& vec)
+{
+    for (std::shared_ptr<int> ptr : vec)
+    {
+        *ptr += 10;
+    }
+}  
+
+void sub10(int * const ptr)
+{
+    *ptr -= 10;
+}
+
+void sub10(std::vector<std::shared_ptr<int>>& vec)
+{
+    for (std::shared_ptr<int> ptr : vec)
+    {
+        sub10(ptr.get());
+    }
+} 
---
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
@@ -0,0 +1,21 @@
+#ifndef VECTOR_FUNCTIONS_HPP
+#define VECTOR_FUNCTIONS_HPP
+
+#include <vector>
+#include <memory>
+
+std::vector<std::shared_ptr<int>> generate(int count);
+    
+void print(const std::vector<std::shared_ptr<int>>& vec);
+    
+void add10(std::vector<std::shared_ptr<int>>& vec);
+
+void sub10(int * const ptr);
+
+void sub10(std::vector<std::shared_ptr<int>>& vec);
+
+
+
+
+#endif
+
