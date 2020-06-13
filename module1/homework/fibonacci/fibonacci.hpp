76dae58a67836a933d344ad04aa489eb95d2beb1
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-06-13 19:55:32 +0200

    changed previousPreviousValue variable name to secondToLastValue

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -4,12 +4,12 @@ int fibonacci_iterative(int sequence) {
     if(sequence == 0 || sequence == 1)
         return sequence;
 
-    int previousPreviousValue, previousValue{0}, currentValue{1};
+    int secondToLastValue, previousValue{0}, currentValue{1};
 
     for(std::size_t i = 1; i < sequence; ++i) {
-        previousPreviousValue = previousValue;
+        secondToLastValue = previousValue;
         previousValue = currentValue;
-        currentValue = previousPreviousValue + previousValue;
+        currentValue = secondToLastValue + previousValue;
     }
     return currentValue;
 }
