637c1c801eae0d26fcaf2f40f2242a1e4458c7e8
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-06-13 19:43:33 +0200

    added curly brackets for if and elseif statements with oneliners

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -2,19 +2,24 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    if(command == "add")
+    if(command == "add") {
         return std::to_string(first + second);  
+    }
     
-    else if (command == "subtract")
+    else if (command == "subtract") {
         return std::to_string(first - second);     
-
-    else if (command == "multiply")
+    }
+    
+    else if (command == "multiply") {
         return std::to_string(first * second);
-
+    }
+    
     else if (command == "divide") {
-        if(second == 0) 
+        if(second == 0) {
             return "Division by 0";
+        }
         return std::to_string(first / second);
     }
+    
     return "Invalid data";
 }
