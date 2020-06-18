cf58ad8988c4639166b90bebba7e75a47e4a2cc4
Przemek Malec <przemyslaw.malec1997@gmail.com>
2020-06-18 22:55:08 +0200

    Printing error measages and passwd eaquals checking  works

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,26 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(ErrorCode err){
+   static const std::map<ErrorCode,std::string> labels  ={{Ok,"OK"},{PasswordNeedsAtLeastNineCharacters,"PasswordNeedsAtLeastNineCharacters"},
+{PasswordNeedsAtLeastOneNumber,"PasswordNeedsAtLeastOneNumber"},
+{PasswordNeedsAtLeastOneSpecialCharacter,"PasswordNeedsAtLeastOneSpecialCharacter"},
+{PasswordNeedsAtLeastOneUppercaseLetter,"PasswordNeedsAtLeastOneUppercaseLetter"},
+{PasswordsDoesNotMatch,"PasswordsDoesNotMatch"}};
+
+return labels.find(err)->second;
+
+}
+bool doesPasswordsMatch(std::string passwd1,std::string passwd2){
+
+ if(passwd1.compare(passwd2)==0)
+     return true;
+ else return false;
+
+
+}
+
+ErrorCode checkPasswordRules(std::string passwd){
+}
+ErrorCode checkPassword(std::string passwd1,std::string passwd2){
+}
+
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,20 @@
 // TODO: I'm empty :) Put enum and function headers here.
 // Don't forget the header guard
+#pragma once
+#include <string>
+#include <map>
+
+enum ErrorCode {
+Ok,
+PasswordNeedsAtLeastNineCharacters,
+PasswordNeedsAtLeastOneNumber,
+PasswordNeedsAtLeastOneSpecialCharacter,
+PasswordNeedsAtLeastOneUppercaseLetter,
+PasswordsDoesNotMatch
+};
+
+std::string getErrorMessage(ErrorCode);
+bool doesPasswordsMatch(std::string passwd1,std::string passwd2);
+ErrorCode checkPasswordRules(std::string passwd);
+ErrorCode checkPassword(std::string passwd1,std::string passwd2);
+
