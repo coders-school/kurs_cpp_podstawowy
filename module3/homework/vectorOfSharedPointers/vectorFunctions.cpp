eed8d2c4ee39228b368bbdf401546bbbc2cfb28d
Przemek Malec <przemyslaw.malec1997@gmail.com>
2020-06-20 17:30:53 +0200

    Vec const ref

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -12,7 +12,7 @@ void print(const std::vector<std::shared_ptr<int> >& vec){
         std::cout << *el << ",";
     }
 }
-void add10(std::vector<std::shared_ptr<int> >& vec){
+void add10(const std::vector<std::shared_ptr<int> >& vec){
     for (auto& el : vec) {
         if (el != nullptr)
             *el += 10;
@@ -23,7 +23,7 @@ void sub10(int* const number){
         *number -= 10;
 }
 
-void sub10(std::vector<std::shared_ptr<int> >& vec){
+void sub10(const std::vector<std::shared_ptr<int> >& vec){
     for (auto& element : vec) {
         sub10(element.get());
     }
--- module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
@@ -3,7 +3,7 @@
 #include <vector>
 std::vector<std::shared_ptr<int>> generate(const int count);
 void print(const std::vector<std::shared_ptr<int>>& vec);
-void add10(std::vector<std::shared_ptr<int>>& vec);
+void add10(const std::vector<std::shared_ptr<int>>& vec);
 void sub10(int *const number);
-void sub10(std::vector<std::shared_ptr<int>>& vec);
+void sub10(const std::vector<std::shared_ptr<int>>& vec);
 
