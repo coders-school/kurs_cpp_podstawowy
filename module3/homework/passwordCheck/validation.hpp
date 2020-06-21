af3018e06f5e5c9f145d5916c99d1f167327676d
Michal <mcamromer@gmail.com>
2020-06-21 21:21:19 +0200

    Declare ErrorCode enum class.

--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,17 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+#include <string>
+
+enum class ErrorCode
+{
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoesNotMatch
+};
+
+
+
+
+
+
