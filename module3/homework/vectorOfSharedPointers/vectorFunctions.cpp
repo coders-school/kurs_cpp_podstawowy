2ac4a0a0c5988b077f9f5fbab925be89728deb85
Michal <mcamromer@gmail.com>
2020-06-28 13:07:22 +0200

    Define and test 2 versions of the sub10 function.

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -30,3 +30,16 @@ void add10(std::vector<std::shared_ptr<int>>& vec)
         **it += 10;
     }
 }
+
+void sub10(std::shared_ptr<int> const ptr)
+{
+    *ptr -= 10;
+}
+
+void sub10(std::vector<std::shared_ptr<int>>& vec)
+{
+    for(std::vector<std::shared_ptr<int>>::iterator it = vec.begin(); it!= vec.end(); it++)
+    {
+        **it -= 10;
+    }
+}
