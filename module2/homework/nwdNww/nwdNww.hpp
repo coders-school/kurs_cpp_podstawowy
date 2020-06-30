b87dc3264de7c89ab0f8295d0e37444514a79b81
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-06-30 20:38:35 +0200

    added implementations of NWD & NWW functions

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,45 @@
 #pragma once
+#include <algorithm>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int& divident = lhs;
+    int& divisor = rhs;
+    
+    if(divident < 0 || divisor < 0) {
+        divident = std::abs(divident);
+        divisor = std::abs(divisor);
+    }
+
+    if (divident == 0 || divisor == 0) {
+        return std::max(divident, divisor);
+    }
+
+    if(divisor > divident) {
+        std::swap(divident, divisor);
+    }
+        
+    int remainder{};
+    do {
+        remainder = divident % divisor;
+        if(remainder != 0) {
+            divident = divisor;
+            divisor = remainder;
+        }
+    } while(remainder != 0);
+
+    return divisor;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs < 0 || rhs < 0) {
+        lhs = std::abs(lhs);
+        rhs = std::abs(rhs);
+    }
+    
+    int NwdValue = NWD(lhs,rhs);
+    if (!NwdValue) {
+        return 0;
+    }
+    
+    return lhs / NwdValue * rhs;
 }
