39e54b99c3754390ab3942318e194851490d4fa7
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-06-30 17:46:55 +0200

    removed unnecessary comments

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -2,7 +2,5 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
     return -1;
 }
