369d2974297ceffc886ae3c1bb4fc26a90720dd7
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-07-04 15:04:50 +0200

    added case default in gerErrorMessage() function

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -11,26 +11,28 @@ std::string getErrorMessage(const ErrorCode& input) {
 // Possible other implementation with a std::map<ErrorCode, std::string>
     std::string returnValue;
     switch(input) {
-        case ErrorCode::Ok:
+        case ErrorCode::Ok: {
             return "Ok";
-            break;
-        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+        }
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters: {
             return "Password Needs At Least Nine Characters";
-            break;
-        case ErrorCode::PasswordNeedsAtLeastOneNumber:
+        }
+        case ErrorCode::PasswordNeedsAtLeastOneNumber: {
             return "Password Needs At Least One Number";
-            break;
-        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+        }
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter: {
             return "Password Needs At Least One Special Character";
-            break;
-        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+        }
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter: {
             return "Password Needs At Least One Uppercase Letter";
-            break;
-        case ErrorCode::PasswordsDoesNotMatch:
+        }
+        case ErrorCode::PasswordsDoesNotMatch: {
             return "Passwords Does Not Match";
-            break;
+        }
+        default: {
+            return std::string("getErrorMessage() error!\n");
+        }
     }
-    return std::string("getErrorMessage() error!\n");
 }
 
 bool doesPasswordsMatch(const std::string& input1, const std::string& input2) {
