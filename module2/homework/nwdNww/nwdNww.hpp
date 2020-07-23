ee115d03b1d6c2b66a7619e6aec7a5803e714772
Kacu <k.ptychu@gmail.com>
2020-07-23 13:32:16 +0200

    NWD Function Done

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,9 +1,16 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+	int pom;
+
+  	while(rhs != 0) {
+    	pom = rhs;
+    	rhs = lhs % rhs;
+    	lhs = pom;  
+  	}
+    return lhs;
 }
+
 
 int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
