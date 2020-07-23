9d831e5eeb64af1da4dffd472e66b1ebc6085be8
Kacu <k.ptychu@gmail.com>
2020-07-23 12:52:12 +0200

    Completed task1: addEven

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -3,6 +3,14 @@
 
 int addEven(const std::vector<int>& numbers) {
     // TODO: Your implementation goes here
+    int result = 0;
+
+    for (const auto& value : numbers) {
+        if (value % 2 == 0) {
+            result += value;
+        }
+    }
+
     // Below return is only to make this function compile now
-    return -1;
+    return result;
 }
