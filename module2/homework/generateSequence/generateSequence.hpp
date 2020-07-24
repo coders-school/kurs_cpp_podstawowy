bf7067a263648e2a2f6d6c72b10f76c7f70f998d
Kacu <k.ptychu@gmail.com>
2020-07-24 10:15:59 +0200

    Task4 Completed - generateSequence

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -1,7 +1,18 @@
 #pragma once
 #include <vector>
+#include <iostream>
 
-std::vector<int> generateSequence(int count, int step) {
+std::vector<int> generateSequence(int count, int step) {    
+    if (count < 1) {
+        return std::vector<int> {};
+    }
+    
+    std::vector<int> result(count, step);
+
+    for (size_t i = 1; i < count; ++i) {
+        result[i] += step * i;
+    }
+
     // TODO: Implement me :)
-    return {};
+    return result;
 }
