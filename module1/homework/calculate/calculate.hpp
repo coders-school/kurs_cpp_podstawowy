ef7eaa4df4adb66169029cdcde4e1be86d0e2b28
Marime <urziemska@gmail.com>
2020-08-12 18:18:24 +0200

    Changed the type of result

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -2,19 +2,19 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    int result;
+    std::string result;
 
     if (command == "add") {
-        result = first + second;
+        result = std::to_string(first + second);
     } else if (command == "subtract") {
-        result = first - second;
+        result = std::to_string(first - second);
     } else if (command == "multiply") {
-        result = first * second;
+        result = std::to_string(first * second);
     } else if (command == "divide") {
         if (second = 0) {
             return "Division by 0";
         } else {
-            result = first / second;
+            result = std::to_string(first / second);
         }
     } else {
         return "Invalid command";
