f10722b1f037165d1beb58167633cd42e577aec3
Marime <urziemska@gmail.com>
2020-08-12 17:48:56 +0200

    Solution of Fibonacci task

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int num = 0;
+    int num2 = 1;
+    int result = 0;
+
+    for (int i = 0; i < sequence; i++) {
+        if (i <= 0) {
+            return 0;
+        } else if (i < 2) {
+            return 1;
+        } else {
+            num = num2;
+            num2 = result;
+            result = num + num2;
+        }
+    }
+
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 0) {
+        return 0;
+    } else if (sequence < 2) {
+        return 1;
+    }
+
+    return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1);
 }
