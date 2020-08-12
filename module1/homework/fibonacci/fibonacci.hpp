d55310e3ee902c607d09ff2aeee0c54720d9c8fc
Marime <urziemska@gmail.com>
2020-08-12 18:08:11 +0200

    Update of iterative function

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,20 +1,20 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
+    if (sequence <= 0) {
+        return 0;
+    } else if (sequence <= 2) {
+        return 1;
+    }
+
     int num = 0;
     int num2 = 1;
     int result = 0;
 
     for (int i = 0; i < sequence; i++) {
-        if (i <= 0) {
-            return 0;
-        } else if (i < 2) {
-            return 1;
-        } else {
-            num = num2;
-            num2 = result;
-            result = num + num2;
-        }
+        num = num2;
+        num2 = result;
+        result = num + num2;
     }
 
     return result;
