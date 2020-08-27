196fc19bd5fe0cba36d5cc523b205ee7b210f49b
Marime <mira.wisna@gmail.com>
2020-08-27 20:20:30 +0200

    password check homework added

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,57 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+#include <algorithm>
+#include <iostream>
+
+std::string getErrorMessage(const ErrorCode& caseOfError) {
+    switch (caseOfError) {
+    case ErrorCode::Ok:
+        return "The passwords are OK!";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+        return "Password needs at least nine characters.";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneNumber:
+        return "Password needs at least one number.";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+        return "Password needs at least one special character.";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+        return "Password needs at least one uppercase letter";
+        break;
+    case ErrorCode::PasswordsDoesNotMatch:
+        return "Passwords do not match.";
+        break;
+    default:
+        return "Error";
+    }
+}
+
+bool doesPasswordsMatch(const std::string& password, const std::string& repeatedPassword) {
+    return password == repeatedPassword;
+}
+
+ErrorCode checkPasswordRules(std::string& input) {
+    const auto front = input.cbegin();
+    const auto end = input.cend();
+    if (input.length() < 9) {
+        return ErrorCode::PasswordNeedsAtLeastNineCharacters;
+    } else if (input.find_first_of("0123456789") != std::string::npos) {
+        return ErrorCode::PasswordNeedsAtLeastOneNumber;
+    } else if (!std::any_of(front, end, ::ispunct)) {
+        return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+    } else if (!std::any_of(front, end, ::isupper)) {
+        return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
+    } else {
+        return ErrorCode::Ok;
+    }
+}
+
+ErrorCode checkPassword(const std::string& password, const std::string& repeatedPassword) {
+    if (doesPasswordsMatch(password, repeatedPassword) == false) {
+        return ErrorCode::PasswordsDoesNotMatch;
+    } else {
+        return checkPasswordRules(password);
+    }
+}
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,17 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+#pragma once
+
+#include <string>
+
+enum class ErrorCode {
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoesNotMatch,
+};
+
+std::string getErrorMessage(const ErrorCode& caseOfError);
+bool doesPasswordsMatch(const std::string& password, const std::string& repeatedPassword);
+ErrorCode checkPasswordRules(const std::string& input);
+ErrorCode checkPassword(const std::string& password, const std::string& repeatedPassword);
