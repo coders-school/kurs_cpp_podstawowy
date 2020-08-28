3defdb616d42d9b80ca5522fae50ad07120eddb2
Marime <mira.wisna@gmail.com>
2020-08-28 14:38:46 +0200

    made changes by recommendation of reviewer

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -7,22 +7,16 @@ std::string getErrorMessage(const ErrorCode& caseOfError) {
     switch (caseOfError) {
     case ErrorCode::Ok:
         return "OK";
-        break;
     case ErrorCode::PasswordNeedsAtLeastNineCharacters:
         return "Password needs at least nine characters.";
-        break;
     case ErrorCode::PasswordNeedsAtLeastOneNumber:
         return "Password needs at least one number.";
-        break;
     case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
         return "Password needs at least one special character.";
-        break;
     case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
         return "Password needs at least one uppercase letter";
-        break;
     case ErrorCode::PasswordsDoesNotMatch:
         return "Passwords do not match.";
-        break;
     default:
         return "Error";
     }
@@ -37,15 +31,18 @@ ErrorCode checkPasswordRules(const std::string& input) {
     const auto end = input.cend();
     if (input.length() < 9) {
         return ErrorCode::PasswordNeedsAtLeastNineCharacters;
-    } else if (input.find_first_of("0123456789") != std::string::npos) {
-        return ErrorCode::PasswordNeedsAtLeastOneNumber;
-    } else if (!std::any_of(front, end, ::ispunct)) {
-        return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
-    } else if (!std::any_of(front, end, ::isupper)) {
-        return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
-    } else {
-        return ErrorCode::Ok;
     }
+    if (input.find_first_of("0123456789") != std::string::npos) {
+        return ErrorCode::PasswordNeedsAtLeastOneNumber;
+    }
+    if (!std::any_of(front, end, ::ispunct)) {
+        return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+    }
+    if (!std::any_of(front, end, ::isupper)) {
+        return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
+    }
+
+    return ErrorCode::Ok;
 }
 
 ErrorCode checkPassword(const std::string& password, const std::string& repeatedPassword) {
