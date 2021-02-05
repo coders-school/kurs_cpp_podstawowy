311639bd28ae50d8675405cc62ccab713daf55a6
BarTes8 <barbara.tessema@gmail.com>
2021-02-05 19:07:51 +0100

    tests passed

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -2,10 +2,28 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    int total = 0, first = 0, second = 1;
+    if (sequence == 0)
+	    return 0;
+    else if (sequence == 1)
+	    return 1;
+    else{
+    	for (int i = 0; i<sequence-1; i++){
+		total = first + second;
+		first = second;
+		second = total;
+	}
+}
+	return total;
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
+    if (sequence == 0)
+	    return 0;
+    else if (sequence == 1)
+	    return 1;
+    else
+	    return fibonacci_recursive(sequence -2) + fibonacci_recursive(sequence - 1);
     return 0;
 }
