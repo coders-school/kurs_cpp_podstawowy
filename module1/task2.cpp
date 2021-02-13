f2f277c3abcd13740976c22be17fd2395502050f
lukasz-kukulka <qqla83@gmail.com>
2021-02-13 04:18:37 +0000

    task2 change

--- module1/task2.cpp
+++ module1/task2.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-// Write your function here
+int max(int a, int b, int c){
+    if (a >= b && a >= c){
+        return a;
+    }
+    else if (b >= c) {
+        return b;
+    }
+    else {
+        return c;
+    }
+}
 
 int main() {
     std::cout << "max (1, 2, 3): " << max(1, 2, 3) << "\n";
