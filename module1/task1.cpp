40345eb29ce16899fa83fdbe4930a27d1adb6d97
lukasz-kukulka <qqla83@gmail.com>
2021-02-13 04:10:43 +0000

    task1 change

--- module1/task1.cpp
+++ module1/task1.cpp
@@ -2,6 +2,10 @@
 
 // Write missing function here
 
+int multiply(int a, int b){
+    return a * b;
+}
+
 int main() {
     std::cout << "4 * 5: " << multiply(4, 5) << "\n";
     std::cout << "10 * 5: " << multiply(10, 5) << "\n";
