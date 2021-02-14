7bfbecaf526022dfece15dd8ce65f094bec6ae00
lukasz-kukulka <qqla83@gmail.com>
2021-02-14 03:49:51 +0000

    Calculate Divide

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -11,6 +11,8 @@ std::string calculate(const std::string& command, int first, int second) {
     else if (command == "multiply"){
         return std::to_string(first * second);
     }
-    
+    else if (command == "divide"){
+        return std::to_string(first / second);
+    }
     return "";
 }
