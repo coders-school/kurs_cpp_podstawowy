722625c2617a426d337cd31a096a2077024b6123
lukasz-kukulka <qqla83@gmail.com>
2021-02-15 04:20:43 +0000

    Fibonacci Done

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,26 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0){
+        return 0;
+    }
+    uint32_t first{0}, second{1};
+    for (int i = 1; i < sequence; i++){
+        second += first;
+        first = second - first;
+    }
+    return second;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0){
+        return 0;
+    }
+    else if (sequence < 3){
+        return 1;
+    }
+    else{
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    }
 }
+
