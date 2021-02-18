cd2be7e1b251198b102131c3fcfe231f4bfe7600
lukasz-kukulka <qqla83@gmail.com>
2021-02-18 06:34:54 +0000

    Revert "Basic #2 task2 DONE"
    This reverts commit a46817c830429c48ffecf896904d302af87abba5.

--- module2/task2.cpp
+++ module2/task2.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
 #include <vector>
 
-auto concatenateVector(std::vector<int>& first_vector, std::vector<int>& second_vector){
-    std::vector<int> result_vector;
-    for (size_t i = 0; i < first_vector.size() || i < second_vector.size(); i++){
-        if (i < first_vector.size()){
-            result_vector.push_back((first_vector[i]));
-        }
-        if (i < second_vector.size()){
-            result_vector.push_back((second_vector[i]));
-        }
-    }
-    return result_vector;
-}
+// Implement concatenateVector.
+// It should take 2 vectors and return one which has alternately elements from the first and the other one.
 
 int main() {
-    std::vector<int> vec1 {1, 2, 3, 4};
-    std::vector<int> vec2 {11, 12, 13, 14, 15, 16, 17};
+    std::vector<int> vec1 {1, 2, 3, 4, 5};
+    std::vector<int> vec2 {11, 12, 13, 14, 15};
 
     auto vec = concatenateVector(vec1, vec2);
     for (const auto& el : vec) {
