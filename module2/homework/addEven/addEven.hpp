431e128ee96ce613647ec1541213af3cc281d9a8
lukasz-kukulka <qqla83@gmail.com>
2021-02-21 18:04:49 +0000

    Homework module2 addEven

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -2,7 +2,11 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int sum = 0;
+    for (const auto& ele : numbers){
+        if (ele % 2 == 0){
+            sum += ele;
+        }
+    }
+    return sum;
 }
