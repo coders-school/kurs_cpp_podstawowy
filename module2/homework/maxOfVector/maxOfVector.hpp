510485f18e63e1b1796e9868117fd002807d4064
lukasz-kukulka <qqla83@gmail.com>
2021-02-21 19:36:29 +0000

    Homework module2 MaxOfVector

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -3,6 +3,11 @@
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int sum = vec[0];
+    for (const auto & ele : vec){
+        if (sum < ele){
+            sum = ele;
+        }
+    }
+    return sum;
 }
