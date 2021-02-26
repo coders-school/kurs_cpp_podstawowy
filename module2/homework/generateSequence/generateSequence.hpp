70b621a94d67042e6af6f65bb8734c0744d31269
Gyrydymptum <gyrydymptum@gmail.com>
2021-02-26 21:39:48 +0100

    adding works

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -1,7 +1,14 @@
 #pragma once
 #include <vector>
 
-std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
-}
+
+std::vector<int> generateSequence(int count, int step){
+    std::vector<int> generate;
+    int j = 0;
+        for (int i= 0; i<count; i++){
+            j=j+step;
+            generate.push_back(j);
+            }
+        return generate;
+    }  
+
