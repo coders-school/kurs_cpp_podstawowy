2be13cdb28b5177bc8e9dea9a983f1c680b72134
Barbara Tessema <barbara.tessema@gmail.com>
2021-03-04 16:37:54 +0100

    vectorOfSharedPointers tests passed

---
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -0,0 +1,36 @@
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count){
+    if (count < 1)
+        return {};
+    std::vector<std::shared_ptr<int>> vec;
+    vec.reserve(count);
+    for(size_t i = 0; i < count; i++){
+        vec.push_back(std::make_shared<int>(i));
+    }
+    return vec;
+}
+
+void print(std::vector<std::shared_ptr<int>> vec){
+    for(const auto& el : vec){
+        std::cout << *el << '\n';
+    }
+}
+
+void add10(std::vector<std::shared_ptr<int>> vec){
+    for(auto & el : vec){
+        if(el != nullptr)
+            *el += 10;
+    }
+}
+
+void sub10(int *const intPtr){
+    if(intPtr)
+        *intPtr -= 10;
+}
+
+void sub10(std::vector<std::shared_ptr<int>> vec){
+    for(const auto& el : vec){
+        sub10(el.get());
+    }
+}
