67b7a84f7e9a2bcf74fc92f1f7863eafc3e9f6ee
MorawskiR <r.morawski92@gmail.com>
2021-03-11 17:51:06 -0500

    homework

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -3,6 +3,23 @@
 #include <limits>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    // for(auto it = vec.begin() + 1 ; it != vec.end(); ++it)
+    // {
+    //     temp = vec[0];
+    //     if (*it > temp)
+    //     {
+    //         temp = *it;
+            
+    //     }
+    //      std::cout<<"wieksza = "<<temp<<std::endl;
+    // }
+    int temp = vec[0];
+    for(const auto& el : vec)
+    {
+        if (el > temp)
+        {
+            temp = el;
+        }
+    }
+    return temp;
 }
