a35178d3aa96ac888786d8d58a0f5a698334f2cf
PawelYak <pawelstojak@gmail.com>
2021-03-12 16:55:47 +0100

    commit test

--- module1/homework/calculate/calculate.hpp
+++ module1/homework/calculate/calculate.hpp
@@ -18,12 +18,12 @@ std::string calculate(const std::string& command, int first, int second){
     }
     else if (command == "divide") {
 
-            if (second==0){
+	    if ((first||second)==0){
             return std::string("Division by 0");
                     }
-                    else if(first==0){
-            return std::string("Division by 0");
-                    }
+          
+            
+           
                         else if((first && second)!=0){
             return std::to_string(first/second);
                         }
