c6db16eb865a621eacf9ce58d5fb855c3e2ae14b
pwiatr3 <pwiatr3@yahoo.pl>
2021-04-14 11:50:17 +0200

    fibonacci works

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,40 @@
 #pragma once
+#include <iostream>
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    int an1 = 1; // a(n-1) - n-1 element of fibonacci series
+    int an2 = 0; // a(n-2) - n-2 element of fibonacci series
+    int an = 0; // a(n) - n element of fibonacci series
+
+    if(sequence == 0){
+        return 0;
+    }
+    if(sequence == 1){
+        return 1;
+    }
+    if (sequence >= 2){
+        for(int n = 2; n <= sequence; n++){
+            an = an2 + an1;
+            an2 = an1;
+            an1 = an;
+        }
+        return an;
+    }
+        
 }
 
 int fibonacci_recursive(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+
+    if(sequence == 0){
+        return 0;
+    }
+    if(sequence == 1){
+        return 1;
+    }
+    if(sequence >= 2) {
+        return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1);
+    }
 }
+
--- module1/homework/fibonacci/main.cpp
+++ module1/homework/fibonacci/main.cpp
@@ -2,8 +2,8 @@
 #include "fibonacci.hpp"
 
 int main() {
-    std::cout << fibonacci_iterative(10) << "\n";
-    std::cout << fibonacci_recursive(10) << "\n";
+    std::cout << fibonacci_iterative(19) << "\n";
+    std::cout << fibonacci_recursive(19) << "\n";
 
     return 0;
 }
