a1c59a4c09c4dbe07263ac8007fa13bb2ef7668f
Krzychtof <kkowalczyk.it@gmail.com>
2021-04-21 14:56:35 +0200

    krzychtof: Zadanie 1 - AddEven

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -2,7 +2,15 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    if(numbers.empty()){
+        std::cerr << "Vector is empty!";
+        return 0;
+    }
+    int sum{};
+    for(const auto& number : numbers){
+        if(!(number % 2)){
+            sum += number;
+        }
+    }
+    return sum;
 }
